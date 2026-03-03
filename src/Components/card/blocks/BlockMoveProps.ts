c5358f5ce1b310ef0908abf5e1658d39d9c00409
Tapi221 <aaatsub1@gmail.com>
2026-03-03 13:29:32 +0900

    componentにgit status修正

--- src/Components/card/blocks/BlockMoveProps.ts
+++
@@ -1,8 +0,0 @@
-export interface BlockMoveProps {
-  canMoveUp?: boolean;
-  canMoveDown?: boolean;
-  onMoveUp?: () => void;
-  onMoveDown?: () => void;
-  onMoveDragStart?: () => void;
-  onMoveDragEnd?: () => void;
-}
