fce2e2cda6b29605463107d9246bfee29b9d6072
Tapi221 <aaatsub1@gmail.com>
2026-04-05 04:40:46 +0900

    辞書未実装プレースホルダー削除

--- src/pages/Dictionary.tsx
+++ src/pages/Dictionary.tsx
@@ -1,7 +1,8 @@
-import NotImplementedPlaceholder from "./NotImplementedPlaceholder";
+import { Navigate } from "react-router-dom";
 
 const Dictionary = () => {
-  return <NotImplementedPlaceholder />;
+  // 辞書ページは現在未実装のため、フォルダ一覧ページにリダイレクトする
+  return <Navigate to="/folders" replace />;
 };
 
 export default Dictionary;
