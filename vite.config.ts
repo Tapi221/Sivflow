eeae9ee2441b100ab3e9f46266617d8d617cc5c3
Tapi221 <aaatsub1@gmail.com>
2026-04-06 21:47:34 +0900

    デフォルト 2 MiB 制限無効化

--- vite.config.ts
+++ vite.config.ts
@@ -20,6 +20,7 @@ export default defineConfig({
       includeAssets: ["favicon.ico", "apple-touch-icon.png", "mask-icon.svg"],
       injectManifest: {
         globIgnores: ["**/index.html"],
+        maximumFileSizeToCacheInBytes: 5 * 1024 * 1024,
       },
       devOptions: {
         enabled: false, // 開発環境ではPWA無効化（manifest生成は行われる）
