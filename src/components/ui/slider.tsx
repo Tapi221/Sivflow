21d88fbaecb24dc4625832fb1af550f49ccbef32
Tapi221 <aaatsub1@gmail.com>
2026-04-07 00:37:45 +0900

    エラー修正2

--- src/components/ui/slider.tsx
+++ src/components/ui/slider.tsx
@@ -3,7 +3,12 @@ import * as SliderPrimitive from "@radix-ui/react-slider";
 
 import { cn } from "@/lib/utils";
 
-const Slider = React.forwardRef(({ className, ...props }, ref) => (
+type SliderProps = React.ComponentPropsWithoutRef<typeof SliderPrimitive.Root>;
+
+const Slider = React.forwardRef<
+  React.ElementRef<typeof SliderPrimitive.Root>,
+  SliderProps
+>(({ className, ...props }, ref) => (
   <SliderPrimitive.Root
     ref={ref}
     className={cn(
@@ -18,6 +23,7 @@ const Slider = React.forwardRef(({ className, ...props }, ref) => (
     <SliderPrimitive.Thumb className="block h-4 w-4 rounded-full border border-primary/50 bg-background shadow transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50" />
   </SliderPrimitive.Root>
 ));
+
 Slider.displayName = SliderPrimitive.Root.displayName;
 
 export { Slider };
