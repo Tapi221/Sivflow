de266820c7b32bcb03e827f4551ada75616e1959
Tapi221 <aaatsub1@gmail.com>
2026-04-20 15:08:07 +0900

    メタパネル開閉アイコン

--- src/layout/TitleBar.tsx
+++ src/layout/TitleBar.tsx
@@ -409,25 +409,11 @@ export const TitleBar: React.FC = () => {
               tabIndex={-1}
               style={noDragStyle}
             >
-              {isCardSetViewMetaOpen ? (
-                <svg
-                  width="10"
-                  height="10"
-                  viewBox="0 0 10 10"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                  aria-hidden="true"
-                >
-                  <path
-                    d="M1 1L9 9M9 1L1 9"
-                    stroke="currentColor"
-                    strokeWidth="1.2"
-                    strokeLinecap="round"
-                  />
-                </svg>
-              ) : (
-                <MetaPanelToggleIcon width="14" height="14" />
-              )}
+              <MetaPanelToggleIcon
+                open={isCardSetViewMetaOpen}
+                width="14"
+                height="14"
+              />
             </button>
           </>
         )}
