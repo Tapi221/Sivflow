9790f34a6e9697c4aca40626031907d221635546
Tapi221 <aaatsub1@gmail.com>
2026-04-24 00:12:28 +0900

    cleanup

---
+++ pdf_workspace_dnd_fix_39a9a2f6/src/components/pdf/usePdfWorkspace.ts
@@ -0,0 +1,17 @@
+
+import { useContext } from "react";
+
+import {
+  PdfWorkspaceContext,
+  type PdfWorkspaceContextValue,
+} from "@/components/pdf/PdfWorkspaceProvider";
+
+export const usePdfWorkspace = (): PdfWorkspaceContextValue => {
+  const context = useContext(PdfWorkspaceContext);
+
+  if (!context) {
+    throw new Error("usePdfWorkspace must be used within PdfWorkspaceProvider");
+  }
+
+  return context;
+};
