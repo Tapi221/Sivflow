f61b7f60aba6f3dcf37ccef12d2c6cf94998571c
Tapi221 <aaatsub1@gmail.com>
2026-04-26 16:16:52 +0900

    カードアイコン

--- src/components/explorer/icons/index.ts
+++ src/components/explorer/icons/index.ts
@@ -1,3 +1,3 @@
 export { ExplorerChromeFolderIcon } from "./ExplorerChromeFolderIcon";
 export { ExplorerChromeCardSetIcon } from "./ExplorerChromeCardSetIcon";
-
+export { ExplorerChromeCardIcon } from "./ExplorerChromeCardIcon";
