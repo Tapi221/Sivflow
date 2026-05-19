9a56e220f4203fe44690acb27407d37769acf83c
Tapi221 <aaatsub1@gmail.com>
2026-05-19 22:46:59 +0900

    calendaricon変更

--- src/features/settings/panes/LanguagePane.tsx
+++ src/features/settings/panes/LanguagePane.tsx
@@ -1,5 +1,18 @@
+import { useState } from "react";
+
 const LanguagePane = () => {
-  return <div>言語設定</div>;
+  const [lang, setLang] = useState("ja");
+
+  return (
+    <div>
+      <h2>言語設定</h2>
+
+      <select value={lang} onChange={(e) => setLang(e.target.value)}>
+        <option value="ja">日本語</option>
+        <option value="en">English</option>
+      </select>
+    </div>
+  );
 };
 
 export default LanguagePane;
