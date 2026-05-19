1515d06bc13b5a0ab66f34b375edcb99368b0ee2
Tapi221 <aaatsub1@gmail.com>
2026-05-19 15:13:11 +0900

    a

--- src/features/settings/SettingDialog.tsx
+++ src/features/settings/SettingDialog.tsx
@@ -32,11 +32,20 @@ const SettingDialog = ({
           className="
             ds-settings-panel-dialog__surface
             w-full
-            min-h-[560px]
             h-[min(780px,calc(100vh-64px))]
+            min-h-[560px]
+            flex
             rounded-lg
+            overflow-hidden
           "
-        />
+        >
+          <div className="w-[280px] shrink-0 bg-muted/30">
+          </div>
+
+          <div className="w-px bg-border" />
+          <div className="flex-1 bg-background">
+          </div>
+        </div>
       </DialogContent>
     </Dialog>
   );
