2ded393a3c7ea990858c848e3a16a7a639a2e1f5
Tapi221 <aaatsub1@gmail.com>
2026-05-22 17:25:18 +0900

    Shift day detail time labels left

--- src/features/calendar/grid/Grid.calendar.daydetail.desktop.tsx
+++ src/features/calendar/grid/Grid.calendar.daydetail.desktop.tsx
@@ -89,7 +89,7 @@ export const GridCalendarDayDetailDesktop = ({ events }: Props) => {
             <span
               className="
                 absolute
-                right-2
+                right-3
                 top-0
                 flex
                 h-6
