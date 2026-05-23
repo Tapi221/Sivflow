06cfd5772318159635f9abd42a1d069ef7e5cf1f
Tapi221 <aaatsub1@gmail.com>
2026-05-24 06:52:15 +0900

    i

--- src/components/icons/icons.card.tsx
+++ src/components/icons/icons.card.tsx
@@ -30,3 +30,57 @@ export const FlagIcon = ({
     />
   </svg>
 );
+
+//Vuesax-Icons
+export const TrashIcon = ({
+  className,
+  label: _label,
+  size: _size,
+  title: _title,
+  ...props
+}: IconProps) => (
+  <svg
+    viewBox="0 0 24 24"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    className={className}
+    aria-hidden="true"
+    {...props}
+  >
+    <path
+      d="M3 6H21"
+      stroke="currentColor"
+      strokeWidth="1.5"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+    <path
+      d="M8 6V4C8 2.9 8.9 2 10 2H14C15.1 2 16 2.9 16 4V6"
+      stroke="currentColor"
+      strokeWidth="1.5"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+    <path
+      d="M19 9L18.35 19.2C18.25 20.75 18.17 22 15.42 22H8.58C5.83 22 5.75 20.75 5.65 19.2L5 9"
+      stroke="currentColor"
+      strokeWidth="1.5"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+    <path
+      d="M10.33 16.5H13.66"
+      stroke="currentColor"
+      strokeWidth="1.5"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+    <path
+      d="M9.5 12.5H14.5"
+      stroke="currentColor"
+      strokeWidth="1.5"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  </svg>
+);
