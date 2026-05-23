dd1cfb6524aec460745470eb8f9a82de83ba8a78
Tapi221 <aaatsub1@gmail.com>
2026-05-23 14:17:29 +0900

    Remove legacy folders route rewrite

--- src/routes/Folders.tsx
+++ src/routes/Folders.tsx
@@ -1,23 +1,8 @@
-import { useEffect } from "react";
-import { useLocation } from "react-router-dom";
-
 import { FoldersScreen } from "@/features/explorer";
 import { useFoldersRouteAdapter } from "@/features/explorer/adapters/web/useFoldersRouteAdapter";
 
 const Folders = () => {
   const route = useFoldersRouteAdapter();
-  const location = useLocation();
-
-  useEffect(() => {
-    const searchParams = new URLSearchParams(location.search);
-
-    if (
-      location.pathname === "/folders" &&
-      searchParams.get("view") === "section-list"
-    ) {
-      window.history.replaceState(window.history.state, "", "/library");
-    }
-  }, [location.pathname, location.search]);
 
   return <FoldersScreen route={route} />;
 };
