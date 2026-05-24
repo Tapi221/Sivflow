ab1843b743852bfe9dc7619c932eb876a01a8987
Tapi221 <aaatsub1@gmail.com>
2026-05-25 08:21:40 +0900

    refactor: move ink layer types to .types module

--- src/components/ink/types.ts
+++ src/components/ink/types.ts
@@ -1,11 +1,1 @@
-export type InkHistoryState = {
-  canUndo: boolean;
-  canRedo: boolean;
-  strokeCount: number;
-};
-
-export interface InkLayerHandle {
-  undo: () => void;
-  redo: () => void;
-  clear: () => void;
-}
+export * from "./inkLayer.types";
