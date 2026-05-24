f00b7eb24ab07d099fd855d9f6fcde81f5c22377
Tapi221 <aaatsub1@gmail.com>
2026-05-24 19:47:45 +0900

    Use task icon for task status marker

--- src/chip/icon/TaskStatusDot.tsx
+++ src/chip/icon/TaskStatusDot.tsx
@@ -1,14 +1,19 @@
+import { CrownIcon } from "@/components/icons/icons.task";
+
+import { cn } from "@/lib/utils";
+
 type TaskStatusDotProps = {
   color?: string;
+  className?: string;
 };
 
-export const TASK_STATUS_DOT_CLASS_NAME = "h-2 w-2 shrink-0 rounded-full";
+export const TASK_STATUS_DOT_CLASS_NAME = "h-3.5 w-3.5 shrink-0";
 
-export const TaskStatusDot = ({ color }: TaskStatusDotProps) => {
+export const TaskStatusDot = ({ color, className }: TaskStatusDotProps) => {
   return (
-    <span
-      className={TASK_STATUS_DOT_CLASS_NAME}
-      style={{ backgroundColor: color }}
+    <CrownIcon
+      className={cn(TASK_STATUS_DOT_CLASS_NAME, className)}
+      style={{ color }}
     />
   );
 };
