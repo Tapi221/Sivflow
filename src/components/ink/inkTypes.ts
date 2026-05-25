4e477431955d0217cde3bd8136c79e6baec8016c
Tapi221 <aaatsub1@gmail.com>
2026-05-25 09:36:49 +0900

    refactor: move ink document definitions to types module

--- src/components/ink/inkTypes.ts
+++ src/components/ink/inkTypes.ts
@@ -1,1 +1,2 @@
-export * from "@/domain/card/inkDocument";
+export type { InkDocument, InkStroke, InkPoint } from "./ink.types";
+export { EMPTY_INK_DOCUMENT, INK_DOCUMENT_VERSION, createEmptyInkDocument } from "./ink.types";
