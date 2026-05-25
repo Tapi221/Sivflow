1655bae7b125834b2a43146ab4db782358100564
Tapi221 <aaatsub1@gmail.com>
2026-05-25 10:34:45 +0900

    Keep image database writer imports on single lines

--- src/services/ImageDatabaseWriter.ts
+++ src/services/ImageDatabaseWriter.ts
@@ -1,11 +1,6 @@
 import { getImageFromFirestore } from "@/infrastructure/images/imageFirestoreReader";
-import {type FirestoreTarget,
-  isImageFirestoreDiagnosticsEnabled,
-  resolveImageFirestoreTarget,
-  saveImageBatchToFirestore,
-  saveImageToFirestore,} from "@/infrastructure/images/imageFirestoreWriter";
-import {getImageFromIndexedDb,
-  saveImageToIndexedDb,} from "@/infrastructure/images/imageIndexedDbStore";
+import { type FirestoreTarget, isImageFirestoreDiagnosticsEnabled, resolveImageFirestoreTarget, saveImageBatchToFirestore, saveImageToFirestore } from "@/infrastructure/images/imageFirestoreWriter";
+import { getImageFromIndexedDb, saveImageToIndexedDb } from "@/infrastructure/images/imageIndexedDbStore";
 import type { UploadedImage } from "@/types";
 
 class ImageDatabaseWriter {
