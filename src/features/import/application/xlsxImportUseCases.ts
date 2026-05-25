8767b5fcc80dc78a8b1ed5e677baf1ec5a4870d3
Tapi221 <aaatsub1@gmail.com>
2026-05-25 15:53:13 +0900

    refactor: import xlsx use case types directly

--- src/features/import/application/xlsxImportUseCases.ts
+++ src/features/import/application/xlsxImportUseCases.ts
@@ -1,8 +1,5 @@
-import {buildImportCardSetName,
-  importCardsFromPayload,
-  type ImportDestination,} from "@/features/import/application/importCards";
-import {hasImportBlockingError,
-  type ImportParseResult,} from "@/features/import/domain/importTypes";
+import { buildImportCardSetName, importCardsFromPayload, type ImportDestination } from "@/features/import/application/importCards";
+import { hasImportBlockingError, type ImportParseResult } from "@/features/import/domain/import.types";
 import { parseXlsxImport } from "@/features/import/infra/web/parseXlsxImport";
 
 import type { Card, CardSet } from "@/types";
