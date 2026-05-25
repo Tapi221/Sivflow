ea4e0954646aee4fdc061f58e7eb55a34abbc16c
Tapi221 <aaatsub1@gmail.com>
2026-05-25 11:28:36 +0900

    refactor: remove legacy ink types file

--- src/components/ink/inkTypes.ts
+++
@@ -1,2 +0,0 @@
-export type { InkDocument, InkEditTool, InkPoint, InkSide, InkStroke, InkTool } from "./ink.types";
-export { INK_DOCUMENT_VERSION, INK_PAPER_H, INK_PAPER_W, cloneInkDocument, createEmptyInkDocument, normalizeInkDocument } from "./ink.types";
