bdcea40e2254d77d0cd4dff6ab19f47cd9040eb0
Tapi221 <aaatsub1@gmail.com>
2026-05-25 09:50:43 +0900

    Delete unused Google Calendar relink panel

--- src/chip/minipanel/GcalRelinkPanel.tsx
+++
@@ -1,8 +0,0 @@
-type GcalRelinkPanelProps = {
-  message?: string;
-  detail?: string | null;
-  onReconnect: () => void;
-  className?: string;
-};
-
-export const GcalRelinkPanel = (_props: GcalRelinkPanelProps) => null;
