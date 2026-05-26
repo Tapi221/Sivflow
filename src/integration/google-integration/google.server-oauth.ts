4ada05b269f80a9b99ea13eafeee979e7000ac5a
Tapi221 <aaatsub1@gmail.com>
2026-05-27 03:25:21 +0900

    Add shared Google server OAuth entrypoint

---
+++ src/integration/google-integration/google.server-oauth.ts
@@ -0,0 +1,2 @@
+export * from "@/integration/googlecalendar-integration/gcal.server-oauth";
+export type { GoogleOAuthCallableErrorReason } from "@/integration/googlecalendar-integration/gcal.server-oauth";
