a0901d3cd612cf10560005480e9cd27986da5e64
Tapi221 <aaatsub1@gmail.com>
2026-05-27 03:33:09 +0900

    Fix IndexedDB rebuild import aliases

--- src/services/IndexedDBRebuildOrchestrator.ts
+++ src/services/IndexedDBRebuildOrchestrator.ts
@@ -1,7 +1,7 @@
 import { Dexie } from "dexie";
 import { getLocalDb } from "./localDB";
-import { CURRENT_TAG_STORE } from "./localdb/tagStoreNames";
-import { CloudSyncAdapter } from "./logic/CloudSyncAdapter";
+import { CURRENT_TAG_STORE } from "@/services/localdb/tagStoreNames";
+import { CloudSyncAdapter } from "@/services/logic/CloudSyncAdapter";
 import { type BlobUrlFix, sanitizeBlobUrlsDeep } from "@/utils/blobUrlSanitizer";
 import { sanitizeForLog } from "@/utils/logSanitizer";
 
