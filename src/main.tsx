1cc304d23843cc7653c76784b7f682363096bc63
Tapi221 <aaatsub1@gmail.com>
2026-05-27 01:59:36 +0900

    Use runtime entry in main

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,4 @@
-import "./bootstrap/installProductionConsoleFilter";
+import "./runtime/installProductionConsoleFilter";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { QueryClientProvider } from "@tanstack/react-query";
@@ -7,11 +7,11 @@ import "./styles/index.css";
 import "@/features/scroll/schedule/weekdayHeaderScrollBridge";
 import "@/services/localDB";
 import App from "./App";
-import { bootstrapApp } from "./bootstrap/bootstrapApp";
-import { queryClient } from "./bootstrap/queryClient";
+import { startAppRuntime } from "./runtime/startAppRuntime";
+import { queryClient } from "./runtime/queryClient";
 import { ErrorBoundary } from "./components/common/ErrorScreen";
 
-bootstrapApp();
+startAppRuntime();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
