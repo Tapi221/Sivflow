9463438e7a088ada0e57bbf83499aee2191cced9
Tapi221 <aaatsub1@gmail.com>
2026-05-27 05:23:14 +0900

    Derive tag backgrounds from text color

--- src/features/tag/tag.types.ts
+++ src/features/tag/tag.types.ts
@@ -3,7 +3,6 @@ import { TAG_COLOR_KEYS } from "./tag.constants";
 export type TagColorKey = (typeof TAG_COLOR_KEYS)[number];
 
 export type TagColorPalette = {
-  bg: string;
   fg: string;
   border: string;
   swatch: string;
