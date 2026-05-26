9959715e389914e39b927d83b557165f4879839a
Tapi221 <aaatsub1@gmail.com>
2026-05-26 12:44:17 +0900

    Remove utils compatibility re-export file

--- src/utils/index.ts
+++
@@ -1,5 +0,0 @@
-// TODO: remove these compatibility re-exports after remaining callers migrate.
-export { normalizeCard } from "@/domain/card/normalizers/normalizeCard";
-export {normalizeFolder,
-  normalizeFolderWithSilent,} from "@/domain/folder/normalizers/normalizeFolder";
-export { createPageUrl } from "@/platform/web/navigation/toWebPath";
