d646f2e082d2bd4e8944779f3d66f678d74160a3
Tapi221 <aaatsub1@gmail.com>
2026-05-27 00:00:32 +0900

    Move app runtime initialization

---
+++ src/runtime/initAppRuntime.ts
@@ -0,0 +1,16 @@
+import { initGlobalChunkRecovery } from "./initGlobalChunkRecovery";
+import { initManifestDebug } from "./initManifestDebug";
+import { initServiceWorkerLifecycle } from "./initServiceWorkerLifecycle";
+import { startTabPresence } from "@/utils/tabPresence";
+
+let started = false;
+
+export const initAppRuntime = () => {
+  if (started || typeof window === "undefined") return;
+  started = true;
+
+  startTabPresence();
+  initManifestDebug();
+  initGlobalChunkRecovery();
+  initServiceWorkerLifecycle();
+};
