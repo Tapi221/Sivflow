36c66d8f8dc53b974b3bd7c2953633ef5d70d81c
Tapi221 <aaatsub1@gmail.com>
2026-05-27 02:15:20 +0900

    Remove old persistent queue bootstrap name

--- src/platform/web/bootstrapPersistentQueue.ts
+++
@@ -1,6 +0,0 @@
-import { bindPersistentQueueAutoProcessing } from "./bindPersistentQueueAutoProcessing";
-import { persistentQueue } from "@/services/PersistentOfflineQueue";
-
-export const bootstrapPersistentQueue = (): void => {
-  bindPersistentQueueAutoProcessing(persistentQueue);
-};
