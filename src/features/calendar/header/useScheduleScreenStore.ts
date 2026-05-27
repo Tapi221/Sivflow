b01bf54cdad8a9c93b3c764393fec02531256ace
Tapi221 <aaatsub1@gmail.com>
2026-05-27 20:52:42 +0900

    Remove day detail panel store state

--- src/features/calendar/header/useScheduleScreenStore.ts
+++ src/features/calendar/header/useScheduleScreenStore.ts
@@ -2,34 +2,18 @@ import { create } from "zustand";
 
 type ScheduleScreenState = {
   isOpen: boolean;
-  isDayDetailPanelOpen: boolean;
-  canToggleDayDetailPanel: boolean;
   open: () => void;
   close: () => void;
   toggle: () => void;
-  openDayDetailPanel: () => void;
-  closeDayDetailPanel: () => void;
-  toggleDayDetailPanel: () => void;
-  setCanToggleDayDetailPanel: (canToggleDayDetailPanel: boolean) => void;
 };
 
-export const useScheduleScreenStore = create<ScheduleScreenState>(
+const useScheduleScreenStore = create<ScheduleScreenState>(
   (set) => ({
     isOpen: false,
-    isDayDetailPanelOpen: true,
-    canToggleDayDetailPanel: false,
     open: () => set({ isOpen: true }),
     close: () => set({ isOpen: false }),
     toggle: () => set((state) => ({ isOpen: !state.isOpen })),
-    openDayDetailPanel: () => set({ isDayDetailPanelOpen: true }),
-    closeDayDetailPanel: () => set({ isDayDetailPanelOpen: false }),
-    toggleDayDetailPanel: () =>
-      set((state) =>
-        state.canToggleDayDetailPanel
-          ? { isDayDetailPanelOpen: !state.isDayDetailPanelOpen }
-          : {},
-      ),
-    setCanToggleDayDetailPanel: (canToggleDayDetailPanel) =>
-      set({ canToggleDayDetailPanel }),
   }),
 );
+
+export { useScheduleScreenStore };
