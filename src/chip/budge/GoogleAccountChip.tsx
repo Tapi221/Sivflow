129edbc55796f7786cf83ad368ab54d93894723d
Tapi221 <aaatsub1@gmail.com>
2026-05-28 06:17:20 +0900

    Replace Google account chip avatar with Google icon

--- src/chip/budge/GoogleAccountChip.tsx
+++ src/chip/budge/GoogleAccountChip.tsx
@@ -1,3 +1,5 @@
+import { GoogleIcon } from "@/chip/icons/icons.nouse";
+
 type GoogleAccountChipProps = {
   name: string;
   photoUrl?: string | null;
@@ -5,23 +7,9 @@ type GoogleAccountChipProps = {
 
 const GOOGLE_ACCOUNT_CHIP_SIZE_CLASS_NAME = "size-[16px]";
 
-export const GoogleAccountChip = ({ name, photoUrl }: GoogleAccountChipProps) => {
-  if (photoUrl) {
-    return (
-      <img
-        src={photoUrl}
-        alt={name}
-        className={`${GOOGLE_ACCOUNT_CHIP_SIZE_CLASS_NAME} shrink-0 rounded-full object-cover`}
-        referrerPolicy="no-referrer"
-      />
-    );
-  }
-
-  return (
-    <div
-      className={`${GOOGLE_ACCOUNT_CHIP_SIZE_CLASS_NAME} flex shrink-0 items-center justify-center rounded-full bg-[#f1f3f4] text-[8px] font-semibold leading-none text-[#5f6368]`}
-    >
-      {name.charAt(0).toUpperCase()}
-    </div>
-  );
-};
+export const GoogleAccountChip = ({ name }: GoogleAccountChipProps) => (
+  <GoogleIcon
+    className={`${GOOGLE_ACCOUNT_CHIP_SIZE_CLASS_NAME} shrink-0 text-[#5f6368]`}
+    label={name}
+  />
+);
