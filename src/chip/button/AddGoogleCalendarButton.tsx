0d01909fa2ef32672363cb5879e16939c0653c87
Tapi221 <aaatsub1@gmail.com>
2026-05-27 22:32:58 +0900

    Delete AddGoogleCalendarButton

--- src/chip/button/AddGoogleCalendarButton.tsx
+++
@@ -1,9 +0,0 @@
-type AddGoogleCalendarButtonProps = {
-  hasGoogleAccounts: boolean;
-  isConnecting: boolean;
-  onAddCalendar: () => void;
-};
-
-const AddGoogleCalendarButton = (_props: AddGoogleCalendarButtonProps) => null;
-
-export { AddGoogleCalendarButton };
