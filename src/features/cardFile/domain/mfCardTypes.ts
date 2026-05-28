83203e4931cc407478a4e087255e66839697d818
Tapi221 <aaatsub1@gmail.com>
2026-05-28 11:45:32 +0900

    Remove mfcard compatibility re-export

--- src/features/cardFile/domain/mfCardTypes.ts
+++
@@ -1,2 +0,0 @@
-export { MF_CARD_FILE_EXTENSION, MF_CARD_FORMAT, MF_CARD_MIME_TYPE, MF_CARD_VERSION, MfCardExportError, MfCardValidationError } from "./mfCard.types";
-export type { MfCardFileV1, MfCardIssue, MfCardValidationResult } from "./mfCard.types";
