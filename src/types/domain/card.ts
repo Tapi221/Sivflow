ae9f25ba5b7317dc2e2f590f480559aa7f3079ae
Tapi221 <aaatsub1@gmail.com>
2026-05-28 12:04:11 +0900

    Add card tags field

--- src/types/domain/card.ts
+++ src/types/domain/card.ts
@@ -63,6 +63,7 @@ export type Card = BaseEntity & {
   questionNumber: string;
   title?: string;
   tagIds?: string[];
+  tags?: string[];
   front: CardFace;
   back: CardFace;
   layoutRows?: number;
