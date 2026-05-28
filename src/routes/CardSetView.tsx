a4be3fe7a0cf2bb7a98d1bbdfbce78aa72669ade
Tapi221 <aaatsub1@gmail.com>
2026-05-28 23:55:58 +0900

    Remove legacy card set view route

--- src/routes/CardSetView.tsx
+++
@@ -1,7 +0,0 @@
-import { CardSetViewScreen } from "@/features/cardsetview/presentation/web/ui/components/CardSetViewScreen";
-
-const CardSetViewRoute = () => {
-  return <CardSetViewScreen />;
-};
-
-export default CardSetViewRoute;
