3425421f91de148bdb5818ed2fa616ae217f2241
Tapi221 <aaatsub1@gmail.com>
2026-05-30 08:12:31 +0900

    Use local AppContent inside web-renderer package

--- packages/web-renderer/src/App.tsx
+++ packages/web-renderer/src/App.tsx
@@ -2,7 +2,7 @@ import { MantineProvider } from "@mantine/core";
 import { Suspense } from "react";
 import { BrowserRouter } from "react-router-dom";
 import "@mantine/core/styles.css";
-import { AppContent } from "@/app/AppContent";
+import { AppContent } from "./app/AppContent";
 import { NotificationProvider } from "@/components/notifications/NotificationProvider";
 import { AuthProvider } from "@/contexts/AuthContext";
 import { BreadcrumbProvider } from "@/contexts/BreadcrumbContext";
