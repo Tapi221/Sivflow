aa561274eee7c0ac93dc403141df595e901a0e3b
Tapi221 <aaatsub1@gmail.com>
2026-05-30 07:14:57 +0900

    Re-export platform runtime helpers from platform package

--- src/platform/runtime.ts
+++ src/platform/runtime.ts
@@ -1,5 +1,1 @@
-import { hasDesktopBridge, hasDesktopRuntime } from "./detectDesktopBridge";
-
-export { hasDesktopBridge, hasDesktopRuntime };
-
-export const isDesktopRuntime = (): boolean => hasDesktopRuntime();
+export { hasDesktopBridge, hasDesktopRuntime, isDesktopRuntime } from "@platform/runtime";
