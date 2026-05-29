fab255e5fc8ef532e3ebc23d71808612e42db1c9
Tapi221 <aaatsub1@gmail.com>
2026-05-30 07:14:44 +0900

    Re-export platform facade from platform package

--- src/platform/index.ts
+++ src/platform/index.ts
@@ -1,11 +1,2 @@
-import { desktopPlatform } from "@/platform/desktop";
-import { hasDesktopBridge } from "./runtime";
-import { webPlatform } from "@/platform/web";
-import type { PlatformApi } from "@/types/externals/desktop-api";
-
-export const platform: PlatformApi = hasDesktopBridge()
-  ? desktopPlatform
-  : webPlatform;
-
-export default platform;
-export type { PlatformApi } from "@/types/externals/desktop-api";
+export { default, platform } from "@platform/index";
+export type { PlatformApi } from "@platform/desktopApi";
