71f9176368d7c6d55083bb10df1fdc0deda19bb3
Tapi221 <aaatsub1@gmail.com>
2026-05-30 08:08:51 +0900

    Move AppContent into web-renderer package

---
+++ packages/web-renderer/src/app/AppContent.tsx
@@ -0,0 +1,36 @@
+import { AccountLockedScreen } from "@/components/security/AccountLockedScreen";
+import { LoadingFallback } from "@/components/common/LoadingFallback";
+import { useAuthSession } from "@/contexts/auth/AuthSessionContext";
+import { LoginPage } from "@/pane.desktop/page/LoginPage";
+import { useStartupTasks } from "@/application/startup/useStartupTasks";
+import { AppRoutes } from "./routing/AppRoutes";
+import { getDevStandaloneRouteElement } from "./routing/DevRoutes";
+import { isTestBypassEnabled } from "./routing/testBypass";
+
+export const AppContent = () => {
+  const { currentUser, loading } = useAuthSession();
+
+  useStartupTasks(currentUser?.uid);
+
+  const isTestBypass = isTestBypassEnabled();
+
+  if (loading) {
+    return <LoadingFallback />;
+  }
+
+  if (!currentUser && !isTestBypass) {
+    return <LoginPage />;
+  }
+
+  const devStandaloneRouteElement = getDevStandaloneRouteElement(isTestBypass);
+  if (devStandaloneRouteElement) {
+    return <>{devStandaloneRouteElement}</>;
+  }
+
+  return (
+    <>
+      <AccountLockedScreen />
+      <AppRoutes />
+    </>
+  );
+};
