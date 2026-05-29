7ecad268bb6ac29d3d0e8f5fc80b68113d53716a
Tapi221 <aaatsub1@gmail.com>
2026-05-29 10:37:58 +0900

    Use generic Google OAuth callback renderer

--- src/main.tsx
+++ src/main.tsx
@@ -9,11 +9,11 @@ import "@/features/scroll/schedule/weekdayHeaderScrollBridge";
 import "@/services/localDB";
 import App from "./App";
 import { ErrorBoundary } from "@/components/common/ErrorScreen";
-import { renderGoogleOAuthPopupCallback } from "@/integration/google-integration/google.oauth-popup-callback";
+import { renderGoogleOAuthCallback } from "@/integration/google-integration/google.oauth-callback";
 import { queryClient } from "@/runtime/queryClient";
 import { startAppRuntime } from "@/runtime/startAppRuntime";
 
-if (!renderGoogleOAuthPopupCallback()) {
+if (!renderGoogleOAuthCallback()) {
   startAppRuntime();
 
   createRoot(document.getElementById("root")!).render(
