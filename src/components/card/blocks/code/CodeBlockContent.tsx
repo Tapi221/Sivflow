e57ce182304667a31c1e26178f0843329a67f4be
Tapi221 <aaatsub1@gmail.com>
2026-05-30 06:13:38 +0900

    Use design token code theme import

--- src/components/card/blocks/code/CodeBlockContent.tsx
+++ src/components/card/blocks/code/CodeBlockContent.tsx
@@ -26,7 +26,7 @@ import { CodeBlockFrame } from "./CodeBlockFrame";
 import { getViewerLanguageLabels, normalizeEditorLanguage, normalizeViewerLanguage } from "./codeBlockLanguage";
 import { cn } from "@/lib/utils";
 import { webClipboardAdapter } from "@/platform/clipboard/webClipboardAdapter";
-import { codeTheme } from "@shared/styles/tokens/codeTheme";
+import { codeTheme } from "@shared/design-tokens/codeTheme";
 
 type CodeBlockContentProps =
   | {
