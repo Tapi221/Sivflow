403c461539871667419cdea39fc9e4f5dc19c3df
Tapi221 <aaatsub1@gmail.com>
2026-05-30 02:48:09 +0900

    Update natural size utility test import

--- tests/unit/imageNaturalSize.test.ts
+++ tests/unit/imageNaturalSize.test.ts
@@ -1,5 +1,5 @@
 import { afterEach, describe, expect, it, vi } from "vitest";
-import { loadImageNaturalSize } from "@/utils/uploaded-image/naturalSize";
+import { loadImageNaturalSize } from "@/utils/uploaded-image/naturalSize.utils";
 
 type MockImageOptions = {
   naturalWidth?: number;
