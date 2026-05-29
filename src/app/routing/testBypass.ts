57368924e5a1484d3b91b12843452a552749914b
Tapi221 <aaatsub1@gmail.com>
2026-05-30 08:15:54 +0900

    Delegate test bypass helper to web-renderer package

--- src/app/routing/testBypass.ts
+++ src/app/routing/testBypass.ts
@@ -1,16 +1,1 @@
-import { DEV_MODE, isLocalHost } from "@/utils/envGuards";
-
-export const isTestBypassEnabled = () => {
-  const hasBypassParam =
-    new URLSearchParams(window.location.search).get("test_bypass") === "true";
-
-  if (!hasBypassParam) {
-    return false;
-  }
-
-  if (!DEV_MODE) {
-    return false;
-  }
-
-  return isLocalHost(window.location.hostname);
-};
+export { isTestBypassEnabled } from "@web-renderer/app/routing/testBypass";
