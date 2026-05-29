99c61b156cd294f9495b98f7e8e16fcae6433988
Tapi221 <aaatsub1@gmail.com>
2026-05-30 06:25:16 +0900

    Make weekday column width prop required

--- src/features/calendar/scheduleScreen.types.ts
+++ src/features/calendar/scheduleScreen.types.ts
@@ -126,7 +126,7 @@ export type CalendarWeekDayGridProps = {
   virtualRail?: ScheduleVirtualRail;
   visibleEvents: GoogleCalendarEvent[];
   calendarDayColumnWidth: number;
-  _calendarDayColumnWidth?: number;
+  _calendarDayColumnWidth: number;
   calendarGridStyle: CalendarGridStyle;
   onScroll?: (event: UIEvent<HTMLDivElement>) => void;
   selectedDate: Date;
