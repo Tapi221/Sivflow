6fb5a8cdf84c3819e49401406f0958067ef17537
Tapi221 <aaatsub1@gmail.com>
2026-05-31 04:24:42 +0900

    Move trash usecase types to core

---
+++ packages/core/src/usecases/trash/trashTypes.ts
@@ -0,0 +1,52 @@
+export type TrashEntityBase = {
+  [key: string]: unknown;
+  id: string;
+  isDeleted?: boolean;
+};
+
+export type TrashFolderBase = TrashEntityBase & {
+  parentFolderId?: string | null;
+};
+
+export type TrashCardBase = TrashEntityBase;
+
+export type TrashItems<
+  TFolder extends TrashFolderBase = TrashFolderBase,
+  TCard extends TrashCardBase = TrashCardBase,
+> = {
+  folders: TFolder[];
+  cards: TCard[];
+};
+
+export type TrashContext<
+  TFolder extends TrashFolderBase = TrashFolderBase,
+  TCard extends TrashCardBase = TrashCardBase,
+> = {
+  folders: TFolder[];
+  cards: TCard[];
+  resolveCardFolderId: (card: TCard) => string | null | undefined;
+};
+
+export type TrashItemIds = {
+  folderIds?: string[];
+  cardIds?: string[];
+};
+
+export type TrashRepository<
+  TFolder extends TrashFolderBase = TrashFolderBase,
+  TCard extends TrashCardBase = TrashCardBase,
+> = {
+  loadContext: (userId: string) => Promise<TrashContext<TFolder, TCard>>;
+  restoreFolder: (userId: string, folderId: string) => Promise<void>;
+  restoreCard: (userId: string, cardId: string) => Promise<void>;
+  purgeFolder: (userId: string, folderId: string) => Promise<void>;
+  purgeCard: (userId: string, cardId: string) => Promise<void>;
+};
+
+export type TrashUseCaseInput<
+  TFolder extends TrashFolderBase = TrashFolderBase,
+  TCard extends TrashCardBase = TrashCardBase,
+> = {
+  userId: string;
+  repository: TrashRepository<TFolder, TCard>;
+};
