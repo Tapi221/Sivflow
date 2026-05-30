b512656674236b0c4a55a3952735bc51f8c1c885
Tapi221 <aaatsub1@gmail.com>
2026-05-30 12:33:19 +0900

    Remove Tauri build artifacts from Git

--- src-tauri/target/debug/deps/fastrand-82dbdd3804e46183.d
+++
@@ -1,7 +0,0 @@
-C:\Manifolia\src-tauri\target\debug\deps\fastrand-82dbdd3804e46183.d: C:\Users\akari\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\fastrand-2.4.1\src\lib.rs
-
-C:\Manifolia\src-tauri\target\debug\deps\libfastrand-82dbdd3804e46183.rlib: C:\Users\akari\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\fastrand-2.4.1\src\lib.rs
-
-C:\Manifolia\src-tauri\target\debug\deps\libfastrand-82dbdd3804e46183.rmeta: C:\Users\akari\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\fastrand-2.4.1\src\lib.rs
-
-C:\Users\akari\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\fastrand-2.4.1\src\lib.rs:
--- src-tauri/target/debug/deps/phf_generator-c6d8bb59aafe678d.d
+++
@@ -1,7 +0,0 @@
-C:\Manifolia\src-tauri\target\debug\deps\phf_generator-c6d8bb59aafe678d.d: C:\Users\akari\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\phf_generator-0.13.1\src\lib.rs
-
-C:\Manifolia\src-tauri\target\debug\deps\libphf_generator-c6d8bb59aafe678d.rlib: C:\Users\akari\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\phf_generator-0.13.1\src\lib.rs
-
-C:\Manifolia\src-tauri\target\debug\deps\libphf_generator-c6d8bb59aafe678d.rmeta: C:\Users\akari\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\phf_generator-0.13.1\src\lib.rs
-
-C:\Users\akari\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\phf_generator-0.13.1\src\lib.rs:
