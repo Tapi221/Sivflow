94a035c11df63554471fc63d5418195508e632ca
Tapi221 <aaatsub1@gmail.com>
2026-05-30 12:33:19 +0900

    Remove Tauri build artifacts from Git

--- src-tauri/target/debug/build/icu_normalizer_data-dae541b0fcc93194/build_script_build-dae541b0fcc93194.d
+++
@@ -1,5 +0,0 @@
-C:\Manifolia\src-tauri\target\debug\build\icu_normalizer_data-dae541b0fcc93194\build_script_build-dae541b0fcc93194.d: C:\Users\akari\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\icu_normalizer_data-2.2.0\build.rs
-
-C:\Manifolia\src-tauri\target\debug\build\icu_normalizer_data-dae541b0fcc93194\build_script_build-dae541b0fcc93194.exe: C:\Users\akari\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\icu_normalizer_data-2.2.0\build.rs
-
-C:\Users\akari\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\icu_normalizer_data-2.2.0\build.rs:
