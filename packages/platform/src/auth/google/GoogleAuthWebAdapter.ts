e7ac9a05f65301570f6663ea4d20089901421e0a
Tapi221 <aaatsub1@gmail.com>
2026-05-30 13:06:21 +0900

    Move Google web auth adapter to platform package

---
+++ packages/platform/src/auth/google/GoogleAuthWebAdapter.ts
@@ -0,0 +1,12 @@
+import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import type { GoogleAuthPort } from "@/application/ports/GoogleAuthPort";
+import { auth } from "@/infrastructure/firebase/client";
+
+const signIn: GoogleAuthPort["signIn"] = async () => {
+  const provider = new GoogleAuthProvider();
+  await signInWithPopup(auth, provider);
+};
+
+export const googleAuthWebAdapter: GoogleAuthPort = {
+  signIn,
+};
