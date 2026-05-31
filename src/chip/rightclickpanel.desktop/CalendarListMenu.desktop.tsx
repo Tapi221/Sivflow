3c71f730df7cfad8d9f1594a98ea2d135d5c0ad0
Tapi221 <aaatsub1@gmail.com>
2026-05-31 17:28:33 +0900

    Tighten calendar context menu height

--- src/chip/rightclickpanel.desktop/CalendarListMenu.desktop.tsx
+++ src/chip/rightclickpanel.desktop/CalendarListMenu.desktop.tsx
@@ -1,6 +1,6 @@
 import { type CSSProperties, type RefObject } from "react";
 import { RightClickPanelSurface } from "./rightClickPanelCommon";
-import { RIGHT_CLICK_PANEL_MARGIN, resolveRightClickPanelTextWidth, type RightClickPanelId } from "./rightClickPanel.utils";
+import { RIGHT_CLICK_PANEL_ITEM_MIN_HEIGHT, RIGHT_CLICK_PANEL_MARGIN, RIGHT_CLICK_PANEL_SURFACE_VERTICAL_EDGE, resolveRightClickPanelTextWidth, type RightClickPanelId } from "./rightClickPanel.utils";
 
 export type CalendarListMenuAction = {
   id: "add-project" | "change-color";
@@ -26,7 +26,7 @@ const CALENDAR_LIST_MENU_LABELS = [
 ];
 
 export const CALENDAR_LIST_MENU_WIDTH = resolveRightClickPanelTextWidth(CALENDAR_LIST_MENU_LABELS);
-export const CALENDAR_LIST_MENU_HEIGHT = 64;
+export const CALENDAR_LIST_MENU_HEIGHT = CALENDAR_LIST_MENU_LABELS.length * RIGHT_CLICK_PANEL_ITEM_MIN_HEIGHT + RIGHT_CLICK_PANEL_SURFACE_VERTICAL_EDGE;
 export const CALENDAR_LIST_MENU_MARGIN = RIGHT_CLICK_PANEL_MARGIN;
 
 export const CalendarListMenu = ({
