723226eb4a720e25290abe33b4f27638ac216ac6
Tapi221 <aaatsub1@gmail.com>
2026-05-31 10:13:45 +0900

    Split AuthProvider from auth context exports

---
+++ src/contexts/AuthProvider.tsx
@@ -0,0 +1,21 @@
+import type { ReactNode } from "react";
+import { AuthSessionProvider } from "./auth/AuthSessionContext";
+import { SecurityProvider } from "./security/SecurityContext";
+import { SyncProvider } from "@/sync/appdata-sync/SyncContext";
+
+type AuthProviderProps = {
+  children: ReactNode;
+};
+
+const AuthProvider = ({ children }: AuthProviderProps) => {
+  return (
+    <AuthSessionProvider>
+      <SyncProvider>
+        <SecurityProvider>{children}</SecurityProvider>
+      </SyncProvider>
+    </AuthSessionProvider>
+  );
+};
+
+export { AuthProvider };
+export type { AuthProviderProps };
