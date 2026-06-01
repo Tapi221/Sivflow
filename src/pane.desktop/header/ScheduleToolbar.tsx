dc623bd5e38fdcc0aa4ab7bc69a4accd2fddc602
Tapi221 <aaatsub1@gmail.com>
2026-06-01 11:38:42 +0900

    Delete unused schedule toolbar component

--- src/pane.desktop/header/ScheduleToolbar.tsx
+++
@@ -1,8 +0,0 @@
-import type { CalendarWorkspaceToolbarProps } from "@/features/calendar/scheduleScreen.types";
-
-const CalendarToolbar = (_props: CalendarWorkspaceToolbarProps) => {
-  void _props;
-  return null;
-};
-
-export const CalendarWorkspaceToolbar = CalendarToolbar;
