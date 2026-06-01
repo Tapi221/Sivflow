a429f776763e4f5b5a90d450dfb1e9a4ccca6cac
Tapi221 <aaatsub1@gmail.com>
2026-06-01 14:18:33 +0900

    Move Sonner toasts to bottom right

--- src/components/notifications/AppSonnerToaster.tsx
+++ src/components/notifications/AppSonnerToaster.tsx
@@ -1,5 +1,5 @@
 import { Toaster } from "sonner";
 
-const AppSonnerToaster = () => <Toaster closeButton richColors position="bottom-center" visibleToasts={3} offset={{ bottom: 24, left: 16, right: 16 }} toastOptions={{ duration: 5000 }} />;
+const AppSonnerToaster = () => <Toaster closeButton richColors position="bottom-right" visibleToasts={3} offset={{ bottom: 24, right: 24 }} toastOptions={{ duration: 5000 }} />;
 
 export { AppSonnerToaster };
