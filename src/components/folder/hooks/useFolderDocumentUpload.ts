da72024cfa20e722f8c3b07dd5f45aa1af5c7d01
Tapi221 <aaatsub1@gmail.com>
2026-06-02 14:27:56 +0900

    Use Drive OAuth helper with desktop support

--- src/components/folder/hooks/useFolderDocumentUpload.ts
+++ src/components/folder/hooks/useFolderDocumentUpload.ts
@@ -1,9 +1,9 @@
 import React, { useCallback, useRef } from "react";
 import { uploadPdfToGoogleDrive } from "@/integration/google-integration/googleDrive.pdfUpload";
+import { requestGoogleDriveFileAccessToken } from "@/integration/google-integration/googleDrive.oauth";
 import { buildStoragePath, createDocumentId, extractPdfFiles } from "@/components/folder/explorer/model/utils";
 import { useAuthSession } from "@/contexts/AuthContext";
 import { useToast } from "@/contexts/ToastContext";
-import { requestGoogleDriveFileAccessToken } from "@/integration/google-integration/google.oauth";
 import { auth } from "@/services/firebase";
 import { saveDocumentWithBlob } from "@/services/documentFileStore";
 import { getLocalDb } from "@/services/localDB";
