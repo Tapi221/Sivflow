bb213323d4eded062f83bf35e80b451e63f669fb
Tapi221 <aaatsub1@gmail.com>
2026-06-04 13:43:00 +0900

    Pass empty card set metadata

--- src/features/cardsetview/presentation/web/ui/components/CardSetViewDesktopContent.tsx
+++ src/features/cardsetview/presentation/web/ui/components/CardSetViewDesktopContent.tsx
@@ -1,18 +1,8 @@
 import { CardSetViewDesktop } from "@/features/cardsetview/presentation/web/ui/CardSetViewDesktop";
 import type { CardSetViewContentProps } from "./cardSetViewContentProps";
 
-export const CardSetViewDesktopContent = ({
-  controller,
-}: CardSetViewContentProps) => {
-  const {
-    folderId,
-    cardSetId,
-    settings,
-    data,
-    state,
-    zoom,
-    effectiveCardLayoutMode,
-  } = controller;
+export const CardSetViewDesktopContent = ({ controller }: CardSetViewContentProps) => {
+  const { folderId, cardSetId, settings, data, state, zoom, effectiveCardLayoutMode } = controller;
 
   return (
     <CardSetViewDesktop
@@ -25,19 +15,17 @@ export const CardSetViewDesktopContent = ({
       currentDisplayMode={state.currentDisplayMode}
       currentCardLayoutMode={effectiveCardLayoutMode}
       folderId={folderId}
-      layoutTransitionScrollAnchorRevision={
-        controller.layoutTransitionScrollAnchorRevision
-      }
+      layoutTransitionScrollAnchorRevision={controller.layoutTransitionScrollAnchorRevision}
       scrollToActiveIndexRequestKey={controller.scrollToActiveIndexRequestKey}
       cardSetId={cardSetId}
+      cardSetName={data.selectedCardSet?.name ?? null}
       viewZoomScale={zoom.zoomScale}
       fixedCardWidthPx={zoom.fixedCardWidthPx}
       fluidAvailableWidthPx={zoom.availableWidthPx}
       onActiveIndexChange={state.handlePagerIndexChange}
       onFlip={state.handleFlip}
-      onActiveScrollAnchorFaceChange={
-        controller.handleActiveScrollAnchorFaceChange
-      }
+      onActiveScrollAnchorFaceChange={controller.handleActiveScrollAnchorFaceChange}
+      onCreateCard={state.createAndFocusCard}
       onToggleUncertainty={state.handleToggleUncertainty}
       onToggleBookmark={state.handleToggleBookmark}
     />
