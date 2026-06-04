b8a145f9fc81f866ee828ae2c4446f2a86dbd477
Tapi221 <aaatsub1@gmail.com>
2026-06-04 21:34:48 +0900

    Fix card corner action icon size

--- constants/shared/flashcard/actionUi.ts
+++ constants/shared/flashcard/actionUi.ts
@@ -1,6 +1,6 @@
 export const CARD_ACTION_ICON_CLASS = "stroke-[1.6]";
-export const CARD_ACTION_BUTTON_PX = 32;
-export const CARD_ACTION_ICON_PX = 20;
+export const CARD_ACTION_BUTTON_PX = 28;
+export const CARD_ACTION_ICON_PX = 16;
 export const CARD_ACTION_COLOR_IDLE_CLASS =
   "text-[#b7b7b7] hover:text-[#8c8c8c]";
 export const CARD_ACTION_COLOR_ACTIVE_CLASS =
