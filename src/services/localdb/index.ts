71220301de2d5e58cf612ca6da6d483305380555
Tapi221 <aaatsub1@gmail.com>
2026-06-04 22:24:13 +0900

    Fix localdb constants export path

--- src/services/localdb/index.ts
+++ src/services/localdb/index.ts
@@ -1,21 +1,11 @@
 // Public API entry point for localdb module
 
 export { isBackingStoreOpenError } from "./errors";
-export {getLocalDb,
-  getLocalDbSync,
-  initializeDB,
-  resetLocalDBForLogout,} from "./instanceManager";
+export { getLocalDb, getLocalDbSync, initializeDB, resetLocalDBForLogout } from "./instanceManager";
 export { LocalDB } from "./LocalDB";
-export type {CardRelation,
-  LocalDBInstance,
-  LocalDBLike,
-  ProjectMap,} from "./types";
-export {clearLocalDBResetFailureReason,
-  getLocalDBRuntimeStatus,
-  getLocalDBTelemetrySnapshot,
-  subscribeLocalDBRuntimeStatus,
-  telemetryOncePerSession,} from "@/services/localDBRuntimeState";
-export { LOCALDB_RECOVERY_GUIDE_URL } from "@constants/shared/storage";
+export { LOCALDB_RECOVERY_GUIDE_URL } from "./localdb.constants";
+export type { CardRelation, LocalDBInstance, LocalDBLike, ProjectMap } from "./types";
+export { clearLocalDBResetFailureReason, getLocalDBRuntimeStatus, getLocalDBTelemetrySnapshot, subscribeLocalDBRuntimeStatus, telemetryOncePerSession } from "@/services/localDBRuntimeState";
 
 if (import.meta.env.DEV) {
   void import("./devtools").then((module) => {
