1de11e6e11cd22ae5079d299bc0426cd37fc388a
Tapi221 <aaatsub1@gmail.com>
2026-06-04 22:18:12 +0900

    Move localdb constants to localdb module

---
+++ src/services/localdb/localdb.constants.ts
@@ -0,0 +1,5 @@
+export const LOCALDB_SCHEMA_VERSION_FOR_NAME = 19;
+export const LOCALDB_GENERATION_MAX = 3;
+export const LOCALDB_GENERATION_KEY_PREFIX = "flashcard.localdb.generation.";
+export const LOCALDB_NAME_PREFIX = "FlashcardMasterDB_";
+export const LOCALDB_RECOVERY_GUIDE_URL = "https://support.google.com/chrome/answer/2392709";
