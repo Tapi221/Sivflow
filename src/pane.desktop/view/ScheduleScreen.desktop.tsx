9d5087da337eac5b225e407fb1523588fe6415e8
Tapi221 <aaatsub1@gmail.com>
2026-06-05 12:46:49 +0900

    Speed up initial month event display

--- src/pane.desktop/view/ScheduleScreen.desktop.tsx
+++ src/pane.desktop/view/ScheduleScreen.desktop.tsx
@@ -45,7 +45,7 @@ const DEFAULT_PLAN_RESULT_MODES: readonly PlanResultMode[] = ["plan", "actual"];
 const PLAN_RESULT_TOGGLE_VIEW_MODES = new Set(["threeDays", "days", "pieChart"]);
 const LIST_AND_PIE_CHART_EVENT_BUFFER_DAYS = 45;
 const WEEKDAY_EVENT_BUFFER_DAYS = 1;
-const MONTH_EVENT_BUFFER_DAYS = 14;
+const MONTH_EVENT_BUFFER_DAYS = 18 * 7;
 
 const isHexColor = (value: string): boolean => /^#[0-9a-f]{6}$/i.test(value);
 
