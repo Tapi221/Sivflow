a874ac7d1736ebcdc71c0487326d5c6477790ecc
Tapi221 <aaatsub1@gmail.com>
2026-06-05 15:08:01 +0900

    Remove unused localdb sync type imports

--- src/services/localdb/localdb.types.ts
+++ src/services/localdb/localdb.types.ts
@@ -1,5 +1,5 @@
 import type { DeleteEntity, UpsertEntity } from "@/application/usecases/syncQueuePayloadGuards";
-import type { AssetRecord, Card, CardSet, DocumentItem as Document, Folder, Note, SyncConflict, SyncError, SyncHistory, SyncQueueItem, SyncSettings, UploadedImage, UserSettings, UserStats } from "@/types";
+import type { AssetRecord, Card, CardSet, DocumentItem as Document, Folder, Note, SyncError, SyncHistory, SyncSettings, UploadedImage, UserSettings, UserStats } from "@/types";
 import type { SyncPayloadByEntity, SyncPriority } from "@/types/domain/sync";
 
 export type CardRelation = {
