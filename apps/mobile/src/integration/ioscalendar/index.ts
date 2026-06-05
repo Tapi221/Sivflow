55bd4b74f2279c8bed28f88d728f5444f36f3cf1
Tapi221 <aaatsub1@gmail.com>
2026-06-05 09:41:40 +0900

    Export writable iOS calendar integration APIs

--- apps/mobile/src/integration/ioscalendar/index.ts
+++ apps/mobile/src/integration/ioscalendar/index.ts
@@ -1,3 +1,3 @@
-export { fetchIosCalendars, fetchIosEvents, getIosCalendarPermissionStatus, isIosCalendarSupported, requestIosCalendarPermission } from "./iosCalendar.api";
+export { createIosCalendarEvent, deleteIosCalendarEvent, fetchIosCalendars, fetchIosEvents, getIosCalendarPermissionStatus, isIosCalendarSupported, requestIosCalendarPermission, updateIosCalendarEvent } from "./iosCalendar.api";
 export { useIosCalendarIntegration } from "./useIosCalendarIntegration";
 export type { IosCalendarConnectionStatus, IosCalendarEvent, IosCalendarListItem, IosCalendarPermissionStatus, IosCalendarRange, IosCalendarWritableEventDeleteInput, IosCalendarWritableEventInput, IosCalendarWritableEventUpdateInput } from "./iosCalendar.types";
