e94693e5a2320b03dfdda1b8c3d3160487b3a073
Tapi221 <aaatsub1@gmail.com>
2026-06-06 14:38:34 +0900

    Close panel event string

--- src/chip/rightclickpanel.desktop/rightClickPanel.utils.ts
+++ src/chip/rightclickpanel.desktop/rightClickPanel.utils.ts
@@ -1,8 +1,9 @@
-import type { CSSProperties } from "react";
+import { type CSSProperties } from "react";
 
 export type RightClickPanelPosition = { x: number; y: number };
 export type RightClickPanelNoDragStyle = CSSProperties & { WebkitAppRegion?: "drag" | "no-drag" };
 export type RightClickPanelDimensions = { width: number; height: number };
 export type RightClickPanelId = string;
 
-export const RIGHT_CLICK_PANEL_OPEN_EVENT = "right-click
+export const RIGHT_CLICK_PANEL_MARGIN = 8;
+export const RIGHT_CLICK_PANEL_OPEN_EVENT = "open";
