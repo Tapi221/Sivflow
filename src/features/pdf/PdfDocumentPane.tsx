874dfb2d387b3dccf75ad97a1324b929ffe3a7f9
Tapi221 <aaatsub1@gmail.com>
2026-06-06 17:43:41 +0900

    Fix PDF document source resolution

--- src/features/pdf/PdfDocumentPane.tsx
+++ src/features/pdf/PdfDocumentPane.tsx
@@ -4,4 +4,7 @@ import type { DocumentItem, PdfViewerState } from "@/types";
 import { createPdfDocumentDataSource, createPdfDocumentUrlSource } from "./pdfDocumentSource";
 import { PdfPane } from "./PdfPane";
 import { resolvePdfDocumentSourceUrl } from "./resolvePdfDocumentSourceUrl";
-import type { PdfDocumentSource } from "./
+import type { PdfDocumentSource } from "./pdfDocumentSource";
+
+type PdfDocumentPaneProps = {
+  document: DocumentItem;
