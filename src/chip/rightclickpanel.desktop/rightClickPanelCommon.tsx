0370f0e47aee01468e85c789085820e5bc4d0c42
Tapi221 <aaatsub1@gmail.com>
2026-06-06 17:51:37 +0900

    Fix right click panel common component

--- src/chip/rightclickpanel.desktop/rightClickPanelCommon.tsx
+++ src/chip/rightclickpanel.desktop/rightClickPanelCommon.tsx
@@ -1,13 +1,4 @@
 import { memo, type CSSProperties, type ReactNode, type RefObject } from "react";
 import { RIGHT_CLICK_PANEL_ITEM_MIN_HEIGHT, RIGHT_CLICK_PANEL_SURFACE_PADDING, type RightClickPanelId } from "./rightClickPanel.utils";
 
-type RightClickPanelSurfaceProps = {
-  x: number;
-  y: number;
-  width: number;
-  panelRef: RefObject<HTMLDivElement | null>;
-  noDragStyle?: CSSProperties;
-  className?: string;
-  role?: string;
-  ariaLabel?: string;
-  panel
+type RightClickPanelSurfaceProps = { x: number; y: number; width: number; panelRef: RefObject<HTMLDivElement | null>; noDragStyle?: CSSProperties; className?: string; role
