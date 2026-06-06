3bae4fc4b254886558a7c357828b0fba990fe56b
Tapi221 <aaatsub1@gmail.com>
2026-06-06 14:08:51 +0900

    Restore right click panel utilities

--- src/chip/rightclickpanel.desktop/rightClickPanel.utils.ts
+++ src/chip/rightclickpanel.desktop/rightClickPanel.utils.ts
@@ -16,4 +16,22 @@ export type RightClickPanelDimensions = {
 
 export type RightClickPanelId = string;
 
-type RightClickPanelDismissIgnored
+type RightClickPanelDismissIgnoredRef = RefObject<Element | null>;
+
+type RightClickPanelDismissOptions = {
+  closeOnScroll?: boolean;
+  ignoredRefs?: readonly RightClickPanelDismissIgnoredRef[];
+};
+
+type RightClickPanelOpenEventDetail = {
+  panelId: RightClickPanelId;
+};
+
+export const RIGHT_CLICK_PANEL_MARGIN = 8;
+export const RIGHT_CLICK_PANEL_SURFACE_PADDING = 2;
+export const RIGHT_CLICK_PANEL_SURFACE_BORDER_WIDTH = 1;
+export const RIGHT_CLICK_PANEL_SURFACE_VERTICAL_EDGE = RIGHT_CLICK_PANEL_SURFACE_PADDING * 2 + RIGHT_CLICK_PANEL_SURFACE_BORDER_WIDTH * 2;
+export const RIGHT_CLICK_PANEL_ITEM_HORIZONTAL_PADDING = 10;
+export const RIGHT_CLICK_PANEL_ITEM_MIN_HEIGHT = 26;
+export const RIGHT_CLICK_PANEL_TEXT_FONT_SIZE = 13;
+export const RIGHT_CLICK_PANEL
