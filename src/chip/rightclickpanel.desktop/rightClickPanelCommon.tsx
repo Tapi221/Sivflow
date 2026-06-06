0202628d518ac7d1c8f0d93085c509e8597dba8e
Tapi221 <aaatsub1@gmail.com>
2026-06-06 17:42:48 +0900

    Fix right click panel text weight

--- src/chip/rightclickpanel.desktop/rightClickPanelCommon.tsx
+++ src/chip/rightclickpanel.desktop/rightClickPanelCommon.tsx
@@ -10,13 +10,4 @@ type RightClickPanelSurfaceProps = {
   className?: string;
   role?: string;
   ariaLabel?: string;
-  panelId?: RightClickPanelId;
-  children?: ReactNode;
-};
-
-const RIGHT_CLICK_PANEL_COMMON_STYLE = `
-.right-click-panel {
-  position: fixed;
-  z-index: 2147483647;
-  display: flex;
-  flex-direction:
+  panel
