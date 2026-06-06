ca99ea2e549a3fa0b596979c27907dd6b8a54053
Tapi221 <aaatsub1@gmail.com>
2026-06-06 17:52:33 +0900

    Restore right click panel common component

--- src/chip/rightclickpanel.desktop/rightClickPanelCommon.tsx
+++ src/chip/rightclickpanel.desktop/rightClickPanelCommon.tsx
@@ -1,4 +1,11 @@
 import { memo, type CSSProperties, type ReactNode, type RefObject } from "react";
 import { RIGHT_CLICK_PANEL_ITEM_MIN_HEIGHT, RIGHT_CLICK_PANEL_SURFACE_PADDING, type RightClickPanelId } from "./rightClickPanel.utils";
 
-type RightClickPanelSurfaceProps = { x: number; y: number; width: number; panelRef: RefObject<HTMLDivElement | null>; noDragStyle?: CSSProperties; className?: string; role
+type RightClickPanelSurfaceProps = {
+  x: number;
+  y: number;
+  width: number;
+  panelRef: RefObject<HTMLDivElement | null>;
+  noDragStyle?: CSSProperties;
+  className?: string;
+  role?:
