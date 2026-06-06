0eef72e8e47e54ad57073785d316e7be62821fe3
Tapi221 <aaatsub1@gmail.com>
2026-06-06 17:44:27 +0900

    Fix PDF document loading source

--- src/features/pdf/PdfDocumentPane.tsx
+++ src/features/pdf/PdfDocumentPane.tsx
@@ -1,10 +1,9 @@
-import { useCallback, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { getDocumentBlob } from "@/services/documentFileStore";
 import type { DocumentItem, PdfViewerState } from "@/types";
-import { createPdfDocumentDataSource, createPdfDocumentUrlSource } from "./pdfDocumentSource";
+import { createPdfDocumentUrlSource } from "./pdfDocumentSource";
 import { PdfPane } from "./PdfPane";
 import { resolvePdfDocumentSourceUrl } from "./resolvePdfDocumentSourceUrl";
 import type { PdfDocumentSource } from "./pdfDocumentSource";
 
 type PdfDocumentPaneProps = {
-  document: DocumentItem;
