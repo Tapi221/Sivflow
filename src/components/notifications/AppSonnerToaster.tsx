d05ce4202cace29f9724419510b342a01a62b56a
Tapi221 <aaatsub1@gmail.com>
2026-06-06 17:43:19 +0900

    Repair toaster syntax

--- src/components/notifications/AppSonnerToaster.tsx
+++ src/components/notifications/AppSonnerToaster.tsx
@@ -1,10 +1,5 @@
 import { Toaster } from "sonner";
 
-const SONNER_TOAST_CLASS_NAMES = {
-  toast: "!rounded-[7px] !border-0 !bg-[var(--ds-semantic-color-background-sidebar)] !text-[#85827e] !shadow-sm",
-  title: "text-[13px] font-bold text-[#111111]",
-  description: "text-[12px] font-medium text-[#85827e]",
-  actionButton: "!rounded-[7px] !bg-transparent !text-[#111111]",
-} as const;
+const AppSonnerToaster = () => <Toaster position="bottom-right" toastOptions={{ classNames: { toast: "!rounded-[7px] !border-0 !bg-[var(--ds-semantic-color-background-sidebar)] !text-[#85827e] !shadow-sm", title: "text-[#111111]", description: "text-[#85827e]", actionButton: "!bg-transparent !text-[#111111]" } }} />;
 
-const AppSonnerToaster = () => <Toaster position="bottom-right" toastOptions={{ classNames: SONNER_TOAST_CLASS
+export { AppSonnerToaster };
