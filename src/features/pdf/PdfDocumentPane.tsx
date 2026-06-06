71eac93e9fee5870977fb678f93c8e93c7a02822
Tapi221 <aaatsub1@gmail.com>
2026-06-06 17:45:33 +0900

    Fix PDF document loading source

--- src/features/pdf/PdfDocumentPane.tsx
+++ src/features/pdf/PdfDocumentPane.tsx
@@ -4,6 +4,4 @@ import type { DocumentItem, PdfViewerState } from "@/types";
 import { createPdfDocumentUrlSource } from "./pdfDocumentSource";
 import { PdfPane } from "./PdfPane";
 import { resolvePdfDocumentSourceUrl } from "./resolvePdfDocumentSourceUrl";
-import type { PdfDocumentSource } from "./pdfDocumentSource";
-
-type PdfDocumentPaneProps = {
+import type { PdfDocumentSource } from "
