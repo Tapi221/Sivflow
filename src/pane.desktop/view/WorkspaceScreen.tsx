020dd319b6d7081247859dfd77853e4b07fe0f7e
Tapi221 <aaatsub1@gmail.com>
2026-06-06 17:50:06 +0900

    Restore workspace screen mobile sidebar implementation

--- src/pane.desktop/view/WorkspaceScreen.tsx
+++ src/pane.desktop/view/WorkspaceScreen.tsx
@@ -4,4 +4,4 @@ import { SidebarOpenIcon } from "@/chip/icons/icons.sidebar";
 import TreeViewLayout from "@/components/folder/layout/TreeViewLayout";
 import { CarvePanel } from "@/components/panel/CarvePanel.desktop";
 import { useSetBreadcrumbCrumbs } from "@/contexts/BreadcrumbContext";
-import { areExplorerBreadcrumbContextsEqual,
+import { areExplorerBreadcrumbContextsEqual, EMPTY_EXPLOR
