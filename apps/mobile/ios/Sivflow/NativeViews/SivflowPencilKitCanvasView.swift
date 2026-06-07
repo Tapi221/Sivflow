631e11757c5db042019d4841f03455a05ebcdff8
Tapi221 <aaatsub1@gmail.com>
2026-06-07 16:31:58 +0900

    Repair PencilKit canvas view

--- apps/mobile/ios/Sivflow/NativeViews/SivflowPencilKitCanvasView.swift
+++ apps/mobile/ios/Sivflow/NativeViews/SivflowPencilKitCanvasView.swift
@@ -1,12 +1,22 @@
 import PencilKit
-import React
 import UIKit
 
 @objc(SivflowPencilKitCanvasView)
-final class SivflowPencilKitCanvasView: UIView, PKCanvasViewDelegate {
-  @objc var onStrokeComplete: RCTDirectEventBlock?
+final class SivflowPencilKitCanvasView: UIView {
   private let canvas = PKCanvasView()
-  private var strokeCount = 0
-  override init(frame: CGRect) { super.init(frame: frame); setUpCanvas() }
-  required init?(coder: NSCoder) { super.init(coder: coder); setUpCanvas() }
+
+  override init(frame: CGRect) {
+    super.init(frame: frame)
+    addSubview(canvas)
+  }
+
+  required init?(coder: NSCoder) {
+    super.init(coder: coder)
+    addSubview(canvas)
+  }
+
   override func layoutSubviews() {
+    super.layoutSubviews()
+    canvas.frame = bounds
+  }
+}
