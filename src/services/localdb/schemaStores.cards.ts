08755cb27dd7c9879f56093b18fb48a50566ac65
Tapi221 <aaatsub1@gmail.com>
2026-06-07 11:46:33 +0900

    Add card local DB store definitions

---
+++ src/services/localdb/schemaStores.cards.ts
@@ -0,0 +1,4 @@
+export const CARD_STORES = {
+  cardSets: "id, userId, folderId, updatedAt, isDeleted, [userId+updatedAt], [userId+folderId]",
+  cards: "id, userId, folderId, cardSetId, updatedAt, nextReviewDate, isDeleted, difficulty, reviewCount, [userId+updatedAt], [userId+isDeleted], [userId+nextReviewDate], [cardSetId+isDeleted], *tagIds",
+};
