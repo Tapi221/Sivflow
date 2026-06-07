ae40c2731bd790be50cf6b00df9dbe23f2814690
Tapi221 <aaatsub1@gmail.com>
2026-06-07 18:32:01 +0900

    Restore workspace screen and offset collapsed breadcrumbs

--- src/pane.desktop/view/WorkspaceScreen.tsx
+++ src/pane.desktop/view/WorkspaceScreen.tsx
@@ -3,4 +3,4 @@ import { useOutletContext } from "react-router-dom";
 import { SidebarOpenIcon } from "@/chip/icons/icons.sidebar";
 import TreeViewLayout from "@/components/folder/layout/TreeViewLayout";
 import { CarvePanel } from "@/components/panel/CarvePanel.desktop";
-import { areExplorerBreadcrumbContextsEqual, EMPTY_EX
+import { areExplorerBreadcrumbContextsEqual, EMPTY_EXPLORER_BREADCRUMB_CONTEXT, type BreadcrumbCrumb, type
