732be874a31f739c6540fb4d21fb4ce9a7cf55b8
Tapi221 <aaatsub1@gmail.com>
2026-06-07 21:23:58 +0900

    Restore compiling month event chip

--- src/chip/eventchip/EventChip.month.tsx
+++ src/chip/eventchip/EventChip.month.tsx
@@ -1,6 +1,6 @@
 import { createElement, memo } from "react";
 import type { GoogleCalendarEvent } from "@/integration/googlecalendar-integration/gcalSync.types";
 
-type CalendarEventChipMonthProps = { event: GoogleCalendarEvent; showTimeLabel?: boolean; tooltipDisabled?: boolean };
+type Props = { event: GoogleCalendarEvent; showTimeLabel?: boolean; tooltipDisabled?: boolean };
 
-const CalendarEventChipMonth = memo(({ event }: CalendarEventChipMonthProps) => createElement("div", { "data-calendar-event-chip": "month", className: "calendar-event-chip-month flex w-full min-w-0 items-center" }, event.title || "Unt
+const CalendarEventChipMonth = memo(({ event }: Props) => createElement("div", { className: "calendar-event-chip-month flex w-full min-w-0 items-center", "data-calendar-event-chip": "month" }, event.title));
