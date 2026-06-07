70fef4879d714c6a309de6b9c9085afd633ee945
Tapi221 <aaatsub1@gmail.com>
2026-06-07 15:50:16 +0900

    Emit PencilKit strokes to React Native

--- apps/mobile/ios/Sivflow/NativeViews/SivflowPencilKitCanvasView.swift
+++ apps/mobile/ios/Sivflow/NativeViews/SivflowPencilKitCanvasView.swift
@@ -1,10 +1,11 @@
 import PencilKit
+import React
 import UIKit
 
 @objc(SivflowPencilKitCanvasView)
-final class SivflowPencilKitCanvasView: UIView {
+final class SivflowPencilKitCanvasView: UIView, PKCanvasViewDelegate {
   private let canvas = PKCanvasView()
+  @objc var onStrokeComplete: RCTBubblingEventBlock?
   override init(frame: CGRect) { super.init(frame: frame); setup() }
   required init?(coder: NSCoder) { super.init(coder: coder); setup() }
-  private func setup() { canvas.frame = bounds; canvas.autoresizingMask = [.flexibleWidth, .flexibleHeight]; addSubview(canvas) }
-}
+  private func setup() { canvas.frame
