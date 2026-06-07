362d480dd6ea8bdd5aadc646fe8a9cc1ec4a4b90
Tapi221 <aaatsub1@gmail.com>
2026-06-07 13:36:34 +0900

    Use block body for Google auth selector

--- packages/platform/src/auth/google/selectGoogleAuthPort.ts
+++ packages/platform/src/auth/google/selectGoogleAuthPort.ts
@@ -1,6 +1,16 @@
 import type { GoogleAuthPort } from "@/application/ports/GoogleAuthPort";
 import { RUNTIME_KINDS, type RuntimeKind } from "@constants/shared/app";
 
-type SelectGoogleAuthPortInput = { webAuth: GoogleAuthPort; desktopAuth: GoogleAuthPort; runtimeKind: RuntimeKind; userAgent: string };
+type SelectGoogleAuthPortInput = {
+  webAuth: GoogleAuthPort;
+  desktopAuth: GoogleAuthPort;
+  runtimeKind: RuntimeKind;
+  userAgent: string;
+};
 
-const selectGoogleAuthPort = (input: SelectGoogleAuthPortInput): GoogleAuthPort => input.runtimeKind === RUNTIME_KINDS.desktop || input.userAgent.includes("Tauri") ?
+const selectGoogleAuthPort = (input: SelectGoogleAuthPortInput): GoogleAuthPort => {
+  if (input.runtimeKind === RUNTIME_KINDS.desktop) {
+    return input.desktopAuth;
+  }
+
+  if (input
