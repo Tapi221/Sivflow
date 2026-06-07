f89d3f6ca362f06370944dec1958a3077d4ba8fb
Tapi221 <aaatsub1@gmail.com>
2026-06-07 15:11:24 +0900

    Route iOS handwriting host events from PencilKit

--- packages/mobile-renderer/src/components/ipad/ink/IpadInkCanvasHost.tsx
+++ packages/mobile-renderer/src/components/ipad/ink/IpadInkCanvasHost.tsx
@@ -1,8 +1,10 @@
 import React from "react";
-import { Platform, requireNativeComponent, Text, View } from "react-native";
+import { Platform, requireNativeComponent, StyleSheet, Text, View } from "react-native";
 
 const NativeInkCanvas = Platform.OS === "ios" ? requireNativeComponent("SivflowPencilKitCanvas") : null;
 
-const IpadInkCanvasHost = (props: any) => NativeInkCanvas ? React.createElement(NativeInkCanvas, props) : React.createElement(View, null, React.createElement(Text, null, "iOS handwriting only"));
+const resolveStroke = (event: any) => event?.nativeEvent ?? event;
 
-export default React.memo(IpadInkCanvasHost);
+const IpadInkCanvasHost = (props: any) => {
+  const handleStrokeComplete = (event: any) => props.onStrokeComplete?.(resolveStroke(event));
+  return NativeInkCanvas ? React.createElement(NativeInkCanvas, { cardId: props.cardId, onStrokeComplete: handleStrokeComplete, style: styles.canvas, tool: props.tool }) : React.createElement(View, { style
