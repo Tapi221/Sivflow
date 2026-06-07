573dbe37ce590248ffa0c6d3ba56782b87922599
Tapi221 <aaatsub1@gmail.com>
2026-06-07 16:17:54 +0900

    Emit PencilKit stroke events

--- apps/mobile/ios/Sivflow/NativeViews/SivflowPencilKitCanvasView.swift
+++ apps/mobile/ios/Sivflow/NativeViews/SivflowPencilKitCanvasView.swift
@@ -1,10 +1,29 @@
 import PencilKit
+import React
 import UIKit
 
 @objc(SivflowPencilKitCanvasView)
-final class SivflowPencilKitCanvasView: UIView {
+final class SivflowPencilKitCanvasView: UIView, PKCanvasViewDelegate {
+  @objc var onStrokeComplete: RCTDirectEventBlock?
   private let canvas = PKCanvasView()
-  override init(frame: CGRect) { super.init(frame: frame); addSubview(canvas) }
-  required init?(coder: NSCoder) { super.init(coder: coder); addSubview(canvas) }
-  override func layoutSubviews() { super.layoutSubviews(); canvas.frame = bounds }
-}
+  private var strokeCount = 0
+
+  override init(frame: CGRect) {
+    super.init(frame: frame)
+    setUpCanvas()
+  }
+
+  required init?(coder: NSCoder) {
+    super.init(coder: coder)
+    setUpCanvas()
+  }
+
+  override func layoutSubviews() {
+    super.layoutSubviews()
+    canvas.frame = bounds
+  }
+
+  private func setUpCanvas() {
+    canvas.backgroundColor = .clear
+    canvas.drawingPolicy = .anyInput
+    canvas
