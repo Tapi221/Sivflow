769265bac7e946ef8330cf42711633dab47f1a70
Tapi221 <aaatsub1@gmail.com>
2026-06-07 22:55:07 +0900

    Repair TreeViewMainPane implementation

--- src/components/folder/components/TreeViewMainPane.tsx
+++ src/components/folder/components/TreeViewMainPane.tsx
@@ -1,8 +1,8 @@
 import { RightPane } from "@/components/folder/panes/RightPane";
 import { cn } from "@/lib/utils";
-import type { Card, DocumentItem, Folder, SelectedExplorerItem } from "@/types";
+import type { DocumentItem, SelectedExplorerItem } from "@/types";
 
-interface TreeViewMainPaneProps {
+type TreeViewMainPaneProps = {
   showMobileDetail: boolean;
   hideOnSectionList?: boolean;
   selectedItem: SelectedExplorerItem;
@@ -10,11 +10,5 @@ interface TreeViewMainPaneProps {
   selectedDocument: DocumentItem | null;
   selectedFolderId: string | null;
   selectedFolderName: string;
-  folders: Folder[];
-  cards: Card[];
-  documents: DocumentItem[];
-  folderCards: Card[];
-  onCardUpdated: () => void;
-  onDocumentUpdated: (documentId: string, updates: Partial<DocumentItem>) => Promise<void> | void;
-  onRenameFolder?: (newName: string) => Promise<void>;
-  handlers: { onStartStudy: () => void; onViewCards: () => void; onCreate
+  folderCards: unknown[];
+  onCardUpdated:
