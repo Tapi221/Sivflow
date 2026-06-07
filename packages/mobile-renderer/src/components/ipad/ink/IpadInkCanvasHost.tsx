ca3986bb20ac3805ed868edd8e4eafac4527729f
Tapi221 <aaatsub1@gmail.com>
2026-06-07 15:15:00 +0900

    Repair PencilKit host export

--- packages/mobile-renderer/src/components/ipad/ink/IpadInkCanvasHost.tsx
+++ packages/mobile-renderer/src/components/ipad/ink/IpadInkCanvasHost.tsx
@@ -3,4 +3,6 @@ import { Platform, requireNativeComponent } from "react-native";
 
 const NativeInkCanvas = Platform.OS === "ios" ? requireNativeComponent("SivflowPencilKitCanvas") : null;
 
-const IpadInkCanvasHost = (props: any) => NativeInkCanvas ? React.createElement(NativeInkCanvas, { onStrokeComplete: props.onStrokeComplete, style: { flex: 1 }, tool: props.tool }) : null;
+const IpadInkCanvasHost = (props: any) => NativeInkCanvas ? React.createElement(NativeInkCanvas, props) : null;
+
+export default React.memo(IpadInkCanvasHost);
