15a2a4be0c1b35e02ebac0e3525f2cb0f5720a5c
Tapi221 <aaatsub1@gmail.com>
2026-06-07 13:35:51 +0900

    Restore Google auth port selector

--- packages/platform/src/auth/google/selectGoogleAuthPort.ts
+++ packages/platform/src/auth/google/selectGoogleAuthPort.ts
@@ -1,15 +1,7 @@
 import type { GoogleAuthPort } from "@/application/ports/GoogleAuthPort";
 import { RUNTIME_KINDS, type RuntimeKind } from "@constants/shared/app";
 
-type SelectGoogleAuthPortInput = {
-  webAuth: GoogleAuthPort;
-  desktopAuth: GoogleAuthPort;
-  runtimeKind: RuntimeKind;
-  userAgent: string;
-};
+type SelectGoogleAuthPortInput = { webAuth: GoogleAuthPort; desktopAuth: GoogleAuthPort; runtimeKind: RuntimeKind; userAgent: string };
 
-const TAURI_USER_AGENT_MARKER = "Tauri";
-
-const hasTauriUserAgent = (userAgent: string): boolean => userAgent.includes(TAURI_USER_AGENT_MARKER);
-
-const selectGoogle
+const selectGoogleAuthPort = ({ webAuth, desktopAuth, runtimeKind, userAgent }: SelectGoogleAuthPortInput): GoogleAuthPort => {
+  if (runtimeKind
