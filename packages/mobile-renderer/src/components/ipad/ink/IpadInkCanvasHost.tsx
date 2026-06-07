3d6d664a4725308cfaf4629e2767413851016a77
Tapi221 <aaatsub1@gmail.com>
2026-06-07 15:47:38 +0900

    Repair PencilKit host syntax

--- packages/mobile-renderer/src/components/ipad/ink/IpadInkCanvasHost.tsx
+++ packages/mobile-renderer/src/components/ipad/ink/IpadInkCanvasHost.tsx
@@ -1,10 +1,8 @@
-import React, { useCallback } from "react";
+import React from "react";
 import { Platform, requireNativeComponent } from "react-native";
 
 const NativeInkCanvas = Platform.OS === "ios" ? requireNativeComponent("SivflowPencilKitCanvas") : null;
 
-const IpadInkCanvasHost = (props: any) => {
-  const handleStrokeComplete = useCallback((event: any) => {
-    props.onStrokeComplete?.(event.nativeEvent);
-  }, [props.onStrokeComplete]);
-  return NativeInkCanvas ? React.createElement
+const IpadInkCanvasHost = (props: any) => NativeInkCanvas ? React.createElement(NativeInkCanvas, props) : null;
+
+export default React.memo(IpadInkCanvasHost);
