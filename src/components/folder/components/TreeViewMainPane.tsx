90fa8e4de0796ed0e055888e28e43a4db655dafb
Tapi221 <aaatsub1@gmail.com>
2026-06-07 22:18:01 +0900

    Repair TreeViewMainPane and keep section list detail visible

--- src/components/folder/components/TreeViewMainPane.tsx
+++ src/components/folder/components/TreeViewMainPane.tsx
@@ -2,9 +2,9 @@ import { RightPane } from "@/components/folder/panes/RightPane";
 import { cn } from "@/lib/utils";
 import type { Card, DocumentItem, Folder, SelectedExplorerItem } from "@/types";
 
-type TreeViewMainPaneProps = {
+interface TreeViewMainPaneProps {
   showMobileDetail: boolean;
   hideOnSectionList?: boolean;
   selectedItem: SelectedExplorerItem;
   selectedCardId: string | null;
-  selectedDocument: DocumentItem | null;
+  selectedDocument: DocumentItem | null
