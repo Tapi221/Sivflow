2d1cc702618b0e29b9ffdd7d72f40b1cc582c441
Tapi221 <aaatsub1@gmail.com>
2026-06-07 21:19:46 +0900

    Restore month event chip component

--- src/chip/eventchip/EventChip.month.tsx
+++ src/chip/eventchip/EventChip.month.tsx
@@ -5,4 +5,4 @@ import { eventChipAllDayClass } from "./eventchip.allday.styles";
 import { HoverMonthEventTooltip } from "@/chip/toolchip/HoverMonthEventTooltip";
 import { generateColorTokens } from "@/features/calendar/schedule.color-tokens";
 import type { GoogleCalendarEvent } from "@/integration/googlecalendar-integration/gcalSync.types";
-import { cn
+import { cn } from "@/lib/utils"
