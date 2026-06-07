417d799f87c1655474b20e1d6c49769fa45a8d8b
Tapi221 <aaatsub1@gmail.com>
2026-06-07 22:34:50 +0900

    Repair TreeViewMainPane

--- src/components/folder/components/TreeViewMainPane.tsx
+++ src/components/folder/components/TreeViewMainPane.tsx
@@ -2,27 +2,4 @@ import { RightPane } from "@/components/folder/panes/RightPane";
 import { cn } from "@/lib/utils";
 import type { Card, DocumentItem, Folder, SelectedExplorerItem } from "@/types";
 
-interface TreeViewMainPaneProps {
-  showMobileDetail: boolean;
-  hideOnSectionList?: boolean;
-  selectedItem: SelectedExplorerItem;
-  selectedCardId: string | null;
-  selectedDocument: DocumentItem | null;
-  selectedFolderId: string | null;
-  selectedFolderName: string;
-  folders: Folder[];
-  cards: Card[];
-  documents: DocumentItem[];
-  folderCards: Card[];
-  onCardUpdated: () => void;
-  onDocumentUpdated: (documentId: string, updates: Partial<DocumentItem>) => Promise<void> | void;
-  onRenameFolder?: (newName: string) => Promise<void>;
-  handlers: {
-    onStartStudy: () => void;
-    onViewCards: () => void;
-    onCreateCard: () => void;
-  };
-  folderSelectionNonce: number;
-}
-
-const TreeViewMainPane = ({ showMobileDetail, hideOnSectionList = false, selectedItem, selectedCardId, selectedDocument, selectedFolderId, selectedFolder
+type TreeViewMainPaneProps = { showMobileDetail: boolean; hideOnSectionList?: boolean; selectedItem: SelectedExplorerItem; selectedCardId: string | null; selectedDocument: DocumentItem | null; selectedFolderId: string | null; selectedFolderName:
