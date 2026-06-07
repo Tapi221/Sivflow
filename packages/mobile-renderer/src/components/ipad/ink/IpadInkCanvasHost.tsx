de7e623c07a3eb1455d727ac5d5bb2ac9171ff1c
Tapi221 <aaatsub1@gmail.com>
2026-06-07 15:54:02 +0900

    Normalize PencilKit native stroke event

--- packages/mobile-renderer/src/components/ipad/ink/IpadInkCanvasHost.tsx
+++ packages/mobile-renderer/src/components/ipad/ink/IpadInkCanvasHost.tsx
@@ -3,6 +3,6 @@ import { Platform, requireNativeComponent } from "react-native";
 
 const NativeInkCanvas = Platform.OS === "ios" ? requireNativeComponent("SivflowPencilKitCanvas") : null;
 
-const IpadInkCanvasHost = (props: any) => NativeInkCanvas ? React.createElement(NativeInkCanvas, props) : null;
+const IpadInkCanvasHost = (props: any) => NativeInkCanvas ? React.createElement(NativeInkCanvas, { ...props, onStrokeComplete: (event: any) => props.onStrokeComplete?.(event.nativeEvent ?? event) }) : null;
 
 export default React.memo(IpadInkCanvasHost);
