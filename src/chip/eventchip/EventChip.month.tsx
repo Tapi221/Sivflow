4272673824fce8d2e36cf46b77970ae6ca35fa70
Tapi221 <aaatsub1@gmail.com>
2026-06-07 21:25:17 +0900

    f

--- src/chip/eventchip/EventChip.month.tsx
+++ src/chip/eventchip/EventChip.month.tsx
@@ -1,6 +1,10 @@
-import { createElement, memo } from "react";
+import type { CSSProperties } from "react";
+import { memo, useMemo } from "react";
+import { format } from "date-fns";
+import { eventChipAllDayClass } from "./eventchip.allday.styles";
+import { HoverMonthEventTooltip } from "@/chip/toolchip/HoverMonthEventTooltip";
+import { generateColorTokens } from "@/features/calendar/schedule.color-tokens";
 import type { GoogleCalendarEvent } from "@/integration/googlecalendar-integration/gcalSync.types";
+import { cn } from "@/lib/utils";
 
-type Props = { event: GoogleCalendarEvent; showTimeLabel?: boolean; tooltipDisabled?: boolean };
-
-const CalendarEventChipMonth = memo(({ event }: Props) => createElement("div", { className: "calendar-event-chip-month flex w-full min-w-0 items-center", "data-calendar-event-chip": "month" }, event.title));
+type CalendarEventChipMonthProps
