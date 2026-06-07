4c2baf0715b79bea7e6dc611f0860cc9f24d6f67
Tapi221 <aaatsub1@gmail.com>
2026-06-07 15:00:41 +0900

    Repair handwriting host syntax

--- packages/mobile-renderer/src/components/ipad/ink/IpadInkCanvasHost.tsx
+++ packages/mobile-renderer/src/components/ipad/ink/IpadInkCanvasHost.tsx
@@ -1,9 +1,8 @@
 import React from "react";
 import { Text, View } from "react-native";
 
-type IpadInkCanvasHostProps = { cardId?: string | null; tool?: unknown; strokes?: unknown[]; onErasePoint?: unknown; onStrokeComplete?: unknown };
+type IpadInkCanvasHostProps = { cardId?: string | null };
 
 const IpadInkCanvasHost = ({ cardId }: IpadInkCanvasHostProps) => <View><Text>{cardId ?? "iOS handwriting"}</Text></View>;
 
 export default React.memo(IpadInkCanvasHost);
-export type { IpadInkCanvasHost
