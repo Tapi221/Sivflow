24263f19d0fd0c7637714d7ace0884101d20e4b5
Tapi221 <aaatsub1@gmail.com>
2026-06-07 18:56:13 +0900

    Replace truncated workspace screen with valid component

--- src/pane.desktop/view/WorkspaceScreen.tsx
+++ src/pane.desktop/view/WorkspaceScreen.tsx
@@ -1,16 +1,3 @@
-import TreeViewLayout from "@/components/folder/layout/TreeViewLayout";
-import { CarvePanel } from "@/components/panel/CarvePanel.desktop";
-import { useFoldersRead } from "@/hooks/folder/useFoldersRead";
+const WorkspaceScreen = () => <div />;
 
-const WorkspaceScreen = () => {
-  const { folders } = useFoldersRead();
-
-  return (
-    <CarvePanel>
-      <TreeViewLayout
-        folders={folders}
-        isSectionListMode={true}
-        selectedFolderId={null}
-        selectedItem={null}
-        selectedCardId={null}
-        selectedDocumentId={null
+export default WorkspaceScreen;
