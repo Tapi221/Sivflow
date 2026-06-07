4363f81d62216591f0e86db6ad51f470d49f4bb0
Tapi221 <aaatsub1@gmail.com>
2026-06-07 16:51:10 +0900

    r

--- src/pane.desktop/leftpane/Sidebar.desktop.tsx
+++
@@ -1,7 +0,0 @@
-import { type MouseEvent, type ReactNode } from "react";
-import { useNavigate } from "react-router-dom";
-import { CalendarIcon, GalleryIcon, HomeIcon, SettingIcon, SidebarOpenIcon } from "@/chip/icons/icons.sidebar";
-import { HoverTooltip } from "@/chip/toolchip/HoverTooltip";
-import { ExplorerChromeFolderIcon } from "@/components/explorer/icons";
-import { useAuthSession } from "@/contexts/auth/useAuthSession";
-import { useSearchStore } from "@/features/search
