06a3b6654c274f6245e7874975dbac63f6390db4
Tapi221 <aaatsub1@gmail.com>
2026-06-07 21:23:57 +0900

    f

--- src/pane.desktop/view/WorkspaceScreen.tsx
+++ src/pane.desktop/view/WorkspaceScreen.tsx
@@ -1,4 +1,7 @@
-const WorkspaceScreen = () => <div />;
-
-export { WorkspaceScreen };
-export default WorkspaceScreen;
+import { useCallback, useEffect, useLayoutEffect, useMemo, useState, type CSSProperties, type ReactNode } from "react";
+import { useOutletContext } from "react-router-dom";
+import { SidebarOpenIcon } from "@/chip/icons/icons.sidebar";
+import TreeViewLayout from "@/components/folder/layout/TreeViewLayout";
+import { CarvePanel } from "@/components/panel/CarvePanel.desktop";
+import { useSetBreadcrumbCrumbs } from "@/contexts/BreadcrumbContext";
+import { areExplorerBreadcrumbContextsEqual
