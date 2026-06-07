9779d6df36603741e2395b21e80c4366a45a9031
Tapi221 <aaatsub1@gmail.com>
2026-06-07 21:28:54 +0900

    Remove any from month event chip

--- src/chip/eventchip/EventChip.month.tsx
+++ src/chip/eventchip/EventChip.month.tsx
@@ -1,5 +1,8 @@
 import { createElement as h, memo } from "react";
+import type { GoogleCalendarEvent } from "@/integration/googlecalendar-integration/gcalSync.types";
 
-const CalendarEventChipMonth = memo(({ event }: any) => h("div", { className: "calendar-event-chip-month flex w-full min-w-0 items-center", "data-calendar-event-chip": "month" }, event.title));
+type Props = { event: GoogleCalendarEvent };
+
+const CalendarEventChipMonth = memo(({ event }: Props) => h("div", { className: "calendar-event-chip-month flex w-full min-w-0 items-center", "data-calendar-event-chip": "month" }, event.title));
 
 export { CalendarEventChipMonth };
