48b33e928af488c4ee0e7375cec8f6da5ec5eb27
Tapi221 <aaatsub1@gmail.com>
2026-06-07 15:12:29 +0900

    Repair iOS handwriting host syntax

--- packages/mobile-renderer/src/components/ipad/ink/IpadInkCanvasHost.tsx
+++ packages/mobile-renderer/src/components/ipad/ink/IpadInkCanvasHost.tsx
@@ -1,10 +1,8 @@
 import React from "react";
-import { Platform, requireNativeComponent, StyleSheet, Text, View } from "react-native";
+import { Platform, requireNativeComponent } from "react-native";
 
 const NativeInkCanvas = Platform.OS === "ios" ? requireNativeComponent("SivflowPencilKitCanvas") : null;
 
-const resolveStroke = (event: any) => event?.nativeEvent ?? event;
+const IpadInkCanvasHost = (props: any) => NativeInkCanvas ? React.createElement(NativeInkCanvas, props) : null;
 
-const IpadInkCanvasHost = (props: any) => {
-  const handleStrokeComplete = (event: any) => props.onStrokeComplete?.(resolveStroke(event));
-  return NativeInkCanvas ? React.createElement(NativeInkCanvas, { cardId: props.cardId, onStrokeComplete: handleStrokeComplete, style: styles.canvas, tool: props.tool }) : React.createElement(View, { style
+export default React.memo(IpadInkCanvasHost);
