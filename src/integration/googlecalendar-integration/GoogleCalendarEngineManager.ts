27199b4f5ece5df571d516abf0d086d24daddc0b
Tapi221 <aaatsub1@gmail.com>
2026-06-07 13:55:47 +0900

    Restore Google Calendar engine manager implementation

--- src/integration/googlecalendar-integration/GoogleCalendarEngineManager.ts
+++ src/integration/googlecalendar-integration/GoogleCalendarEngineManager.ts
@@ -1,6 +1,11 @@
-import { GoogleCalendarWatchManager } from "@/sync/googlecalendar-sync/GoogleCalendarWatchManager";
 import { GoogleCalendarSyncEngine } from "@/sync/googlecalendar-sync/GoogleCalendarSyncEngine";
-import { auth } from "@/services/firebase";
 import type { GCalForceSyncOptions, GCalWritableEventDeleteInput, GCalWritableEventInput, GCalWritableEventUpdateInput, GoogleCalendarEvent, GoogleCalendarListItem } from "./gcalSync.types";
 
-type EngineContext = { access
+type EngineContext = {
+  accessToken: string;
+  selectedCalendarIds: Set<string>;
+  calendars: GoogleCalendarListItem[];
+};
+
+type EngineState = {
+  token: string;
