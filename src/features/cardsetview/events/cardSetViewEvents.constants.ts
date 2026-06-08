f9c373e6fa99768e0430687c0b8d9bb340a8d49a
Tapi221 <aaatsub1@gmail.com>
2026-06-09 00:26:57 +0900

    refactor(cardsetview): add card set view event constants module

---
+++ src/features/cardsetview/events/cardSetViewEvents.constants.ts
@@ -0,0 +1,10 @@
+const CARD_SET_VIEW_EVENTS = {
+  editingChange: "cardsetview:editing-change",
+  metaOpenChange: "cardsetview:meta-open-change",
+  editingDraftPatch: "cardsetview:editing-draft-patch",
+  createCardRequest: "cardsetview:create-card-request",
+  toggleEditingRequest: "cardsetview:toggle-editing-request",
+  toggleMetaPanelRequest: "cardsetview:toggle-meta-panel-request",
+} as const;
+
+export { CARD_SET_VIEW_EVENTS };
