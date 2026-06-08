336162a12110c7f3fa94cfe54160397fd27694e7
Tapi221 <aaatsub1@gmail.com>
2026-06-08 23:41:27 +0900

    Read calendar week start setting

---
+++ src/features/calendar/useCalendarWeekStartSetting.ts
@@ -0,0 +1,9 @@
+import { DEFAULT_CALENDAR_MONTH_WEEK_START_DAY } from "@/features/calendar/model/calendarMonth.model";
+import { useUserSettings } from "@/features/settings/hooks/useUserSettings";
+import type { CalendarWeekStartDay } from "./calendar.types";
+
+export const useCalendarWeekStartSetting = (override?: CalendarWeekStartDay): CalendarWeekStartDay => {
+  const { settings } = useUserSettings();
+
+  return override ?? settings?.weekStartDay ?? DEFAULT_CALENDAR_MONTH_WEEK_START_DAY;
+};
