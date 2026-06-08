5d8849fb8bde0ce3ba07929bd8903d761b3e0a19
Tapi221 <aaatsub1@gmail.com>
2026-06-08 23:06:13 +0900

    refactor(storage): use platform storage keys in layout fallback preferences

--- src/services/cardLayoutFallbackPreferences.ts
+++ src/services/cardLayoutFallbackPreferences.ts
@@ -1,4 +1,4 @@
-import { SHARED_STORAGE_KEYS } from "@constants/shared/storage";
+import { SHARED_STORAGE_KEYS } from "@platform/storage/storageKeys.constants";
 import { DEFAULT_SPLIT_FALLBACK_CARD_LAYOUT_MODE, normalizeSplitFallbackCardLayoutMode, type SplitFallbackCardLayoutMode } from "@/features/cardsetview/domain/cardLayoutMode";
 
 interface SplitFallbackPreferencesStore {
