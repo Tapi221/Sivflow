25d7bf38659db7b63cf0a1523fe9faaec0cf06f5
Tapi221 <aaatsub1@gmail.com>
2026-06-09 03:09:20 +0900

    Cleanup

--- constants/web/app/context.ts
+++
@@ -1,9 +0,0 @@
-export const CONTEXT_STORAGE_KEY_PREFIXES = {
-  lastInit: "last_init_",
-  errorFlag: "error_flag_",
-  lastSync: "last_sync_",
-} as const;
-
-export const CONTEXT_SYNC_THRESHOLDS = {
-  offlineRecoveryMs: 60 * 60 * 1000,
-} as const;
