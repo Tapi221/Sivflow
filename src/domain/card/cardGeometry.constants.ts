41d2953775c42a828e4f10b4655b156916c23324
Tapi221 <aaatsub1@gmail.com>
2026-06-09 00:14:39 +0900

    refactor(card): expose complete card geometry constants

--- src/domain/card/cardGeometry.constants.ts
+++ src/domain/card/cardGeometry.constants.ts
@@ -1,4 +1,5 @@
 const CARD_BASE_WIDTH = 480;
+const CARD_DISPLAY_SCALE = 1.25;
 const CANONICAL_CARD_WIDTH = CARD_BASE_WIDTH;
 const CARD_SAFE_PADDING_PX = 24;
 const CARD_ROW_PX = 24;
@@ -10,4 +11,10 @@ const CARD_HEIGHT_PHASE_PX = (CARD_RULED_OFFSET_TOP_PX + CARD_RULED_OFFSET_BOTTO
 
 const layoutRowsToCardHeightPx = (rows: number) => rows * CARD_ROW_PX + CARD_HEIGHT_PHASE_PX;
 
-export { CARD_CONTENT_TOP_PX, CARD_ROW_PX, CANONICAL_CARD_WIDTH, layoutRowsToCardHeightPx };
+const cardHeightPxToLayoutRows = (heightPx: number) => Math.round((heightPx - CARD_HEIGHT_PHASE_PX) / CARD_ROW_PX);
+
+const minCardHeightPxToLayoutRows = (heightPx: number) => Math.ceil((heightPx - CARD_HEIGHT_PHASE_PX) / CARD_ROW_PX);
+
+const snapMinCardHeightPx = (heightPx: number) => layoutRowsToCardHeightPx(minCardHeightPxToLayoutRows(heightPx));
+
+export { CARD_BASE_WIDTH, CARD_CONTENT_TOP_PX, CARD_DISPLAY_SCALE, CARD_HEIGHT_PHASE_PX, CARD_ROW_PX, CARD_RULED_OFFSET_BOTTOM_PX, CARD_RULED_OFFSET_TOP_PX, CARD_SAFE_PADDING_PX, CANONICAL_CARD_WIDTH, cardHeightPxToLayoutRows, layoutRowsToCardHeightPx, minCardHeightPxToLayoutRows, snapMinCardHeightPx };
