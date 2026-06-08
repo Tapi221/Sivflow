96bc5ca845b377e5d5f0e4adda8a52bcb88757da
Tapi221 <aaatsub1@gmail.com>
2026-06-09 03:01:48 +0900

    Remove web preload constants file

--- constants/web/app/preload.ts
+++
@@ -1,8 +0,0 @@
-export const CARD_IMAGE_PRELOAD_DEBUG_STORAGE_KEY = "sivflow_preload_debug";
-
-export const CARD_IMAGE_PRELOAD = {
-  eagerRadiusFallback: 8,
-  eagerBuffer: 2,
-  idleExtra: 12,
-  maxEagerConcurrent: 5,
-} as const;
