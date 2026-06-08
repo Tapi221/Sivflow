24c0218fd3decf74b505904c9be1e2776aa853c0
Tapi221 <aaatsub1@gmail.com>
2026-06-09 03:15:14 +0900

    Remove obsolete constants file

--- constants/shared/flashcard/editorEvents.ts
+++
@@ -1,8 +0,0 @@
-export const CARD_SET_VIEW_EVENTS = {
-  editingChange: "cardsetview:editing-change",
-  metaOpenChange: "cardsetview:meta-open-change",
-  editingDraftPatch: "cardsetview:editing-draft-patch",
-  createCardRequest: "cardsetview:create-card-request",
-  toggleEditingRequest: "cardsetview:toggle-editing-request",
-  toggleMetaPanelRequest: "cardsetview:toggle-meta-panel-request",
-} as const;
