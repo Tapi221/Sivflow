32501d236bef88c942e79d25d734dd41592e0e40
Tapi221 <aaatsub1@gmail.com>
2026-06-09 22:42:14 +0900

    Remount Plate editor per note

--- src/components/note/NoteDocumentEditor.tsx
+++ src/components/note/NoteDocumentEditor.tsx
@@ -1,1 +1,11 @@
-export { PlateDocumentEditor as NoteDocumentEditor } from "./PlateDocumentEditor";
+import { PlateDocumentEditor } from "./PlateDocumentEditor";
+import type { Note } from "@/types";
+
+type NoteDocumentEditorProps = {
+  note: Note;
+  onChange: (changes: Pick<Note, "content" | "contentText" | "contentVersion" | "editor">) => void | Promise<void>;
+};
+
+const NoteDocumentEditor = ({ note, onChange }: NoteDocumentEditorProps) => <PlateDocumentEditor key={note.id} note={note} onChange={onChange} />;
+
+export { NoteDocumentEditor };
