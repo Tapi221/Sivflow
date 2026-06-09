897e1e7d894d6b5270df9ad3d5a0d5b9633dbe95
Tapi221 <aaatsub1@gmail.com>
2026-06-09 12:44:25 +0900

    Move localdb constants exports to footer

--- src/services/localdb/localdb.constants.ts
+++ src/services/localdb/localdb.constants.ts
@@ -1,5 +1,7 @@
-export const LOCALDB_SCHEMA_VERSION_FOR_NAME = 19;
-export const LOCALDB_GENERATION_MAX = 3;
-export const LOCALDB_GENERATION_KEY_PREFIX = "flashcard.localdb.generation.";
-export const LOCALDB_NAME_PREFIX = "FlashcardMasterDB_";
-export const LOCALDB_RECOVERY_GUIDE_URL = "https://support.google.com/chrome/answer/2392709";
+const LOCALDB_SCHEMA_VERSION_FOR_NAME = 19;
+const LOCALDB_GENERATION_MAX = 3;
+const LOCALDB_GENERATION_KEY_PREFIX = "flashcard.localdb.generation.";
+const LOCALDB_NAME_PREFIX = "FlashcardMasterDB_";
+const LOCALDB_RECOVERY_GUIDE_URL = "https://support.google.com/chrome/answer/2392709";
+
+export { LOCALDB_GENERATION_KEY_PREFIX, LOCALDB_GENERATION_MAX, LOCALDB_NAME_PREFIX, LOCALDB_RECOVERY_GUIDE_URL, LOCALDB_SCHEMA_VERSION_FOR_NAME };
