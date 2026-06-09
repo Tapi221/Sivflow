7bbc1e6540aaa904048ff883b2faed485e22f6b1
Tapi221 <aaatsub1@gmail.com>
2026-06-09 12:44:08 +0900

    Move tag constants exports to footer

--- src/chip/tag/tag.constants.ts
+++ src/chip/tag/tag.constants.ts
@@ -1,4 +1,4 @@
-export const TAG_COLOR_KEYS = [
+const TAG_COLOR_KEYS = [
   "gray",
   "purple",
   "teal",
@@ -10,6 +10,7 @@ export const TAG_COLOR_KEYS = [
   "coral",
   "sky",
 ] as const;
+const DEFAULT_TAG_COLOR_KEY = "gray";
+const DEFAULT_TAG_COLOR_CLASS_NAME = `tag-color-${DEFAULT_TAG_COLOR_KEY}`;
 
-export const DEFAULT_TAG_COLOR_KEY = "gray";
-export const DEFAULT_TAG_COLOR_CLASS_NAME = `tag-color-${DEFAULT_TAG_COLOR_KEY}`;
+export { DEFAULT_TAG_COLOR_CLASS_NAME, DEFAULT_TAG_COLOR_KEY, TAG_COLOR_KEYS };
