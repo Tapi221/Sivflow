759390a30749b77c73808def121b580f25bc2780
Tapi221 <aaatsub1@gmail.com>
2026-06-11 01:03:57 +0900

    Inline mobile hook outside registry

--- src/hooks/use-mobile.ts
+++ src/hooks/use-mobile.ts
@@ -1,1 +1,21 @@
-export { useIsMobile, useIsMobile as useMobile } from "@/registry/hooks/use-mobile";
+import { useCallback, useSyncExternalStore } from "react";
+
+const MOBILE_BREAKPOINT = 768;
+const MOBILE_MEDIA_QUERY = `(max-width: ${MOBILE_BREAKPOINT - 1}px)`;
+
+const getServerSnapshot = () => false;
+
+const useIsMobile = () => {
+  const subscribe = useCallback((onStoreChange: () => void) => {
+    const mql = window.matchMedia(MOBILE_MEDIA_QUERY);
+    mql.addEventListener("change", onStoreChange);
+
+    return () => mql.removeEventListener("change", onStoreChange);
+  }, []);
+
+  const getSnapshot = useCallback(() => window.matchMedia(MOBILE_MEDIA_QUERY).matches, []);
+
+  return useSyncExternalStore(subscribe, getSnapshot, getServerSnapshot);
+};
+
+export { useIsMobile, useIsMobile as useMobile };
