d095c40f89d0ba848a8586144d133323703d3465
Tapi221 <aaatsub1@gmail.com>
2026-06-10 14:22:12 +0900

    Use official Plate note editor

--- src/components/note/NoteDocumentEditor.tsx
+++ src/components/note/NoteDocumentEditor.tsx
@@ -1,4 +1,4 @@
-import { PlateDocumentEditor } from "./PlateDocumentEditor";
+import { PlateDocumentEditor } from "./PlateDocumentEditorOfficial";
 import type { Note } from "@/types";
 
 type NoteDocumentEditorProps = {
