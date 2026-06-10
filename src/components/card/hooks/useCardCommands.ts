c7109d7d3ec4159d65617ee9f9746f0f56380c22
Tapi221 <aaatsub1@gmail.com>
2026-06-10 22:15:36 +0900

    Update auth import

--- src/components/card/hooks/useCardCommands.ts
+++ src/components/card/hooks/useCardCommands.ts
@@ -1,7 +1,7 @@
 import { DEFAULT_LAYOUT_ROWS, normalizeLayoutRows } from "@/domain/card/extraRows";
 import { normalizeCardFolderId, resolveBlocksFromCardData, resolveExtraRowsFromCardData, resolveInkFromCardData } from "@/domain/card/normalizers/cardShape";
 import { normalizeCard } from "@/domain/card/normalizers/normalizeCard";
-import { useAuthSession } from "@/contexts/AuthContext";
+import { useAuthSession } from "@/contexts/auth/useAuthSession";
 import { DEFAULT_SETTINGS, useUserSettings } from "@/features/settings/hooks/useUserSettings";
 import { getLocalDb } from "@/services/localDB";
 import type { Card, CardPatch } from "@/types";
