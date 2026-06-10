e41c7e51d6840b0542335fc8c674c141867c1dfa
Tapi221 <aaatsub1@gmail.com>
2026-06-10 20:53:34 +0900

    Replace search store type import with source module

--- src/features/search/store/useSearchStore.ts
+++ src/features/search/store/useSearchStore.ts
@@ -1,5 +1,5 @@
 import { create } from "zustand";
-import type { SearchSource } from "@/features/search/model/searchTypes";
+import type { SearchSource } from "@/features/search/model/search.types";
 
 type SearchState = {
   isOpen: boolean;
