20640babf6e8feb77553f0ecec820144eb579c1f
Tapi221 <aaatsub1@gmail.com>
2026-06-10 13:19:55 +0900

    Remove global schedule wheel bridge import

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -7,7 +7,6 @@ import { createRoot } from "react-dom/client";
 import { QueryClientProvider } from "@tanstack/react-query";
 import "katex/dist/katex.min.css";
 import "@/styles/index.css";
-import "@/features/scroll/schedule/weekdayHeaderScrollBridge";
 import "@/services/localDB";
 import { ErrorBoundary } from "@/components/common/ErrorScreen";
 import { LoadingSpinner } from "@/components/common/LoadingSpinner";
