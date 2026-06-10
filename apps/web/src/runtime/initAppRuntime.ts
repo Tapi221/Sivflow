acff150d09e7d6d34dc1d9e2715320830b793080
Tapi221 <aaatsub1@gmail.com>
2026-06-10 22:28:59 +0900

    Move web runtime bootstrap into apps/web

---
+++ apps/web/src/runtime/initAppRuntime.ts
@@ -0,0 +1,14 @@
+import { initGlobalChunkRecovery } from "./initGlobalChunkRecovery";
+import { initServiceWorkerLifecycle } from "./initServiceWorkerLifecycle";
+import { startTabPresence } from "@/utils/tabPresence";
+
+let started = false;
+
+export const initAppRuntime = () => {
+  if (started || typeof window === "undefined") return;
+  started = true;
+
+  startTabPresence();
+  initGlobalChunkRecovery();
+  initServiceWorkerLifecycle();
+};
