6866146f975a465ea32a518fd7cc9ceb693c9c4a
Tapi221 <aaatsub1@gmail.com>
2026-06-10 15:22:16 +0900

    Move cardShellClassNames export to file end

--- src/components/card/frame/cardShellClassNames.ts
+++ src/components/card/frame/cardShellClassNames.ts
@@ -1,2 +1,4 @@
-export const CARD_SHELL_COMMON_CLASS_NAME =
+const CARD_SHELL_COMMON_CLASS_NAME =
   "premium-paper-depth card-shell--interactive card-shell--paper";
+
+export { CARD_SHELL_COMMON_CLASS_NAME };
