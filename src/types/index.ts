93c81144d6c0cc93ce2f594319718bb59c223574
Tapi221 <aaatsub1@gmail.com>
2026-06-10 21:03:42 +0900

    Replace localdb types export source

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,4 +1,4 @@
-export type { TagRecord } from "@/services/localdb/types";
+export type { TagRecord } from "@/services/localdb/localdb.types";
 export * from "@/types/domain/assets";
 export * from "@/types/domain/base";
 export * from "@/types/domain/card";
