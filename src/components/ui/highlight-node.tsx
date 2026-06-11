02272b25c93a67d9ab474fe006d88a3589bfcd76
Tapi221 <aaatsub1@gmail.com>
2026-06-11 20:48:00 +0900

    refactor: convert highlight leaf to const

--- src/components/ui/highlight-node.tsx
+++ src/components/ui/highlight-node.tsx
@@ -3,8 +3,12 @@
 import * as React from 'react';
 
 import type { PlateLeafProps } from 'platejs/react';
-
 import { PlateLeaf } from 'platejs/react';
 
-export function HighlightLeaf(props: PlateLeafProps) { return ( <PlateLeaf {...props} as="mark" className="bg-highlight/30 text-inherit"> {props.children} </PlateLeaf> );
-}
+export const HighlightLeaf = (props: PlateLeafProps) => {
+  return (
+    <PlateLeaf {...props} as="mark" className="bg-highlight/30 text-inherit">
+      {props.children}
+    </PlateLeaf>
+  );
+};
