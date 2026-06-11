f029b3167873d02ed76cf7156ac78960150ef864
Tapi221 <aaatsub1@gmail.com>
2026-06-11 20:03:46 +0900

    Fix Plate table node import

--- src/components/editor/plugins/table-kit.tsx
+++ src/components/editor/plugins/table-kit.tsx
@@ -1,22 +1,14 @@
 'use client';
 
-import {
-  TableCellHeaderPlugin,
-  TableCellPlugin,
-  TablePlugin,
-  TableRowPlugin,
-} from '@platejs/table/react';
+import { TableCellHeaderPlugin, TableCellPlugin, TablePlugin, TableRowPlugin } from '@platejs/table/react';
 
-import {
-  TableCellElement,
-  TableCellHeaderElement,
-  TableElement,
-  TableRowElement,
-} from '@/components/table-node';
+import { TableCellElement, TableCellHeaderElement, TableElement, TableRowElement } from '@/components/ui/table-node';
 
-export const TableKit = [
+const TableKit = [
   TablePlugin.withComponent(TableElement),
   TableRowPlugin.withComponent(TableRowElement),
   TableCellPlugin.withComponent(TableCellElement),
   TableCellHeaderPlugin.withComponent(TableCellHeaderElement),
 ];
+
+export { TableKit };
