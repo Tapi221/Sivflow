62cb5919b73839c89cc3c308ba16b92e0cb41d3e
Tapi221 <aaatsub1@gmail.com>
2026-06-12 08:17:29 +0900

    r

--- src/components/ui/equation-node-static.tsx
+++ src/components/ui/equation-node-static.tsx
@@ -7,6 +7,8 @@ import { SlateElement } from "platejs/static";
 import { inlineSuggestionVariants } from "@/lib/suggestion";
 import { cn } from "@/lib/utils";
 
+
+
 const EquationElementStatic = (props: SlateElementProps<TEquationElement>) => {
   const { element } = props;
 
@@ -140,5 +142,7 @@ const InlineEquationElementDocx = (props: SlateElementProps<TEquationElement>) =
     </SlateElement>
   );
 };
+
+
 
 export { EquationElementStatic, InlineEquationElementStatic, EquationElementDocx, InlineEquationElementDocx };
--- src/config/appChrome.ts
+++ src/config/appChrome.ts
@@ -1,3 +1,5 @@
 const APP_CHROME = { brandLabel: "Sivflow", desktopTitleBar: { showBrandLabel: false } } as const;
 
+
+
 export { APP_CHROME };
