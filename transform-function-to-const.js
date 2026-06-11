8bd2e89276ae3b6dbb7fc38827c54a023eeab9e3
Tapi221 <aaatsub1@gmail.com>
2026-06-11 18:46:35 +0900

    refactor: convert function transform script to const arrow

--- transform-function-to-const.js
+++ transform-function-to-const.js
@@ -1,4 +1,4 @@
-export default function transformer(file, api) {
+const transformer = (file, api) => {
   const j = api.jscodeshift;
 
   return j(file.source)
@@ -16,4 +16,6 @@ export default function transformer(file, api) {
       ]);
     })
     .toSource();
-}
+};
+
+export default transformer;
