be9e7f80e1673e1b08b5541bde9232a7a61cc607
Tapi221 <aaatsub1@gmail.com>
2026-06-11 20:12:12 +0900

    Fix Plate column node import

--- src/components/editor/plugins/column-kit.tsx
+++ src/components/editor/plugins/column-kit.tsx
@@ -2,9 +2,11 @@
 
 import { ColumnItemPlugin, ColumnPlugin } from '@platejs/layout/react';
 
-import { ColumnElement, ColumnGroupElement } from '@/components/column-node';
+import { ColumnElement, ColumnGroupElement } from '@/components/ui/column-node';
 
-export const ColumnKit = [
+const ColumnKit = [
   ColumnPlugin.withComponent(ColumnGroupElement),
   ColumnItemPlugin.withComponent(ColumnElement),
 ];
+
+export { ColumnKit };
