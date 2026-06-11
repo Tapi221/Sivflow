ebc37a62d852fb2b85fef651129dd3d6c076ecb9
Tapi221 <aaatsub1@gmail.com>
2026-06-11 21:21:12 +0900

    refactor: convert caption components to const

--- src/components/ui/caption.tsx
+++ src/components/ui/caption.tsx
@@ -4,15 +4,12 @@ import * as React from 'react';
 
 import type { VariantProps } from 'class-variance-authority';
 
-import { Caption as CaptionPrimitive, CaptionTextarea as CaptionTextareaPrimitive, useCaptionButton, useCaptionButtonState, } from '@platejs/caption/react';
-
+import { Caption as CaptionPrimitive, CaptionTextarea as CaptionTextareaPrimitive, useCaptionButton, useCaptionButtonState } from '@platejs/caption/react';
 import { createPrimitiveComponent } from '@udecode/cn';
-
 import { cva } from 'class-variance-authority';
 
-import { Button } from './button';
-
 import { cn } from '@/lib/utils';
+import { Button } from './button';
 
 const captionVariants = cva('max-w-full', {
   defaultVariants: {
@@ -27,10 +24,22 @@ const captionVariants = cva('max-w-full', {
   },
 });
 
-export function Caption({ align, className, ...props }: React.ComponentProps<typeof CaptionPrimitive> & VariantProps<typeof captionVariants>) { return ( <CaptionPrimitive {...props} className={cn(captionVariants({ align }), className)} /> );
-}
+export const Caption = ({ align, className, ...props }: React.ComponentProps<typeof CaptionPrimitive> & VariantProps<typeof captionVariants>) => {
+  return <CaptionPrimitive {...props} className={cn(captionVariants({ align }), className)} />;
+};
 
-export function CaptionTextarea( props: React.ComponentProps<typeof CaptionTextareaPrimitive> ) { return ( <CaptionTextareaPrimitive {...props} className={cn( 'mt-2 w-full resize-none border-none bg-inherit p-0 font-[inherit] text-inherit', 'focus:outline-none focus:[&::placeholder]:opacity-0', 'text-center print:placeholder:text-transparent', props.className )} /> );
-}
+export const CaptionTextarea = (props: React.ComponentProps<typeof CaptionTextareaPrimitive>) => {
+  return (
+    <CaptionTextareaPrimitive
+      {...props}
+      className={cn(
+        'mt-2 w-full resize-none border-none bg-inherit p-0 font-[inherit] text-inherit',
+        'focus:outline-none focus:[&::placeholder]:opacity-0',
+        'text-center print:placeholder:text-transparent',
+        props.className
+      )}
+    />
+  );
+};
 
-export const CaptionButton = createPrimitiveComponent(Button)({ propsHook: useCaptionButton, stateHook: useCaptionButtonState, });
+export const CaptionButton = createPrimitiveComponent(Button)({ propsHook: useCaptionButton, stateHook: useCaptionButtonState });
