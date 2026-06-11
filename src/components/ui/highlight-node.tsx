9cf76d55287f979562fee531bf9b5c667d3a37d3
Tapi221 <aaatsub1@gmail.com>
2026-06-11 20:49:38 +0900

    r

--- src/components/ui/highlight-node.tsx
+++ src/components/ui/highlight-node.tsx
@@ -3,12 +3,8 @@
 import * as React from 'react';
 
 import type { PlateLeafProps } from 'platejs/react';
+
 import { PlateLeaf } from 'platejs/react';
 
-export const HighlightLeaf = (props: PlateLeafProps) => {
-  return (
-    <PlateLeaf {...props} as="mark" className="bg-highlight/30 text-inherit">
-      {props.children}
-    </PlateLeaf>
-  );
+export const HighlightLeaf = (props: PlateLeafProps) => { return ( <PlateLeaf {...props} as="mark" className="bg-highlight/30 text-inherit"> {props.children} </PlateLeaf> );
 };
--- src/components/ui/paragraph-node-static.tsx
+++ src/components/ui/paragraph-node-static.tsx
@@ -5,10 +5,5 @@ import { SlateElement } from 'platejs/static';
 
 import { cn } from '@/lib/utils';
 
-export const ParagraphElementStatic = (props: SlateElementProps) => {
-  return (
-    <SlateElement {...props} className={cn('m-0 px-0 py-1')}>
-      {props.children}
-    </SlateElement>
-  );
+export const ParagraphElementStatic = (props: SlateElementProps) => { return ( <SlateElement {...props} className={cn('m-0 px-0 py-1')}> {props.children} </SlateElement> );
 };
