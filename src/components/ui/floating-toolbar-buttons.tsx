2a2a1d43d8f2fd5db57b7544e394ed3d666bc417
Tapi221 <aaatsub1@gmail.com>
2026-06-11 21:33:06 +0900

    refactor: convert floating toolbar buttons to const

--- src/components/ui/floating-toolbar-buttons.tsx
+++ src/components/ui/floating-toolbar-buttons.tsx
@@ -2,31 +2,22 @@
 
 import * as React from 'react';
 
-import { BoldIcon, Code2Icon, ItalicIcon, StrikethroughIcon, UnderlineIcon, WandSparklesIcon, } from 'lucide-react';
-
+import { BoldIcon, Code2Icon, ItalicIcon, StrikethroughIcon, UnderlineIcon, WandSparklesIcon } from 'lucide-react';
 import { KEYS } from 'platejs';
-
 import { useEditorReadOnly } from 'platejs/react';
 
 import { AIToolbarButton } from './ai-toolbar-button';
-
 import { CommentToolbarButton } from './comment-toolbar-button';
-
 import { InlineEquationToolbarButton } from './equation-toolbar-button';
-
 import { LinkToolbarButton } from './link-toolbar-button';
-
 import { MarkToolbarButton } from './mark-toolbar-button';
-
 import { MoreToolbarButton } from './more-toolbar-button';
-
 import { SuggestionToolbarButton } from './suggestion-toolbar-button';
-
 import { ToolbarGroup } from './toolbar';
-
 import { TurnIntoToolbarButton } from './turn-into-toolbar-button';
 
-export function FloatingToolbarButtons() { const readOnly = useEditorReadOnly();
+export const FloatingToolbarButtons = () => {
+  const readOnly = useEditorReadOnly();
 
   return (
     <>
@@ -50,17 +41,11 @@ export function FloatingToolbarButtons() { const readOnly = useEditorReadOnly();
               <ItalicIcon />
             </MarkToolbarButton>
 
-            <MarkToolbarButton
-              nodeType={KEYS.underline}
-              tooltip="Underline (⌘+U)"
-            >
+            <MarkToolbarButton nodeType={KEYS.underline} tooltip="Underline (⌘+U)">
               <UnderlineIcon />
             </MarkToolbarButton>
 
-            <MarkToolbarButton
-              nodeType={KEYS.strikethrough}
-              tooltip="Strikethrough (⌘+⇧+M)"
-            >
+            <MarkToolbarButton nodeType={KEYS.strikethrough} tooltip="Strikethrough (⌘+⇧+M)">
               <StrikethroughIcon />
             </MarkToolbarButton>
 
@@ -83,4 +68,4 @@ export function FloatingToolbarButtons() { const readOnly = useEditorReadOnly();
       </ToolbarGroup>
     </>
   );
-}
+};
