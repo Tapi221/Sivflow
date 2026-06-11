4433c52198b289ca17f036a844a045feec422930
Tapi221 <aaatsub1@gmail.com>
2026-06-11 19:08:44 +0900

    Keep persistent offline queue exports on one line

--- src/application/usecases/persistentOfflineQueueModels.ts
+++ src/application/usecases/persistentOfflineQueueModels.ts
@@ -1,11 +1,5 @@
 export type { AssetLikeRecord } from "./persistentOfflineQueueAssetMappers";
-export {makeAssetRecord,
-  toAssetLikeRecord,} from "./persistentOfflineQueueAssetMappers";
-export {getDocumentKindLabel,
-  isDocumentQueueItem,
-  isDocumentUploadReady,
-  isPdfQueueItem,
-  toDocumentLike,} from "./persistentOfflineQueueDocumentGuards";
+export { makeAssetRecord, toAssetLikeRecord } from "./persistentOfflineQueueAssetMappers";
+export { getDocumentKindLabel, isDocumentQueueItem, isDocumentUploadReady, isPdfQueueItem, toDocumentLike } from "./persistentOfflineQueueDocumentGuards";
 export { createAssetQueueImage } from "./persistentOfflineQueueFactories";
-export type {AssetUploadRequest,
-  QueueItem,} from "./persistentOfflineQueueTypes";
+export type { AssetUploadRequest, QueueItem } from "./persistentOfflineQueueTypes";
