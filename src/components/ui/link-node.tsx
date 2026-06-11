65060b7e91a7769221813fa39267a7440fc66344
Tapi221 <aaatsub1@gmail.com>
2026-06-11 21:20:09 +0900

    refactor: convert link element to const

--- src/components/ui/link-node.tsx
+++ src/components/ui/link-node.tsx
@@ -3,22 +3,29 @@
 import * as React from 'react';
 
 import type { TLinkElement } from 'platejs';
-
 import type { PlateElementProps } from 'platejs/react';
 
 import { getLinkAttributes } from '@platejs/link';
-
 import { PlateElement } from 'platejs/react';
 
+import { inlineSuggestionVariants } from '@/components/suggestion';
 import { cn } from '@/lib/utils';
 
-import { inlineSuggestionVariants } from '@/components/suggestion';
-
-export function LinkElement(props: PlateElementProps<TLinkElement>) { return ( <PlateElement {...props} as="a" className={cn( 'font-medium text-primary underline decoration-primary underline-offset-4', inlineSuggestionVariants() )} attributes={{ ...props.attributes, ...getLinkAttributes(props.editor, props.element), onMouseOver: (e) => { e.stopPropagation();
+export const LinkElement = (props: PlateElementProps<TLinkElement>) => {
+  return (
+    <PlateElement
+      {...props}
+      as="a"
+      className={cn('font-medium text-primary underline decoration-primary underline-offset-4', inlineSuggestionVariants())}
+      attributes={{
+        ...props.attributes,
+        ...getLinkAttributes(props.editor, props.element),
+        onMouseOver: (event) => {
+          event.stopPropagation();
         },
       }}
     >
       {props.children}
     </PlateElement>
   );
-}
+};
