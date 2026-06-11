da403ba27d11969d3aa46d135621acfa8e360e26
Tapi221 <aaatsub1@gmail.com>
2026-06-11 20:19:32 +0900

    Fix Plate mention node import

--- src/components/editor/plugins/mention-kit.tsx
+++ src/components/editor/plugins/mention-kit.tsx
@@ -2,6 +2,15 @@
 
 import { MentionInputPlugin, MentionPlugin } from '@platejs/mention/react';
 
-import { MentionElement, MentionInputElement, } from '@/components/mention-node';
+import { MentionElement, MentionInputElement } from '@/components/ui/mention-node';
 
-export const MentionKit = [ MentionPlugin.configure({ options: { triggerPreviousCharPattern: /^$|^[\s"']$/, }, }).withComponent(MentionElement), MentionInputPlugin.withComponent(MentionInputElement), ];
+const MentionKit = [
+  MentionPlugin.configure({
+    options: {
+      triggerPreviousCharPattern: /^$|^[\s"']$/,
+    },
+  }).withComponent(MentionElement),
+  MentionInputPlugin.withComponent(MentionInputElement),
+];
+
+export { MentionKit };
