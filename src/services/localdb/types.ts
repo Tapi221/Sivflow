6ddd3e947e7dd0b6e57ba8ed1295589563b4700e
Tapi221 <aaatsub1@gmail.com>
2026-06-12 21:51:37 +0900

    Align localdb public type methods

--- src/services/localdb/types.ts
+++ src/services/localdb/types.ts
@@ -1,11 +1,14 @@
 import type { CardRelation, LocalDBInstance, LocalDBLike as BaseLocalDBLike, LocalDBSyncApi, LocalDBTableMap, ProjectMap, QueryableCollection, QueryableKeyPath, QueryableTable, QueryableWhereClause, QueryableWhereFunction, SyncableEntityTable, TagRecord } from "./localdb.types";
+import type { SyncConflict } from "@/types/domain/sync";
 
 type LocalDBLike = BaseLocalDBLike & {
   table<T extends object = Record<string, unknown>, TKey = string>(name: string): QueryableTable<T, TKey>;
   transaction<T>(mode: string, first: unknown, ...rest: unknown[]): Promise<T>;
   close(): void;
-  getConflict(id: string): Promise<unknown>;
-  ["removeConflict"](id: string): Promise<void>;
+  cleanupSyncHistory(): Promise<void>;
+  cleanupSyncErrors(): Promise<void>;
+  getConflict(id: string): Promise<SyncConflict | undefined>;
+  removeConflict(id: string): Promise<void>;
   metadata: QueryableTable<Record<string, unknown>, string>;
   levelHistories: QueryableTable<Record<string, unknown>, string>;
   deviceMeta: QueryableTable<Record<string, unknown>, string>;
