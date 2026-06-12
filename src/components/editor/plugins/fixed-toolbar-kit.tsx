3e78b5caf4809e7251d37a924ddbd1b840e1cbf9
Tapi221 <aaatsub1@gmail.com>
2026-06-13 01:15:33 +0900

    Fix moved chip ui toolbar imports

--- src/components/editor/plugins/fixed-toolbar-kit.tsx
+++ src/components/editor/plugins/fixed-toolbar-kit.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { createPlatePlugin } from "platejs/react";
-import { FixedToolbar } from "@/chip/ui/fixed-toolbar";
+import { FixedToolbar } from "@/chip/ui/toolbar/fixed-toolbar";
 import { FixedToolbarButtons } from "@/chip/ui/button/fixed-toolbar-buttons";
 
 const FixedToolbarKit = [
