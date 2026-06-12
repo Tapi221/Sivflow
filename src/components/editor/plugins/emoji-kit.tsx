4af34604b12c5f5306c288e42c5203851aeb7793
Tapi221 <aaatsub1@gmail.com>
2026-06-13 01:58:02 +0900

    Fix emoji node import path

--- src/components/editor/plugins/emoji-kit.tsx
+++ src/components/editor/plugins/emoji-kit.tsx
@@ -2,7 +2,7 @@
 
 import emojiMartData from "@emoji-mart/data";
 import { EmojiInputPlugin, EmojiPlugin } from "@platejs/emoji/react";
-import { EmojiInputElement } from "@/chip/ui/emoji-node";
+import { EmojiInputElement } from "@/chip/ui/node/emoji-node";
 
 const EmojiKit = [EmojiPlugin.configure({ options: { data: emojiMartData as any } }), EmojiInputPlugin.withComponent(EmojiInputElement)];
 
