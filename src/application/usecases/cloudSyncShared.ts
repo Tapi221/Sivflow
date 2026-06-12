7eed2ea985e7492b8b5daaf801c21b596be4ad30
Tapi221 <aaatsub1@gmail.com>
2026-06-12 15:44:41 +0900

    Update shared sync module format

--- src/application/usecases/cloudSyncShared.ts
+++ src/application/usecases/cloudSyncShared.ts
@@ -1,6 +1,21 @@
+import * as ChangeParsing from "./cloudSyncChangeParsing";
+import * as EntityMetadata from "./cloudSyncEntityMetadata";
+import * as Sanitizers from "./cloudSyncSanitizers";
+import * as Timestamps from "./cloudSyncTimestamps";
 
+const getChangeId = ChangeParsing.getChangeId;
+const getChangeParts = ChangeParsing.getChangeParts;
+const COLLECTION_BY_TYPE = EntityMetadata.COLLECTION_BY_TYPE;
+const CURRENT_TAG_COLLECTION = EntityMetadata.CURRENT_TAG_COLLECTION;
+const isCloudEntityType = EntityMetadata.isCloudEntityType;
+const PULLABLE_ENTITY_TYPES = EntityMetadata.PULLABLE_ENTITY_TYPES;
+const deepStripUndefined = Sanitizers.deepStripUndefined;
+const sanitizeSyncDataForCloud = Sanitizers.sanitizeSyncDataForCloud;
+const sanitizeSyncDataFromCloud = Sanitizers.sanitizeSyncDataFromCloud;
+const getUpdatedAtMillis = Timestamps.getUpdatedAtMillis;
 
-export { getChangeId, getChangeParts } from "./cloudSyncChangeParsing";
-export { type CloudEntityType, COLLECTION_BY_TYPE, CURRENT_TAG_COLLECTION, isCloudEntityType, PULLABLE_ENTITY_TYPES, type PullableEntityType } from "./cloudSyncEntityMetadata";
-export { deepStripUndefined, sanitizeSyncDataForCloud, sanitizeSyncDataFromCloud } from "./cloudSyncSanitizers";
-export { getUpdatedAtMillis } from "./cloudSyncTimestamps";
+type CloudEntityType = EntityMetadata.CloudEntityType;
+type PullableEntityType = EntityMetadata.PullableEntityType;
+
+export { COLLECTION_BY_TYPE, CURRENT_TAG_COLLECTION, PULLABLE_ENTITY_TYPES, deepStripUndefined, getChangeId, getChangeParts, getUpdatedAtMillis, isCloudEntityType, sanitizeSyncDataForCloud, sanitizeSyncDataFromCloud };
+export type { CloudEntityType, PullableEntityType };
