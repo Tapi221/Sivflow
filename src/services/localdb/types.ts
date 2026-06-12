a62bfca7c65e49a4c24f7acf7d7d2ab6bb15f5d7
Tapi221 <aaatsub1@gmail.com>
2026-06-12 19:55:06 +0900

    Expose complete LocalDB store types

--- src/services/localdb/types.ts
+++ src/services/localdb/types.ts
@@ -1,3 +1,13 @@
-import type { CardRelation, LocalDBInstance, LocalDBLike, LocalDBSyncApi, LocalDBTableMap, ProjectMap, QueryableCollection, QueryableKeyPath, QueryableTable, QueryableWhereClause, QueryableWhereFunction, SyncableEntityTable, TagRecord } from "./localdb.types";
+import type { CardRelation, LocalDBInstance, LocalDBLike as BaseLocalDBLike, LocalDBSyncApi, LocalDBTableMap, ProjectMap, QueryableCollection, QueryableKeyPath, QueryableTable, QueryableWhereClause, QueryableWhereFunction, SyncableEntityTable, TagRecord } from "./localdb.types";
 
-export type { CardRelation, LocalDBInstance, LocalDBLike, LocalDBSyncApi, LocalDBTableMap, ProjectMap, QueryableCollection, QueryableKeyPath, QueryableTable, QueryableWhereClause, QueryableWhereFunction, SyncableEntityTable, TagRecord };
+type LocalDBLike = BaseLocalDBLike & {
+  metadata: QueryableTable<Record<string, unknown>, string>;
+  levelHistories: QueryableTable<Record<string, unknown>, string>;
+  deviceMeta: QueryableTable<Record<string, unknown>, string>;
+  cardRelations: QueryableTable<Record<string, unknown>, string>;
+  projectMaps: QueryableTable<ProjectMap, string>;
+  studyLogs: QueryableTable<Record<string, unknown>, string>;
+};
+type LocalDBSyncStore = LocalDBLike;
+
+export type { CardRelation, LocalDBInstance, LocalDBLike, LocalDBSyncApi, LocalDBSyncStore, LocalDBTableMap, ProjectMap, QueryableCollection, QueryableKeyPath, QueryableTable, QueryableWhereClause, QueryableWhereFunction, SyncableEntityTable, TagRecord };
