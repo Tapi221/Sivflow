7422c9f91c9a67127f99e06d25ce02ac686f9dc7
Tapi221 <aaatsub1@gmail.com>
2026-06-12 22:15:28 +0900

    Neutralize Plate link color

--- src/components/ui/link-node.tsx
+++ src/components/ui/link-node.tsx
@@ -1,21 +1,33 @@
 "use client";
 
-import * as React from "react";
 import { getLinkAttributes } from "@platejs/link";
+import type { MouseEvent } from "react";
 import type { TLinkElement } from "platejs";
 import type { PlateElementProps } from "platejs/react";
 import { PlateElement } from "platejs/react";
 import { inlineSuggestionVariants } from "@/lib/suggestion";
 import { cn } from "@/lib/utils";
 
+const LINK_ELEMENT_CLASS_NAME = "font-medium text-inherit underline decoration-current underline-offset-4";
+
 const LinkElement = (props: PlateElementProps<TLinkElement>) => {
-  return (<PlateElement {...props} as="a" className={cn("font-medium text-primary underline decoration-primary underline-offset-4", inlineSuggestionVariants())} attributes={{ ...props.attributes, ...getLinkAttributes(props.editor, props.element), onMouseOver: (event) => {
-    event.stopPropagation();
-  },
-  }}
-  >
-    {props.children}
-  </PlateElement>
+  const { attributes, children, editor, element } = props;
+
+  return (
+    <PlateElement
+      {...props}
+      as="a"
+      className={cn(LINK_ELEMENT_CLASS_NAME, inlineSuggestionVariants())}
+      attributes={{
+        ...attributes,
+        ...getLinkAttributes(editor, element),
+        onMouseOver: (event: MouseEvent<HTMLAnchorElement>) => {
+          event.stopPropagation();
+        },
+      }}
+    >
+      {children}
+    </PlateElement>
   );
 };
 
