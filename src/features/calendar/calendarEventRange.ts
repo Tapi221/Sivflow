38bfaf874236fed4c63dc4da9bc8dcfdda5a8abd
Tapi221 <aaatsub1@gmail.com>
2026-06-12 15:43:06 +0900

    Update calendar range module

--- src/features/calendar/calendarEventRange.ts
+++ src/features/calendar/calendarEventRange.ts
@@ -1,3 +1,11 @@
+import * as source from "@core/calendar/calendarEventRange";
 
+const clipEventToDay = source.clipEventToDay;
+const compareCalendarEvents = source.compareCalendarEvents;
+const eventOverlapsDay = source.eventOverlapsDay;
+const eventOverlapsRange = source.eventOverlapsRange;
+const getCalendarDateKey = source.getCalendarDateKey;
+const getDayRange = source.getDayRange;
+const getEventDateKeys = source.getEventDateKeys;
 
-export { clipEventToDay, compareCalendarEvents, eventOverlapsDay, eventOverlapsRange, getCalendarDateKey, getDayRange, getEventDateKeys } from "@core/calendar/calendarEventRange";
+export { clipEventToDay, compareCalendarEvents, eventOverlapsDay, eventOverlapsRange, getCalendarDateKey, getDayRange, getEventDateKeys };
