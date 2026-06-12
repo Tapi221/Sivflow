3d800b0860f2971e63c401cc07079c72595a4d27
Tapi221 <aaatsub1@gmail.com>
2026-06-13 01:30:36 +0900

    Fix moved media toolbar imports

--- src/chip/ui/button/media-toolbar-button.tsx
+++ src/chip/ui/button/media-toolbar-button.tsx
@@ -8,10 +8,10 @@ import { isUrl, KEYS } from "platejs";
 import { useEditorRef } from "platejs/react";
 import { toast } from "sonner";
 import { useFilePicker } from "use-file-picker";
-import { AlertDialog, AlertDialogAction, AlertDialogCancel, AlertDialogContent, AlertDialogDescription, AlertDialogFooter, AlertDialogHeader, AlertDialogTitle } from "./dialog/alert-dialog";
-import { DropdownMenu, DropdownMenuContent, DropdownMenuGroup, DropdownMenuItem, DropdownMenuTrigger } from "./menu/dropdown-menu";
-import { Input } from "./input";
-import { ToolbarSplitButton, ToolbarSplitButtonPrimary, ToolbarSplitButtonSecondary } from "./toolbar";
+import { AlertDialog, AlertDialogAction, AlertDialogCancel, AlertDialogContent, AlertDialogDescription, AlertDialogFooter, AlertDialogHeader, AlertDialogTitle } from "../dialog/alert-dialog";
+import { DropdownMenu, DropdownMenuContent, DropdownMenuGroup, DropdownMenuItem, DropdownMenuTrigger } from "../menu/dropdown-menu";
+import { Input } from "../input";
+import { ToolbarSplitButton, ToolbarSplitButtonPrimary, ToolbarSplitButtonSecondary } from "../toolbar";
 
 const MEDIA_CONFIG: Record<
   string,
