ad5472749d57e9ecf9839fe72c7a357e7a7a5e0d
Tapi221 <aaatsub1@gmail.com>
2026-06-12 15:51:19 +0900

    Update desktop api type entry

--- src/types/desktop-api.ts
+++ src/types/desktop-api.ts
@@ -1,3 +1,3 @@
-
+void "desktop-api-types";
 
 export type { DesktopBridgeApi, DesktopFileApi, DesktopImportFileOpenPayload, DesktopImportFileReadResult, DesktopOauthApi, DesktopOauthCallbackPayload, DesktopWindowApi, PlatformApi } from "@platform/desktopApi";
