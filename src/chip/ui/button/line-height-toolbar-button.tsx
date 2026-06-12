545104ff05408a38129cd477b33eebe7b00dbf6b
Tapi221 <aaatsub1@gmail.com>
2026-06-13 01:28:25 +0900

    Fix moved toolbar menu imports

--- src/chip/ui/button/line-height-toolbar-button.tsx
+++ src/chip/ui/button/line-height-toolbar-button.tsx
@@ -6,8 +6,8 @@ import type { DropdownMenuProps } from "@radix-ui/react-dropdown-menu";
 import { DropdownMenuItemIndicator } from "@radix-ui/react-dropdown-menu";
 import { CheckIcon, WrapText } from "lucide-react";
 import { useEditorRef, useSelectionFragmentProp } from "platejs/react";
-import { DropdownMenu, DropdownMenuContent, DropdownMenuRadioGroup, DropdownMenuRadioItem, DropdownMenuTrigger } from "./menu/dropdown-menu";
-import { ToolbarButton } from "./toolbar";
+import { DropdownMenu, DropdownMenuContent, DropdownMenuRadioGroup, DropdownMenuRadioItem, DropdownMenuTrigger } from "../menu/dropdown-menu";
+import { ToolbarButton } from "../toolbar";
 
 const LineHeightToolbarButton = (props: DropdownMenuProps) => {
   const editor = useEditorRef();
