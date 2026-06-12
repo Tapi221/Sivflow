070139e346d504b672273e197d94a8bc3fc94da5
Tapi221 <aaatsub1@gmail.com>
2026-06-12 16:13:44 +0900

    fix import order

--- src/features/explorer/contracts/explorerBreadcrumbContext.ts
+++ src/features/explorer/contracts/explorerBreadcrumbContext.ts
@@ -1,5 +1,5 @@
-import { EMPTY_EXPLORER_BREADCRUMB_CONTEXT as EMPTY_CONTEXT, areExplorerBreadcrumbContextsEqual as areContextsEqual } from "@/features/breadcrumbs/breadcrumbs.types";
 import type { ExplorerBreadcrumbContext } from "@/features/breadcrumbs/breadcrumbs.types";
+import { EMPTY_EXPLORER_BREADCRUMB_CONTEXT as EMPTY_CONTEXT, areExplorerBreadcrumbContextsEqual as areContextsEqual } from "@/features/breadcrumbs/breadcrumbs.types";
 
 const EMPTY_EXPLORER_BREADCRUMB_CONTEXT = EMPTY_CONTEXT;
 const areExplorerBreadcrumbContextsEqual = areContextsEqual;
