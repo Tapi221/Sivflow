d96684be13480dd58dbeed2707ccdea304b75aa6
Tapi221 <aaatsub1@gmail.com>
2026-06-12 22:04:47 +0900

    Expose localdb aggregate read methods

--- src/services/localdb/types.ts
+++ src/services/localdb/types.ts
@@ -1,6 +1,6 @@
 import type { CardRelation, LocalDBInstance, LocalDBLike as BaseLocalDBLike, LocalDBSyncApi, LocalDBTableMap, ProjectMap, QueryableCollection, QueryableKeyPath, QueryableTable, QueryableWhereClause, QueryableWhereFunction, SyncableEntityTable, TagRecord } from "./localdb.types";
+import type { Card, Folder } from "@/types";
 import type { SyncConflict, SyncQueueItem, SyncSettings } from "@/types/domain/sync";
-
 
 type LocalDBLike = BaseLocalDBLike & {
   table<T extends object = Record<string, unknown>, TKey = string>(name: string): QueryableTable<T, TKey>;
@@ -15,6 +15,8 @@ type LocalDBLike = BaseLocalDBLike & {
   bulkUpsert(table: string, items: unknown[], skipSync?: boolean): Promise<void>;
   cleanupSyncHistory(): Promise<void>;
   cleanupSyncErrors(): Promise<void>;
+  getAllCards(): Promise<Card[]>;
+  getAllFolders(): Promise<Folder[]>;
   getConflict(id: string): Promise<SyncConflict | undefined>;
   removeConflict(id: string): Promise<void>;
   metadata: QueryableTable<Record<string, unknown>, string>;
