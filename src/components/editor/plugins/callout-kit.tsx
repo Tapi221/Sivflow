f5b7e50f3928e5844b7c7fca99a4e3a1e4184b4e
Tapi221 <aaatsub1@gmail.com>
2026-06-13 01:58:18 +0900

    Fix callout node import path

--- src/components/editor/plugins/callout-kit.tsx
+++ src/components/editor/plugins/callout-kit.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { CalloutPlugin } from "@platejs/callout/react";
-import { CalloutElement } from "@/chip/ui/callout-node";
+import { CalloutElement } from "@/chip/ui/node/callout-node";
 
 const CalloutKit = [CalloutPlugin.withComponent(CalloutElement)];
 
