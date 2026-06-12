9188fcd9cf9d1c303a33e2a2abd404750f00e229
Tapi221 <aaatsub1@gmail.com>
2026-06-12 21:52:39 +0900

    Guard optional localdb cleanup methods

--- src/contexts/auth/bootstrapUser.ts
+++ src/contexts/auth/bootstrapUser.ts
@@ -1,6 +1,11 @@
 import { AppInitializer } from "@/services/AppInitializer";
 import { getLocalDb, initializeDB } from "@/services/localdb";
 
+type CleanupCapableLocalDb = Awaited<ReturnType<typeof getLocalDb>> & {
+  cleanupSyncHistory?: () => Promise<void>;
+  cleanupSyncErrors?: () => Promise<void>;
+};
+
 const bootstrapUser = async (userId: string) => {
   await initializeDB(userId);
 
@@ -14,8 +19,9 @@ const bootstrapUser = async (userId: string) => {
   }
 
   const db = await getLocalDb();
-  await db.cleanupSyncHistory();
-  await db.cleanupSyncErrors();
+  const cleanupDb = db as CleanupCapableLocalDb;
+  await cleanupDb.cleanupSyncHistory?.();
+  await cleanupDb.cleanupSyncErrors?.();
 };
 
 export { bootstrapUser };
