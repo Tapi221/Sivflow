fcaa4f8325b2f2679b3a7a5f305bf6ee337592c4
Tapi221 <aaatsub1@gmail.com>
2026-06-13 18:01:55 +0900

    Fill Plate registry floating toolbar UI

--- src/chip/ui/plate/floating-toolbar.tsx
+++ src/chip/ui/plate/floating-toolbar.tsx
@@ -1,1 +1,20 @@
-export {};
+"use client";
+
+import * as React from "react";
+import { Toolbar } from "@/chip/ui/plate/toolbar";
+import { cn } from "@/lib/utils";
+
+const FloatingToolbar = ({ className, ...props }: React.ComponentProps<typeof Toolbar>) => {
+  return (
+    <Toolbar
+      {...props}
+      className={cn(
+        "scrollbar-hide absolute z-50 overflow-x-auto whitespace-nowrap rounded-md border bg-popover p-1 opacity-100 shadow-md print:hidden",
+        "max-w-[80vw]",
+        className,
+      )}
+    />
+  );
+};
+
+export { FloatingToolbar };
