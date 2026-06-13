ae60722bf08d4cc9ebf27e58bb0b2bcd9e7ae109
Tapi221 <aaatsub1@gmail.com>
2026-06-14 05:50:54 +0900

    Fix line-height toolbar menu indicator layout

--- src/chip/ui/plate/line-height-toolbar-button.tsx
+++ src/chip/ui/plate/line-height-toolbar-button.tsx
@@ -1,28 +1,21 @@
 "use client";
 
-import * as React from "react";
-
 import { LineHeightPlugin } from "@platejs/basic-styles/react";
-
-import type { DropdownMenuProps } from "@radix-ui/react-dropdown-menu";
-
 import { DropdownMenuItemIndicator } from "@radix-ui/react-dropdown-menu";
-
+import type { DropdownMenuProps } from "@radix-ui/react-dropdown-menu";
 import { CheckIcon, WrapText } from "lucide-react";
-
 import { useEditorRef, useSelectionFragmentProp } from "platejs/react";
-
+import { useState } from "react";
 import { DropdownMenu, DropdownMenuContent, DropdownMenuRadioGroup, DropdownMenuRadioItem, DropdownMenuTrigger } from "@/chip/panel/dropdown-menu";
+import { ToolbarButton } from "@/chip/ui/plate/toolbar";
 
-import { ToolbarButton } from "./toolbar";
-
-
+const LINE_HEIGHT_MENU_ITEM_CLASS_NAME = "dropdown-menu__radio-item--check-end min-w-[180px]";
 
 const LineHeightToolbarButton = (props: DropdownMenuProps) => {
   const editor = useEditorRef();
   const { defaultNodeValue, validNodeValues: values = [] } = editor.getInjectProps(LineHeightPlugin);
   const value = useSelectionFragmentProp({ defaultValue: defaultNodeValue, getProp: (node) => node.lineHeight });
-  const [open, setOpen] = React.useState(false);
+  const [open, setOpen] = useState(false);
   return (
     <DropdownMenu open={open} onOpenChange={setOpen} modal={false} {...props}>
       <DropdownMenuTrigger asChild>
@@ -39,8 +32,8 @@ const LineHeightToolbarButton = (props: DropdownMenuProps) => {
           }}
         >
           {values.map((nextValue) => (
-            <DropdownMenuRadioItem key={nextValue} className="min-w-[180px] pl-2 *:first:[span]:hidden" value={nextValue}>
-              <span className="pointer-events-none absolute right-2 flex size-3.5 items-center justify-center">
+            <DropdownMenuRadioItem key={nextValue} className={LINE_HEIGHT_MENU_ITEM_CLASS_NAME} value={nextValue}>
+              <span className="dropdown-menu__check-indicator">
                 <DropdownMenuItemIndicator>
                   <CheckIcon />
                 </DropdownMenuItemIndicator>
@@ -53,7 +46,5 @@ const LineHeightToolbarButton = (props: DropdownMenuProps) => {
     </DropdownMenu>
   );
 };
-
-
 
 export { LineHeightToolbarButton };
