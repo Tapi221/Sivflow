34987705ef83b07563c80883147e72293a661251
Tapi221 <aaatsub1@gmail.com>
2026-06-14 03:49:31 +0900

    Add Plate classic list toolbar buttons

---
+++ src/chip/ui/plate/list-classic-toolbar-button.tsx
@@ -0,0 +1,58 @@
+"use client";
+
+import type { ToolbarButtonProps } from "@/chip/ui/plate/toolbar";
+import { IndentIcon, ListIcon, ListOrderedIcon, OutdentIcon } from "lucide-react";
+import { KEYS } from "platejs";
+import { useEditorRef } from "platejs/react";
+import { ToolbarButton } from "@/chip/ui/plate/toolbar";
+
+type ListToolbarButtonProps = ToolbarButtonProps & {
+  nodeType: string;
+};
+type IndentToolbarButtonProps = ToolbarButtonProps & {
+  reverse?: boolean;
+};
+
+const ListToolbarButton = ({ nodeType, ...props }: ListToolbarButtonProps) => {
+  const editor = useEditorRef();
+  const icon = nodeType === KEYS.olClassic ? <ListOrderedIcon /> : <ListIcon />;
+  return (
+    <ToolbarButton
+      {...props}
+      tooltip={nodeType === KEYS.olClassic ? "Numbered list" : "Bulleted list"}
+      onClick={() => {
+        editor.tf.toggleBlock(nodeType);
+        editor.tf.focus();
+      }}
+      onMouseDown={(event) => event.preventDefault()}
+    >
+      {props.children ?? icon}
+    </ToolbarButton>
+  );
+};
+const IndentToolbarButton = ({ reverse = false, ...props }: IndentToolbarButtonProps) => {
+  const editor = useEditorRef();
+  return (
+    <ToolbarButton
+      {...props}
+      tooltip={reverse ? "Outdent" : "Indent"}
+      onClick={() => {
+        if (reverse) {
+          editor.tf.outdent();
+        } else {
+          editor.tf.indent();
+        }
+        editor.tf.focus();
+      }}
+      onMouseDown={(event) => event.preventDefault()}
+    >
+      {props.children ?? (reverse ? <OutdentIcon /> : <IndentIcon />)}
+    </ToolbarButton>
+  );
+};
+const TodoListToolbarButton = (props: ToolbarButtonProps) => {
+  return <ListToolbarButton {...props} nodeType={KEYS.taskList} />;
+};
+
+export { IndentToolbarButton, ListToolbarButton, TodoListToolbarButton };
+export type { IndentToolbarButtonProps, ListToolbarButtonProps };
