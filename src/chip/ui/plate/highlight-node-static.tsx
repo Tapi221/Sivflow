106f502672989163515afd0fe6982a40abf217af
Tapi221 <aaatsub1@gmail.com>
2026-06-14 05:50:50 +0900

    Normalize Plate static highlight style

--- src/chip/ui/plate/highlight-node-static.tsx
+++ src/chip/ui/plate/highlight-node-static.tsx
@@ -1,14 +1,12 @@
 import type { SlateLeafProps } from "platejs/static";
 import { SlateLeaf } from "platejs/static";
 
-
-
-const HighlightLeafStatic = (props: SlateLeafProps) => (
-  <SlateLeaf {...props} as="mark" className="bg-highlight/30 text-inherit">
-    {props.children}
-  </SlateLeaf>
-);
-
-
+const HighlightLeafStatic = (props: SlateLeafProps) => {
+  return (
+    <SlateLeaf {...props} as="mark" className="bg-muted text-inherit">
+      {props.children}
+    </SlateLeaf>
+  );
+};
 
 export { HighlightLeafStatic };
