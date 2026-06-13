275d159da0f6697f39a5a526482aa00aefb0bef4
Tapi221 <aaatsub1@gmail.com>
2026-06-13 18:10:17 +0900

    Add Plate registry static audio media node UI

---
+++ src/chip/ui/plate/media-audio-node-static.tsx
@@ -0,0 +1,19 @@
+import type { TAudioElement } from "platejs";
+import type { PlateElementProps } from "platejs/react";
+import { PlateElement } from "platejs/react";
+
+const AudioElementStatic = (props: PlateElementProps<TAudioElement>) => {
+  const { element } = props;
+  const caption = element.caption?.[0]?.children?.[0]?.text;
+  return (
+    <PlateElement {...props} className="py-2.5">
+      <figure className="my-0">
+        <audio className="w-full" src={element.url as string} controls />
+        {caption ? <figcaption className="mt-2 text-center text-muted-foreground text-sm">{caption}</figcaption> : null}
+      </figure>
+      {props.children}
+    </PlateElement>
+  );
+};
+
+export { AudioElementStatic };
