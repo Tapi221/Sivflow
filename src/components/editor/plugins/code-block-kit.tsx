569363ddc4c406881c6d60bca3eadb51948a7888
Tapi221 <aaatsub1@gmail.com>
2026-06-13 19:49:06 +0900

    Use plate code block components

--- src/components/editor/plugins/code-block-kit.tsx
+++ src/components/editor/plugins/code-block-kit.tsx
@@ -3,7 +3,7 @@
 import { CodeBlockRules } from "@platejs/code-block";
 import { CodeBlockPlugin, CodeLinePlugin, CodeSyntaxPlugin } from "@platejs/code-block/react";
 import { all, createLowlight } from "lowlight";
-import { CodeBlockElement, CodeLineElement, CodeSyntaxLeaf } from "@/chip/ui/node/code-block-node";
+import { CodeBlockElement, CodeLineElement, CodeSyntaxLeaf } from "@/chip/ui/plate/code-block-node";
 
 const lowlight = createLowlight(all);
 const CodeBlockKit = [
