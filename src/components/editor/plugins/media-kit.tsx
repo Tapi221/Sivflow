35af86838aaac005c2d792d54e21a3eff49794fb
Tapi221 <aaatsub1@gmail.com>
2026-06-13 18:27:30 +0900

    Use Plate file media UI

--- src/components/editor/plugins/media-kit.tsx
+++ src/components/editor/plugins/media-kit.tsx
@@ -6,10 +6,10 @@ import { KEYS } from "platejs";
 import { MediaPreviewDialog } from "@/chip/ui/dialog/media-preview-dialog";
 import { MediaUploadToast } from "@/chip/ui/media-upload-toast";
 import { MediaEmbedElement } from "@/chip/ui/node/media-embed-node";
-import { FileElement } from "@/chip/ui/node/media-file-node";
 import { PlaceholderElement } from "@/chip/ui/node/media-placeholder-node";
 import { VideoElement } from "@/chip/ui/node/media-video-node";
 import { AudioElement } from "@/chip/ui/plate/media-audio-node";
+import { FileElement } from "@/chip/ui/plate/media-file-node";
 import { ImageElement } from "@/chip/ui/plate/media-image-node";
 
 const MediaKit = [
