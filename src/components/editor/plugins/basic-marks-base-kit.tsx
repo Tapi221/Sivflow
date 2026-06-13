dd247a077864031c40cda93fb3afd576b8f37a71
Tapi221 <aaatsub1@gmail.com>
2026-06-13 20:06:23 +0900

    Move basic marks base kit to plate UI

--- src/components/editor/plugins/basic-marks-base-kit.tsx
+++ src/components/editor/plugins/basic-marks-base-kit.tsx
@@ -1,8 +1,18 @@
 import { BaseBoldPlugin, BaseCodePlugin, BaseHighlightPlugin, BaseItalicPlugin, BaseKbdPlugin, BaseStrikethroughPlugin, BaseSubscriptPlugin, BaseSuperscriptPlugin, BaseUnderlinePlugin } from "@platejs/basic-nodes";
-import { CodeLeafStatic } from "@/chip/ui/node/code-node-static";
-import { HighlightLeafStatic } from "@/chip/ui/node/highlight-node-static";
-import { KbdLeafStatic } from "@/chip/ui/node/kbd-node-static";
+import { CodeLeafStatic } from "@/chip/ui/plate/code-node-static";
+import { HighlightLeafStatic } from "@/chip/ui/plate/highlight-node-static";
+import { KbdLeafStatic } from "@/chip/ui/plate/kbd-node-static";
 
-const BaseBasicMarksKit = [BaseBoldPlugin, BaseItalicPlugin, BaseUnderlinePlugin, BaseCodePlugin.withComponent(CodeLeafStatic), BaseStrikethroughPlugin, BaseSubscriptPlugin, BaseSuperscriptPlugin, BaseHighlightPlugin.withComponent(HighlightLeafStatic), BaseKbdPlugin.withComponent(KbdLeafStatic)];
+const BaseBasicMarksKit = [
+  BaseBoldPlugin,
+  BaseItalicPlugin,
+  BaseUnderlinePlugin,
+  BaseCodePlugin.withComponent(CodeLeafStatic),
+  BaseStrikethroughPlugin,
+  BaseSubscriptPlugin,
+  BaseSuperscriptPlugin,
+  BaseHighlightPlugin.withComponent(HighlightLeafStatic),
+  BaseKbdPlugin.withComponent(KbdLeafStatic),
+];
 
 export { BaseBasicMarksKit };
