614b646fc0366db02e7391a2f1a637098cafc8b5
Tapi221 <aaatsub1@gmail.com>
2026-06-13 20:07:27 +0900

    Move math kit to plate UI

--- src/components/editor/plugins/math-kit.tsx
+++ src/components/editor/plugins/math-kit.tsx
@@ -2,8 +2,21 @@
 
 import { MathRules } from "@platejs/math";
 import { EquationPlugin, InlineEquationPlugin } from "@platejs/math/react";
-import { EquationElement, InlineEquationElement } from "@/chip/ui/node/equation-node";
+import { EquationElement, InlineEquationElement } from "@/chip/ui/plate/equation-node";
 
-const MathKit = [InlineEquationPlugin.configure({ inputRules: [MathRules.markdown({ variant: "$" })], node: { component: InlineEquationElement } }), EquationPlugin.configure({ inputRules: [MathRules.markdown({ on: "break", variant: "$$" })], node: { component: EquationElement } })];
+const MathKit = [
+  InlineEquationPlugin.configure({
+    inputRules: [
+      MathRules.markdown({ variant: "$" }),
+    ],
+    node: { component: InlineEquationElement },
+  }),
+  EquationPlugin.configure({
+    inputRules: [
+      MathRules.markdown({ on: "break", variant: "$$" }),
+    ],
+    node: { component: EquationElement },
+  }),
+];
 
 export { MathKit };
