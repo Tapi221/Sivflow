df615d3981683254ce85fd0ea25cba57c7b82f00
Tapi221 <aaatsub1@gmail.com>
2026-06-13 19:48:23 +0900

    Use plate link node

--- src/components/editor/plugins/link-kit.tsx
+++ src/components/editor/plugins/link-kit.tsx
@@ -2,7 +2,7 @@
 
 import { LinkRules } from "@platejs/link";
 import { LinkPlugin } from "@platejs/link/react";
-import { LinkElement } from "@/chip/ui/node/link-node";
+import { LinkElement } from "@/chip/ui/plate/link-node";
 import { LinkFloatingToolbar } from "@/chip/ui/toolbar/link-toolbar";
 
 const LinkKit = [
