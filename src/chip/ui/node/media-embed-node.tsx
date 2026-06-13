483b6267b7f846d4a571023ea368c12c2428fe64
Tapi221 <aaatsub1@gmail.com>
2026-06-13 18:41:00 +0900

    Replace media embed node with Plate registry implementation

--- src/chip/ui/node/media-embed-node.tsx
+++ src/chip/ui/node/media-embed-node.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import * as React from "react";
 import { parseTwitterUrl, parseVideoUrl } from "@platejs/media";
 import { MediaEmbedPlugin, useMediaState } from "@platejs/media/react";
 import { ResizableProvider, useResizableValue } from "@platejs/resizable";
@@ -8,22 +9,33 @@ import type { PlateElementProps } from "platejs/react";
 import { PlateElement, withHOC } from "platejs/react";
 import LiteYouTubeEmbed from "react-lite-youtube-embed";
 import { Tweet } from "react-tweet";
-import { Caption, CaptionTextarea } from "@/chip/ui/caption";
-import { mediaResizeHandleVariants, Resizable, ResizeHandle } from "@/chip/ui/resize-handle";
-import { MediaToolbar } from "@/chip/ui/toolbar/media-toolbar";
+import { Caption, CaptionTextarea } from "@/chip/ui/plate/caption";
+import { MediaToolbar } from "@/chip/ui/plate/media-toolbar";
+import { mediaResizeHandleVariants, Resizable, ResizeHandle } from "@/chip/ui/plate/resize-handle";
 import { cn } from "@/lib/utils";
 
-const MediaEmbedElementBase = (props: PlateElementProps<TMediaEmbedElement>) => {
-  const { align = "center", embed, focused, isTweet, isVideo, isYoutube, readOnly, selected } = useMediaState({
+const MediaEmbedElement = withHOC(ResizableProvider, (props: PlateElementProps<TMediaEmbedElement>) => {
+  const {
+    align = "center",
+    embed,
+    focused,
+    isTweet,
+    isVideo,
+    isYoutube,
+    readOnly,
+    selected,
+  } = useMediaState({
     urlParsers: [parseTwitterUrl, parseVideoUrl],
   });
   const width = useResizableValue("width");
   const provider = embed?.provider;
-
   return (
     <MediaToolbar plugin={MediaEmbedPlugin}>
       <PlateElement className="py-2.5" {...props}>
-        <figure className="group relative m-0 w-full cursor-default" contentEditable={false}>
+        <figure
+          className="group relative m-0 w-full cursor-default"
+          contentEditable={false}
+        >
           <Resizable
             align={align}
             options={{
@@ -32,7 +44,10 @@ const MediaEmbedElementBase = (props: PlateElementProps<TMediaEmbedElement>) => 
               minWidth: isTweet ? 300 : 100,
             }}
           >
-            <ResizeHandle className={mediaResizeHandleVariants({ direction: "left" })} options={{ direction: "left" }} />
+            <ResizeHandle
+              className={mediaResizeHandleVariants({ direction: "left" })}
+              options={{ direction: "left" }}
+            />
             {isVideo ? (
               isYoutube ? (
                 <LiteYouTubeEmbed
@@ -42,18 +57,7 @@ const MediaEmbedElementBase = (props: PlateElementProps<TMediaEmbedElement>) => 
                     "rounded-sm",
                     focused && selected && "ring-2 ring-ring ring-offset-2",
                     "relative block cursor-pointer bg-black bg-center bg-cover [contain:content]",
-                    "[&.lyt-activated]:before:absolute [&.lyt-activated]:before:top-0 [&.lyt-activated]:before:h-[60px] [&.lyt-activated]:before:w-full [&.lyt-activated]:before:bg-top [&.lyt-activated]:before:bg-repeat-x [&.lyt-activated]:before:pb-[50px] [&.lyt-activated]:before:[transition:all_0.2s_cubic-bezier(0,_0,_0.2,_1)]",
-                    "[&.lyt-activated]:before:bg-[url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAADGCAYAAAAT+OqFAAAAdklEQVQoz42QQQ7AIAgEF/T/D+kbq/RWAlnQyyazA4aoAB4FsBSA/bFjuF1EOL7VbrIrBuusmrt4ZZORfb6ehbWdnRHEIiITaEUKa5EJqUakRSaEYBJSCY2dEstQY7AuxahwXFrvZmWl2rh4JZ07z9dLtesfNj5q0FU3A5ObbwAAAABJRU5ErkJggg==)]",
                     "after:block after:pb-[var(--aspect-ratio)] after:content-[\"\"]",
-                    "[&_>_iframe]:absolute [&_>_iframe]:top-0 [&_>_iframe]:left-0 [&_>_iframe]:size-full",
-                    "[&_>_.lty-playbtn]:z-1 [&_>_.lty-playbtn]:h-[46px] [&_>_.lty-playbtn]:w-[70px] [&_>_.lty-playbtn]:rounded-[14%] [&_>_.lty-playbtn]:bg-[#212121] [&_>_.lty-playbtn]:opacity-80 [&_>_.lty-playbtn]:[transition:all_0.2s_cubic-bezier(0,_0,_0.2,_1)]",
-                    "[&:hover_>_.lty-playbtn]:bg-[red] [&:hover_>_.lty-playbtn]:opacity-100",
-                    "[&_>_.lty-playbtn]:before:border-[transparent_transparent_transparent_#fff] [&_>_.lty-playbtn]:before:border-y-[11px] [&_>_.lty-playbtn]:before:border-r-0 [&_>_.lty-playbtn]:before:border-l-[19px] [&_>_.lty-playbtn]:before:content-[\"\"]",
-                    "[&_>_.lty-playbtn]:absolute [&_>_.lty-playbtn]:top-1/2 [&_>_.lty-playbtn]:left-1/2 [&_>_.lty-playbtn]:[transform:translate3d(-50%,-50%,0)]",
-                    "[&_>_.lty-playbtn]:before:absolute [&_>_.lty-playbtn]:before:top-1/2 [&_>_.lty-playbtn]:before:left-1/2 [&_>_.lty-playbtn]:before:[transform:translate3d(-50%,-50%,0)]",
-                    "[&.lyt-activated]:cursor-[unset]",
-                    "[&.lyt-activated]:before:pointer-events-none [&.lyt-activated]:before:opacity-0",
-                    "[&.lyt-activated_>_.lty-playbtn]:pointer-events-none [&.lyt-activated_>_.lty-playbtn]:opacity-0!",
                   )}
                 />
               ) : (
@@ -82,13 +86,18 @@ const MediaEmbedElementBase = (props: PlateElementProps<TMediaEmbedElement>) => 
               <div
                 className={cn(
                   "[&_.react-tweet-theme]:my-0",
-                  !readOnly && selected && "[&_.react-tweet-theme]:ring-2 [&_.react-tweet-theme]:ring-ring [&_.react-tweet-theme]:ring-offset-2",
+                  !readOnly &&
+                    selected &&
+                    "[&_.react-tweet-theme]:ring-2 [&_.react-tweet-theme]:ring-ring [&_.react-tweet-theme]:ring-offset-2",
                 )}
               >
                 <Tweet id={embed!.id!} />
               </div>
             )}
-            <ResizeHandle className={mediaResizeHandleVariants({ direction: "right" })} options={{ direction: "right" }} />
+            <ResizeHandle
+              className={mediaResizeHandleVariants({ direction: "right" })}
+              options={{ direction: "right" }}
+            />
           </Resizable>
           <Caption style={{ width }} align={align}>
             <CaptionTextarea placeholder="Write a caption..." />
@@ -98,10 +107,6 @@ const MediaEmbedElementBase = (props: PlateElementProps<TMediaEmbedElement>) => 
       </PlateElement>
     </MediaToolbar>
   );
-};
-
-MediaEmbedElementBase.displayName = "MediaEmbedElement";
-
-const MediaEmbedElement = withHOC(ResizableProvider, MediaEmbedElementBase);
+});
 
 export { MediaEmbedElement };
