88c418e20f25c398cf721dc4b94870cd0f7af5f8
Tapi221 <aaatsub1@gmail.com>
2026-06-13 20:11:24 +0900

    Move link base kit to plate UI

--- src/components/editor/plugins/link-base-kit.tsx
+++ src/components/editor/plugins/link-base-kit.tsx
@@ -1,6 +1,8 @@
 import { BaseLinkPlugin } from "@platejs/link";
-import { LinkElementStatic } from "@/chip/ui/node/link-node-static";
+import { LinkElementStatic } from "@/chip/ui/plate/link-node-static";
 
-const BaseLinkKit = [BaseLinkPlugin.withComponent(LinkElementStatic)];
+const BaseLinkKit = [
+  BaseLinkPlugin.withComponent(LinkElementStatic),
+];
 
 export { BaseLinkKit };
