6494f57c008e9a02accffe1a663a56da3f971b1f
Tapi221 <aaatsub1@gmail.com>
2026-06-13 17:30:09 +0900

    Fill Plate registry date node UI

--- src/chip/ui/plate/date-node.tsx
+++ src/chip/ui/plate/date-node.tsx
@@ -1,1 +1,17 @@
-export {};
+"use client";
+
+import type { TDateElement } from "platejs";
+import type { PlateElementProps } from "platejs/react";
+import { PlateElement } from "platejs/react";
+
+const DateElement = (props: PlateElementProps<TDateElement>) => {
+  const { element } = props;
+  return (
+    <PlateElement {...props} className="inline-block">
+      <span contentEditable={false}>{element.date ?? element.rawDate ?? "Pick a date"}</span>
+      {props.children}
+    </PlateElement>
+  );
+};
+
+export { DateElement };
