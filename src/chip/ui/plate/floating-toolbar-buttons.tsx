b904d7462f690998c48fb081abf8f1a716b07b6f
Tapi221 <aaatsub1@gmail.com>
2026-06-14 00:07:59 +0900

    Align Plate floating toolbar with official controls

--- src/chip/ui/plate/floating-toolbar-buttons.tsx
+++ src/chip/ui/plate/floating-toolbar-buttons.tsx
@@ -1,35 +1,59 @@
-import { BoldIcon, ItalicIcon, LinkIcon, MessageSquareIcon } from "lucide-react";
+"use client";
+
+import { BoldIcon, Code2Icon, ItalicIcon, StrikethroughIcon, UnderlineIcon, WandSparklesIcon } from "lucide-react";
+import { KEYS } from "platejs";
+import { useEditorReadOnly } from "platejs/react";
+import { AiToolbarButton } from "@/chip/ui/plate/ai-toolbar-button";
 import { CommentToolbarButton } from "@/chip/ui/plate/comment-toolbar-button";
+import { InlineEquationToolbarButton } from "@/chip/ui/plate/equation-toolbar-button";
 import { LinkToolbarButton } from "@/chip/ui/plate/link-toolbar-button";
 import { MarkToolbarButton } from "@/chip/ui/plate/mark-toolbar-button";
+import { MoreToolbarButton } from "@/chip/ui/plate/more-toolbar-button";
+import { SuggestionToolbarButton } from "@/chip/ui/plate/suggestion-toolbar-button";
 import { ToolbarGroup } from "@/chip/ui/plate/toolbar";
+import { TurnIntoToolbarButton } from "@/chip/ui/plate/turn-into-toolbar-button";
 
-type FloatingToolbarButtonsProps = {
-  withComment?: boolean;
+const FloatingToolbarButtons = () => {
+  const readOnly = useEditorReadOnly();
+  return (
+    <>
+      {!readOnly && (
+        <>
+          <ToolbarGroup>
+            <AiToolbarButton tooltip="AI commands">
+              <WandSparklesIcon />
+              Ask AI
+            </AiToolbarButton>
+          </ToolbarGroup>
+          <ToolbarGroup>
+            <TurnIntoToolbarButton />
+            <MarkToolbarButton nodeType={KEYS.bold} tooltip="Bold (⌘+B)">
+              <BoldIcon />
+            </MarkToolbarButton>
+            <MarkToolbarButton nodeType={KEYS.italic} tooltip="Italic (⌘+I)">
+              <ItalicIcon />
+            </MarkToolbarButton>
+            <MarkToolbarButton nodeType={KEYS.underline} tooltip="Underline (⌘+U)">
+              <UnderlineIcon />
+            </MarkToolbarButton>
+            <MarkToolbarButton nodeType={KEYS.strikethrough} tooltip="Strikethrough (⌘+⇧+M)">
+              <StrikethroughIcon />
+            </MarkToolbarButton>
+            <MarkToolbarButton nodeType={KEYS.code} tooltip="Code (⌘+E)">
+              <Code2Icon />
+            </MarkToolbarButton>
+            <InlineEquationToolbarButton />
+            <LinkToolbarButton />
+          </ToolbarGroup>
+        </>
+      )}
+      <ToolbarGroup>
+        <CommentToolbarButton />
+        <SuggestionToolbarButton />
+        {!readOnly && <MoreToolbarButton />}
+      </ToolbarGroup>
+    </>
+  );
 };
 
-const FloatingToolbarButtons = ({ withComment = true }: FloatingToolbarButtonsProps) => (
-  <>
-    <ToolbarGroup>
-      <MarkToolbarButton nodeType="bold" tooltip="Bold">
-        <BoldIcon />
-      </MarkToolbarButton>
-      <MarkToolbarButton nodeType="italic" tooltip="Italic">
-        <ItalicIcon />
-      </MarkToolbarButton>
-    </ToolbarGroup>
-    <ToolbarGroup>
-      <LinkToolbarButton tooltip="Link">
-        <LinkIcon />
-      </LinkToolbarButton>
-      {withComment ? (
-        <CommentToolbarButton>
-          <MessageSquareIcon />
-        </CommentToolbarButton>
-      ) : null}
-    </ToolbarGroup>
-  </>
-);
-
 export { FloatingToolbarButtons };
-export type { FloatingToolbarButtonsProps };
