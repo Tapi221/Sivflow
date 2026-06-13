4a4190b8e084cf7fb7f5ffdc8e3bb125f33b388a
Tapi221 <aaatsub1@gmail.com>
2026-06-13 23:51:05 +0900

    Use SlateElement for static file media

--- src/chip/ui/plate/media-file-node-static.tsx
+++ src/chip/ui/plate/media-file-node-static.tsx
@@ -1,15 +1,15 @@
 import type { TFileElement } from "platejs";
-import type { PlateElementProps } from "platejs/react";
-import { PlateElement } from "platejs/react";
+import type { SlateElementProps } from "platejs/static";
+import { SlateElement } from "platejs/static";
 
-const FileElementStatic = (props: PlateElementProps<TFileElement>) => {
+const FileElementStatic = (props: SlateElementProps<TFileElement>) => {
   const { element } = props;
   const label = element.name ?? element.url ?? "file";
   return (
-    <PlateElement {...props} className="my-px rounded-sm">
+    <SlateElement {...props} className="my-px rounded-sm">
       <span className="inline-flex items-center rounded px-1 py-0.5 text-primary underline underline-offset-4">{label as string}</span>
       {props.children}
-    </PlateElement>
+    </SlateElement>
   );
 };
 
