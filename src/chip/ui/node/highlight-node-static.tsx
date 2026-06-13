38c941116b6a56af8546572b70579e3c348806a4
Tapi221 <aaatsub1@gmail.com>
2026-06-13 20:17:10 +0900

    Remove migrated highlight static node

--- src/chip/ui/node/highlight-node-static.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from "react";
-import type { SlateLeafProps } from "platejs/static";
-import { SlateLeaf } from "platejs/static";
-
-const HighlightLeafStatic = (props: SlateLeafProps) => {
-  return (<SlateLeaf {...props} as="mark" className="bg-highlight/30 text-inherit"> {props.children} </SlateLeaf>);
-};
-
-export { HighlightLeafStatic };
