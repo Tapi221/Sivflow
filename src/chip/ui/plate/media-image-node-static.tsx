dcf6b11c6449b80a5bcff7cec896c52338140ae8
Tapi221 <aaatsub1@gmail.com>
2026-06-13 23:52:39 +0900

    Use SlateElement for static image media

--- src/chip/ui/plate/media-image-node-static.tsx
+++ src/chip/ui/plate/media-image-node-static.tsx
@@ -1,13 +1,13 @@
 import type { TImageElement } from "platejs";
-import type { PlateElementProps } from "platejs/react";
-import { PlateElement } from "platejs/react";
+import type { SlateElementProps } from "platejs/static";
+import { SlateElement } from "platejs/static";
 import { cn } from "@/lib/utils";
 
-const ImageElementStatic = (props: PlateElementProps<TImageElement>) => {
+const ImageElementStatic = (props: SlateElementProps<TImageElement>) => {
   const { element } = props;
   const caption = (element as any).caption?.[0]?.children?.[0]?.text;
   return (
-    <PlateElement {...props} className="py-2.5">
+    <SlateElement {...props} className="py-2.5">
       <figure className="my-0">
         <img
           className={cn("block max-w-full rounded-sm object-cover")}
@@ -17,7 +17,7 @@ const ImageElementStatic = (props: PlateElementProps<TImageElement>) => {
         {caption ? <figcaption className="mt-2 text-center text-muted-foreground text-sm">{caption}</figcaption> : null}
       </figure>
       {props.children}
-    </PlateElement>
+    </SlateElement>
   );
 };
 
