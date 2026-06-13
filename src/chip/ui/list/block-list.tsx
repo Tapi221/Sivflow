5329ad7fd69aca7714098e83f7a3e1296d491377
Tapi221 <aaatsub1@gmail.com>
2026-06-14 06:05:05 +0900

    Align legacy list block rendering

--- src/chip/ui/list/block-list.tsx
+++ src/chip/ui/list/block-list.tsx
@@ -1,85 +1,51 @@
 "use client";
-
-import React from "react";
-
+import type * as React from "react";
 import { isOrderedList } from "@platejs/list";
-
 import { useTodoListElement, useTodoListElementState } from "@platejs/list/react";
-
 import type { TListElement } from "platejs";
-
 import type { PlateElementProps, RenderNodeWrapper } from "platejs/react";
-
 import { useReadOnly } from "platejs/react";
-
 import { Checkbox } from "@/chip/ui/checkbox";
-
 import { cn } from "@/lib/utils";
 
+type ListItemProps = PlateElementProps & {
+  lineBreakBadge?: React.ReactNode;
+};
+type ListConfig = Record<string, { Li: React.FC<ListItemProps>; Marker: React.FC<PlateElementProps> }>;
 
+const TODO_CHECKBOX_CLASSNAME = "-left-6 absolute top-1 size-4 border-input bg-background shadow-none data-[state=checked]:border-foreground data-[state=checked]:bg-foreground data-[state=checked]:text-background";
 
 const TodoMarker = (props: PlateElementProps) => {
   const state = useTodoListElementState({ element: props.element });
   const todoProps = Object.values(useTodoListElement(state))[0];
   const readOnly = useReadOnly();
-
   return (
     <div contentEditable={false}>
-      <Checkbox
-        className={cn(
-          "-left-6 absolute top-1",
-          readOnly && "pointer-events-none",
-        )}
-        {...todoProps}
-      />
+      <Checkbox className={cn(TODO_CHECKBOX_CLASSNAME, readOnly && "pointer-events-none")} {...todoProps} />
     </div>
   );
 };
-
-const TodoLi = (props: PlateElementProps & { lineBreakBadge?: React.ReactNode; }) => {
+const TodoLi = (props: ListItemProps) => {
   return (
-    <li
-      className={cn(
-        "list-none",
-        (props.element.checked as boolean) &&
-        "text-muted-foreground line-through",
-      )}
-    >
+    <li className={cn("list-none", props.element.checked === true && "text-muted-foreground line-through")}>
       {props.children}
       {props.lineBreakBadge}
     </li>
   );
 };
-
-
-
-const config: Record<
-  string,
-  {
-    Li: React.FC<PlateElementProps & { lineBreakBadge?: React.ReactNode; }>;
-    Marker: React.FC<PlateElementProps>;
-  }
-> = {
+const LIST_CONFIG: ListConfig = {
   todo: {
     Li: TodoLi,
     Marker: TodoMarker,
   },
 };
-
-
-
-const List = (props: PlateElementProps & { lineBreakBadge?: React.ReactNode; }) => {
+const List = (props: ListItemProps) => {
   const { listStart, listStyleType } = props.element as TListElement;
-  const { Li, Marker } = config[listStyleType] ?? {};
-  const List = isOrderedList(props.element) ? "ol" : "ul";
-
+  const { Li, Marker } = LIST_CONFIG[listStyleType] ?? {};
+  const ListTag = isOrderedList(props.element) ? "ol" : "ul";
   return (
-    <List
-      className="relative m-0 p-0"
-      style={{ listStyleType }}
-      start={listStart}
-    >
-      {Marker && <Marker {...props} />}
+    <ListTag className="relative m-0 p-0" style={{ listStyleType }} start={listStart}>
+      {Marker ? <Marker {...props} /> : null}
       {Li ? (
         <Li {...props} />
       ) : (
@@ -88,17 +54,12 @@ const List = (props: PlateElementProps & { lineBreakBadge?: React.ReactNode; }) 
           {props.lineBreakBadge}
         </li>
       )}
-    </List>
+    </ListTag>
   );
 };
-
 const BlockList: RenderNodeWrapper = (props) => {
   if (!props.element.listStyleType) return;
-  if (!isOrderedList(props.element)) return;
-
-  return (props) => <List {...props} />;
+  return (nextProps) => <List {...nextProps} />;
 };
-
-
 
 export { BlockList };
