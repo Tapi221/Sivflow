8bf357580b5d7dced6fd77298f38b1d995526c5d
Tapi221 <aaatsub1@gmail.com>
2026-06-13 23:20:32 +0900

    r

--- src/chip/ui/plate/cursor-overlay.tsx
+++ src/chip/ui/plate/cursor-overlay.tsx
@@ -9,43 +9,35 @@ import { cn } from "@/lib/utils";
 const Cursor = ({ caretPosition, data, id, selection, selectionRects }: CursorOverlayState<CursorData>) => {
   const { style, selectionStyle = style } = data ?? ({} as CursorData);
   const isCursor = RangeApi.isCollapsed(selection);
-  return (
-    <>
-      {selectionRects.map((position, index) => (
-        <div
-          key={index}
-          className={cn(
-            "pointer-events-none absolute z-10",
-            id === "selection" && "bg-brand/25",
-            id === "selection" && isCursor && "bg-primary",
-          )}
-          style={{
-            ...selectionStyle,
-            ...position,
-          }}
-        />
-      ))}
-      {caretPosition ? (
-        <div
-          className={cn(
-            "pointer-events-none absolute z-10 w-0.5",
-            id === "drag" && "w-px bg-brand",
-          )}
-          style={{ ...caretPosition, ...style }}
-        />
-      ) : null}
-    </>
+  return selectionRects.map((position, index) => (
+    <div
+      key={index}
+      className={cn(
+        "pointer-events-none absolute z-10",
+        id === "selection" && "bg-brand/25",
+        id === "selection" && isCursor && "bg-primary",
+      )}
+      style={{
+        ...selectionStyle,
+        ...position,
+      }}
+    />
+  )).concat(
+    caretPosition ? (
+      <div
+        key="caret"
+        className={cn(
+          "pointer-events-none absolute z-10 w-0.5",
+          id === "drag" && "w-px bg-brand",
+        )}
+        style={{ ...caretPosition, ...style }}
+      />
+    ) : [],
   );
 };
 const CursorOverlay = () => {
   const { cursors } = useCursorOverlay();
-  return (
-    <>
-      {cursors.map((cursor) => (
-        <Cursor key={cursor.id} {...cursor} />
-      ))}
-    </>
-  );
+  return cursors.map((cursor) => <Cursor key={cursor.id} {...cursor} />);
 };
 
 export { CursorOverlay };
--- src/components/editor/plugins/discussion-kit.tsx
+++ src/components/editor/plugins/discussion-kit.tsx
@@ -1,8 +1,8 @@
 "use client";
 
 import { createPlatePlugin } from "platejs/react";
-import { BlockDiscussion } from "@/chip/ui/plate/block-discussion";
 import type { TComment } from "@/chip/ui/comment";
+import { BlockDiscussion } from "@/chip/ui/plate/block-discussion";
 
 type TDiscussion = {
   id: string;
