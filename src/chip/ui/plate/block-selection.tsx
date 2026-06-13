89b83b4980c767b5dc63ad6f9ec1b631874048ec
Tapi221 <aaatsub1@gmail.com>
2026-06-13 17:33:34 +0900

    Add Plate registry block selection UI

---
+++ src/chip/ui/plate/block-selection.tsx
@@ -0,0 +1,8 @@
+"use client";
+
+import { cva } from "class-variance-authority";
+
+const blockSelectionVariants = cva("pointer-events-none absolute inset-0 z-1 bg-brand/[.13] transition-opacity");
+const BlockSelection = () => null;
+
+export { BlockSelection, blockSelectionVariants };
