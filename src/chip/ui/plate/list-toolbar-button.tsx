bef1eeaba5984b246871feb56a4cad895ec8bc72
Tapi221 <aaatsub1@gmail.com>
2026-06-14 01:48:47 +0900

    Mark Plate list toolbar as client component

--- src/chip/ui/plate/list-toolbar-button.tsx
+++ src/chip/ui/plate/list-toolbar-button.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import * as React from "react";
 import { ListStyleType, someList, toggleList } from "@platejs/list";
 import { useIndentTodoToolBarButton, useIndentTodoToolBarButtonState } from "@platejs/list/react";
