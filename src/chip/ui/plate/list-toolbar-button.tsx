ed51334529d76b8dc8161bea971c5b5067276f20
Tapi221 <aaatsub1@gmail.com>
2026-06-13 20:58:06 +0900

    r

--- src/chip/ui/plate/list-toolbar-button.tsx
+++ src/chip/ui/plate/list-toolbar-button.tsx
@@ -1,24 +1,52 @@
-import { toggleList } from "@platejs/list";
+import { ListStyleType, toggleList } from "@platejs/list";
+import { useIndentTodoToolBarButton, useIndentTodoToolBarButtonState } from "@platejs/list/react";
+import { ListIcon, ListOrderedIcon, ListTodoIcon } from "lucide-react";
 import { useEditorRef } from "platejs/react";
-import type { ToolbarButtonProps } from "@/chip/ui/plate/toolbar";
 import { ToolbarButton } from "@/chip/ui/plate/toolbar";
+import type { ToolbarButtonProps } from "@/chip/ui/plate/toolbar";
 
 type ListToolbarButtonProps = ToolbarButtonProps & {
-  nodeType: string;
+  listStyleType?: ListStyleType;
 };
 
-const ListToolbarButton = ({ nodeType, onClick, ...props }: ListToolbarButtonProps) => {
+const ListToolbarButton = ({ listStyleType = ListStyleType.Disc, onClick, ...props }: ListToolbarButtonProps) => {
   const editor = useEditorRef();
   return (
     <ToolbarButton
       {...props}
       onClick={(event) => {
         onClick?.(event);
-        toggleList(editor, { listStyleType: nodeType });
+        toggleList(editor, { listStyleType });
       }}
     />
   );
 };
 
-export { ListToolbarButton };
+const BulletedListToolbarButton = ({ children, tooltip = "Bulleted list", ...props }: ToolbarButtonProps) => {
+  return (
+    <ListToolbarButton listStyleType={ListStyleType.Disc} tooltip={tooltip} {...props}>
+      {children ?? <ListIcon />}
+    </ListToolbarButton>
+  );
+};
+
+const NumberedListToolbarButton = ({ children, tooltip = "Numbered list", ...props }: ToolbarButtonProps) => {
+  return (
+    <ListToolbarButton listStyleType={ListStyleType.Decimal} tooltip={tooltip} {...props}>
+      {children ?? <ListOrderedIcon />}
+    </ListToolbarButton>
+  );
+};
+
+const TodoListToolbarButton = ({ children, tooltip = "Todo list", ...props }: ToolbarButtonProps) => {
+  const state = useIndentTodoToolBarButtonState({ nodeType: "todo" });
+  const { props: buttonProps } = useIndentTodoToolBarButton(state);
+  return (
+    <ToolbarButton tooltip={tooltip} {...props} {...buttonProps}>
+      {children ?? <ListTodoIcon />}
+    </ToolbarButton>
+  );
+};
+
+export { BulletedListToolbarButton, ListToolbarButton, NumberedListToolbarButton, TodoListToolbarButton };
 export type { ListToolbarButtonProps };
