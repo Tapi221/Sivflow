88dd20cab96bec59a17dcb7d87a7d5df0f7b999f
Tapi221 <aaatsub1@gmail.com>
2026-06-13 20:58:06 +0900

    r

--- src/chip/ui/plate/fixed-toolbar-buttons.tsx
+++ src/chip/ui/plate/fixed-toolbar-buttons.tsx
@@ -1,14 +1,28 @@
 "use client";
 
-import { BoldIcon, Code2Icon, HighlighterIcon, ItalicIcon, LinkIcon, ListIcon, ListOrderedIcon, MessageSquareIcon, OutdentIcon, TextIcon, UnderlineIcon } from "lucide-react";
+import { ArrowUpToLineIcon, BaselineIcon, BoldIcon, Code2Icon, HighlighterIcon, ItalicIcon, PaintBucketIcon, StrikethroughIcon, UnderlineIcon, WandSparklesIcon } from "lucide-react";
 import { KEYS } from "platejs";
 import { useEditorReadOnly } from "platejs/react";
+import { TableToolbarButton } from "@/chip/panel/buttonclickpanel.desktop/ButtonClickPanel.Note.Table";
+import { AlignToolbarButton } from "@/chip/ui/button/align-toolbar-button";
+import { EmojiToolbarButton } from "@/chip/ui/button/emoji-toolbar-button";
+import { ExportToolbarButton } from "@/chip/ui/button/export-toolbar-button";
+import { FontColorToolbarButton } from "@/chip/ui/button/font-color-toolbar-button";
+import { FontSizeToolbarButton } from "@/chip/ui/button/font-size-toolbar-button";
+import { ImportToolbarButton } from "@/chip/ui/button/import-toolbar-button";
+import { InsertToolbarButton } from "@/chip/ui/button/insert-toolbar-button";
+import { LineHeightToolbarButton } from "@/chip/ui/button/line-height-toolbar-button";
+import { MediaToolbarButton } from "@/chip/ui/button/media-toolbar-button";
+import { ModeToolbarButton } from "@/chip/ui/button/mode-toolbar-button";
+import { MoreToolbarButton } from "@/chip/ui/button/more-toolbar-button";
+import { ToggleToolbarButton } from "@/chip/ui/button/toggle-toolbar-button";
+import { TurnIntoToolbarButton } from "@/chip/ui/turn-into-toolbar-button";
 import { AiToolbarButton } from "@/chip/ui/plate/ai-toolbar-button";
 import { CommentToolbarButton } from "@/chip/ui/plate/comment-toolbar-button";
 import { RedoToolbarButton, UndoToolbarButton } from "@/chip/ui/plate/history-toolbar-button";
 import { IndentToolbarButton } from "@/chip/ui/plate/indent-toolbar-button";
 import { LinkToolbarButton } from "@/chip/ui/plate/link-toolbar-button";
-import { ListToolbarButton } from "@/chip/ui/plate/list-toolbar-button";
+import { BulletedListToolbarButton, NumberedListToolbarButton, TodoListToolbarButton } from "@/chip/ui/plate/list-toolbar-button";
 import { MarkToolbarButton } from "@/chip/ui/plate/mark-toolbar-button";
 import { ToolbarGroup } from "@/chip/ui/plate/toolbar";
 
@@ -23,9 +37,20 @@ const FixedToolbarButtons = () => {
             <RedoToolbarButton />
           </ToolbarGroup>
           <ToolbarGroup>
-            <AiToolbarButton>
-              <TextIcon />
+            <AiToolbarButton tooltip="AI commands">
+              <WandSparklesIcon />
             </AiToolbarButton>
+          </ToolbarGroup>
+          <ToolbarGroup>
+            <ExportToolbarButton>
+              <ArrowUpToLineIcon />
+            </ExportToolbarButton>
+            <ImportToolbarButton />
+          </ToolbarGroup>
+          <ToolbarGroup>
+            <InsertToolbarButton />
+            <TurnIntoToolbarButton />
+            <FontSizeToolbarButton />
           </ToolbarGroup>
           <ToolbarGroup>
             <MarkToolbarButton nodeType={KEYS.bold} tooltip="Bold">
@@ -37,25 +62,45 @@ const FixedToolbarButtons = () => {
             <MarkToolbarButton nodeType={KEYS.underline} tooltip="Underline">
               <UnderlineIcon />
             </MarkToolbarButton>
+            <MarkToolbarButton nodeType={KEYS.strikethrough} tooltip="Strikethrough">
+              <StrikethroughIcon />
+            </MarkToolbarButton>
             <MarkToolbarButton nodeType={KEYS.code} tooltip="Code">
               <Code2Icon />
             </MarkToolbarButton>
+            <FontColorToolbarButton nodeType={KEYS.color} tooltip="Text color">
+              <BaselineIcon />
+            </FontColorToolbarButton>
+            <FontColorToolbarButton nodeType={KEYS.backgroundColor} tooltip="Background color">
+              <PaintBucketIcon />
+            </FontColorToolbarButton>
           </ToolbarGroup>
           <ToolbarGroup>
-            <ListToolbarButton nodeType={KEYS.ol} tooltip="Numbered list">
-              <ListOrderedIcon />
-            </ListToolbarButton>
-            <ListToolbarButton nodeType={KEYS.ul} tooltip="Bulleted list">
-              <ListIcon />
-            </ListToolbarButton>
+            <AlignToolbarButton />
+            <NumberedListToolbarButton />
+            <BulletedListToolbarButton />
+            <TodoListToolbarButton />
+            <ToggleToolbarButton />
+          </ToolbarGroup>
+          <ToolbarGroup>
+            <LinkToolbarButton />
+            <TableToolbarButton />
+            <EmojiToolbarButton />
+          </ToolbarGroup>
+          <ToolbarGroup>
+            <MediaToolbarButton nodeType={KEYS.img} />
+            <MediaToolbarButton nodeType={KEYS.video} />
+            <MediaToolbarButton nodeType={KEYS.audio} />
+            <MediaToolbarButton nodeType={KEYS.file} />
+          </ToolbarGroup>
+          <ToolbarGroup>
+            <LineHeightToolbarButton />
             <IndentToolbarButton reverse>
-              <OutdentIcon />
             </IndentToolbarButton>
+            <IndentToolbarButton />
           </ToolbarGroup>
           <ToolbarGroup>
-            <LinkToolbarButton>
-              <LinkIcon />
-            </LinkToolbarButton>
+            <MoreToolbarButton />
           </ToolbarGroup>
         </>
       )}
@@ -64,9 +109,10 @@ const FixedToolbarButtons = () => {
         <MarkToolbarButton nodeType={KEYS.highlight} tooltip="Highlight">
           <HighlighterIcon />
         </MarkToolbarButton>
-        <CommentToolbarButton>
-          <MessageSquareIcon />
-        </CommentToolbarButton>
+        <CommentToolbarButton />
+      </ToolbarGroup>
+      <ToolbarGroup>
+        <ModeToolbarButton />
       </ToolbarGroup>
     </div>
   );
