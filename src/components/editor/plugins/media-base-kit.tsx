3fef6147adac11d84ca4fbf3a98bc8e91da3a78e
Tapi221 <aaatsub1@gmail.com>
2026-06-13 18:26:31 +0900

    Use Plate static file media UI

--- src/components/editor/plugins/media-base-kit.tsx
+++ src/components/editor/plugins/media-base-kit.tsx
@@ -1,9 +1,9 @@
 import { BaseCaptionPlugin } from "@platejs/caption";
 import { BaseAudioPlugin, BaseFilePlugin, BaseImagePlugin, BaseMediaEmbedPlugin, BasePlaceholderPlugin, BaseVideoPlugin } from "@platejs/media";
 import { KEYS } from "platejs";
-import { FileElementStatic } from "@/chip/ui/node/media-file-node-static";
 import { VideoElementStatic } from "@/chip/ui/node/media-video-node-static";
 import { AudioElementStatic } from "@/chip/ui/plate/media-audio-node-static";
+import { FileElementStatic } from "@/chip/ui/plate/media-file-node-static";
 import { ImageElementStatic } from "@/chip/ui/plate/media-image-node-static";
 
 const BaseMediaKit = [
