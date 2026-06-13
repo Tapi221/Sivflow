cfd5e853c50c0aca5fb5f8004f4933e611da6e29
Tapi221 <aaatsub1@gmail.com>
2026-06-14 05:45:09 +0900

    Align Plate todo checkbox styling

--- src/chip/ui/plate/block-list.tsx
+++ src/chip/ui/plate/block-list.tsx
@@ -1,27 +1,16 @@
 "use client";
-
-import * as React from "react";
-
+import type * as React from "react";
 import { isOrderedList } from "@platejs/list";
-
 import { useTodoListElement, useTodoListElementState } from "@platejs/list/react";
-
 import type { TListElement } from "platejs";
-
 import type { PlateElementProps, RenderNodeWrapper } from "platejs/react";
-
 import { useReadOnly } from "platejs/react";
-
 import { Checkbox } from "@/chip/ui/checkbox";
-
 import { cn } from "@/lib/utils";
-
-
 
 type ListItemProps = PlateElementProps & {
   lineBreakBadge?: React.ReactNode;
 };
-
 type ListConfig = Record<
   string,
   {
@@ -30,8 +19,7 @@ type ListConfig = Record<
   }
 >;
 
-
-
+const TODO_CHECKBOX_CLASSNAME = "-left-6 absolute top-1 size-4 border-input bg-background shadow-none data-[state=checked]:border-foreground data-[state=checked]:bg-foreground data-[state=checked]:text-background";
 const TodoMarker = (props: PlateElementProps) => {
   const state = useTodoListElementState({ element: props.element });
   const todoProps = Object.values(useTodoListElement(state))[0];
@@ -40,7 +28,7 @@ const TodoMarker = (props: PlateElementProps) => {
     <div contentEditable={false}>
       <Checkbox
         className={cn(
-          "-left-6 absolute top-1",
+          TODO_CHECKBOX_CLASSNAME,
           readOnly && "pointer-events-none",
         )}
         {...todoProps}
@@ -48,7 +36,6 @@ const TodoMarker = (props: PlateElementProps) => {
     </div>
   );
 };
-
 const TodoLi = (props: ListItemProps) => {
   return (
     <li
@@ -62,18 +49,12 @@ const TodoLi = (props: ListItemProps) => {
     </li>
   );
 };
-
-
-
 const config: ListConfig = {
   todo: {
     Li: TodoLi,
     Marker: TodoMarker,
   },
 };
-
-
-
 const List = (props: ListItemProps) => {
   const { listStart, listStyleType } = props.element as TListElement;
   const { Li, Marker } = config[listStyleType] ?? {};
@@ -92,13 +73,10 @@ const List = (props: ListItemProps) => {
     </ListTag>
   );
 };
-
 const BlockList: RenderNodeWrapper = (props) => {
   if (!props.element.listStyleType) return;
   if (!isOrderedList(props.element)) return;
   return (nextProps) => <List {...nextProps} />;
 };
-
-
 
 export { BlockList };
