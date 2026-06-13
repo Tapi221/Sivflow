304ef7acc94892a98dc25e52db47c9f0486a6878
Tapi221 <aaatsub1@gmail.com>
2026-06-13 23:35:02 +0900

    Use plate static media embed component

--- src/components/editor/plugins/media-base-kit.tsx
+++ src/components/editor/plugins/media-base-kit.tsx
@@ -2,6 +2,7 @@ import { BaseCaptionPlugin } from "@platejs/caption";
 import { BaseAudioPlugin, BaseFilePlugin, BaseImagePlugin, BaseMediaEmbedPlugin, BasePlaceholderPlugin, BaseVideoPlugin } from "@platejs/media";
 import { KEYS } from "platejs";
 import { AudioElementStatic } from "@/chip/ui/plate/media-audio-node-static";
+import { MediaEmbedElementStatic } from "@/chip/ui/plate/media-embed-node-static";
 import { FileElementStatic } from "@/chip/ui/plate/media-file-node-static";
 import { ImageElementStatic } from "@/chip/ui/plate/media-image-node-static";
 import { VideoElementStatic } from "@/chip/ui/plate/media-video-node-static";
@@ -11,6 +12,7 @@ const BaseMediaKit = [
   BaseVideoPlugin.withComponent(VideoElementStatic),
   BaseAudioPlugin.withComponent(AudioElementStatic),
   BaseFilePlugin.withComponent(FileElementStatic),
+  BaseMediaEmbedPlugin.withComponent(MediaEmbedElementStatic),
   BaseCaptionPlugin.configure({
     options: {
       query: {
@@ -18,7 +20,6 @@ const BaseMediaKit = [
       },
     },
   }),
-  BaseMediaEmbedPlugin,
   BasePlaceholderPlugin,
 ];
 
