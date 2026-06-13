c0e3bb55f42473c80fd54d459b58c712fad2572d
Tapi221 <aaatsub1@gmail.com>
2026-06-13 23:50:37 +0900

    Use SlateElement for static audio media

--- src/chip/ui/plate/media-audio-node-static.tsx
+++ src/chip/ui/plate/media-audio-node-static.tsx
@@ -1,18 +1,18 @@
 import type { TAudioElement } from "platejs";
-import type { PlateElementProps } from "platejs/react";
-import { PlateElement } from "platejs/react";
+import type { SlateElementProps } from "platejs/static";
+import { SlateElement } from "platejs/static";
 
-const AudioElementStatic = (props: PlateElementProps<TAudioElement>) => {
+const AudioElementStatic = (props: SlateElementProps<TAudioElement>) => {
   const { element } = props;
   const caption = (element as any).caption?.[0]?.children?.[0]?.text;
   return (
-    <PlateElement {...props} className="py-2.5">
+    <SlateElement {...props} className="py-2.5">
       <figure className="my-0">
         <audio className="w-full" src={element.url as string} controls />
         {caption ? <figcaption className="mt-2 text-center text-muted-foreground text-sm">{caption}</figcaption> : null}
       </figure>
       {props.children}
-    </PlateElement>
+    </SlateElement>
   );
 };
 
