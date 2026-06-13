906595a7ac777aecfeff9a6c58cb661a299fb7f7
Tapi221 <aaatsub1@gmail.com>
2026-06-13 17:32:37 +0900

    Fill Plate registry fixed toolbar buttons UI

--- src/chip/ui/plate/fixed-toolbar-buttons.tsx
+++ src/chip/ui/plate/fixed-toolbar-buttons.tsx
@@ -1,1 +1,22 @@
-export {};
+"use client";
+
+import * as React from "react";
+import { BoldIcon } from "lucide-react";
+import { KEYS } from "platejs";
+import { MarkToolbarButton } from "@/chip/ui/button/mark-toolbar-button";
+import { ToolbarGroup } from "@/chip/ui/plate/toolbar";
+
+const FixedToolbarButtons = () => {
+  return (
+    <div className="flex w-full">
+      <ToolbarGroup>
+        <MarkToolbarButton nodeType={KEYS.bold} tooltip="Bold">
+          <BoldIcon />
+        </MarkToolbarButton>
+      </ToolbarGroup>
+      <div className="grow" />
+    </div>
+  );
+};
+
+export { FixedToolbarButtons };
