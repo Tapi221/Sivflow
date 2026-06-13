584955d1ce079f9af7f83dddf38c6b05c448d5bf
Tapi221 <aaatsub1@gmail.com>
2026-06-13 23:11:44 +0900

    Enable missing Plate editor kits

--- src/components/editor/editor-kit.tsx
+++ src/components/editor/editor-kit.tsx
@@ -13,6 +13,7 @@ import { BlockMenuKit } from "@/components/editor/plugins/block-menu-kit";
 import { BlockPlaceholderKit } from "@/components/editor/plugins/block-placeholder-kit";
 import { CalloutKit } from "@/components/editor/plugins/callout-kit";
 import { CodeBlockKit } from "@/components/editor/plugins/code-block-kit";
+import { CodeDrawingKit } from "@/components/editor/plugins/code-drawing-kit";
 import { ColumnKit } from "@/components/editor/plugins/column-kit";
 import { CommentKit } from "@/components/editor/plugins/comment-kit";
 import { CopilotKit } from "@/components/editor/plugins/copilot-kit";
@@ -20,10 +21,12 @@ import { CursorOverlayKit } from "@/components/editor/plugins/cursor-overlay-kit
 import { DateKit } from "@/components/editor/plugins/date-kit";
 import { DiscussionKit } from "@/components/editor/plugins/discussion-kit";
 import { DndKit } from "@/components/editor/plugins/dnd-kit";
+import { DocxKit } from "@/components/editor/plugins/docx-kit";
 import { EmojiKit } from "@/components/editor/plugins/emoji-kit";
 import { ExitBreakKit } from "@/components/editor/plugins/exit-break-kit";
 import { FixedToolbarKit } from "@/components/editor/plugins/fixed-toolbar-kit";
 import { FloatingToolbarKit } from "@/components/editor/plugins/floating-toolbar-kit";
+import { FootnoteKit } from "@/components/editor/plugins/footnote-kit";
 import { FontKit } from "@/components/editor/plugins/font-kit";
 import { LineHeightKit } from "@/components/editor/plugins/line-height-kit";
 import { LinkKit } from "@/components/editor/plugins/link-kit";
@@ -45,6 +48,7 @@ const EditorKit = [
   // Elements
   ...BasicBlocksKit,
   ...CodeBlockKit,
+  ...CodeDrawingKit,
   ...TableKit,
   ...ToggleKit,
   ...TocKit,
@@ -81,7 +85,9 @@ const EditorKit = [
   TrailingBlockPlugin,
 
   // Parsers
+  ...DocxKit,
   ...MarkdownKit,
+  ...FootnoteKit,
 
   // UI
   ...BlockPlaceholderKit,
