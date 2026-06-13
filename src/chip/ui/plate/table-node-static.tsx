fa19764f8a70b9ff564b2be29b18e28076bbc5ab
Tapi221 <aaatsub1@gmail.com>
2026-06-14 01:57:37 +0900

    Align static Plate table UI with registry

--- src/chip/ui/plate/table-node-static.tsx
+++ src/chip/ui/plate/table-node-static.tsx
@@ -1,38 +1,86 @@
-import type { SlateElementProps } from "platejs/static";
+import * as React from "react";
+import { BaseTablePlugin } from "@platejs/table";
+import type { TTableCellElement, TTableElement } from "platejs";
 import { SlateElement } from "platejs/static";
+import type { SlateElementProps } from "platejs/static";
 import { cn } from "@/lib/utils";
 
-type TableNodeStaticProps = SlateElementProps;
+type TableCellElementStaticProps = SlateElementProps<TTableCellElement> & {
+  isHeader?: boolean;
+};
 
-const TableNodeStatic = ({ className, children, ...props }: TableNodeStaticProps) => (
-  <SlateElement className={cn("my-4 overflow-x-auto", className)} {...props}>
-    <table className="w-full border-collapse border border-border text-sm">{children}</table>
-  </SlateElement>
-);
-const TableRowNodeStatic = ({ className, children, ...props }: SlateElementProps) => (
-  <SlateElement as="tr" className={cn("border-border", className)} {...props}>
-    {children}
-  </SlateElement>
-);
-const TableCellNodeStatic = ({ className, children, ...props }: SlateElementProps) => (
-  <SlateElement as="td" className={cn("border border-border px-3 py-2 align-top", className)} {...props}>
-    {children}
-  </SlateElement>
-);
-const TableCellHeaderStatic = ({ className, children, ...props }: SlateElementProps) => (
-  <SlateElement
-    as="th"
-    className={cn("border border-border bg-muted px-3 py-2 text-left align-top font-medium", className)}
-    {...props}
-  >
-    {children}
-  </SlateElement>
-);
+const TableElementStatic = ({ children, ...props }: SlateElementProps<TTableElement>) => {
+  const { disableMarginLeft } = props.editor.getOptions(BaseTablePlugin);
+  const marginLeft = disableMarginLeft ? 0 : props.element.marginLeft;
+  return (
+    <SlateElement
+      {...props}
+      className="overflow-x-auto py-5"
+      style={{ paddingLeft: marginLeft }}
+    >
+      <div className="group/table relative w-fit">
+        <table
+          className="mr-0 ml-px table h-px table-fixed border-collapse"
+          style={{ borderCollapse: "collapse", width: "100%" }}
+        >
+          <tbody className="min-w-full">{children}</tbody>
+        </table>
+      </div>
+    </SlateElement>
+  );
+};
+const TableRowElementStatic = (props: SlateElementProps) => {
+  return (
+    <SlateElement {...props} as="tr" className="h-full">
+      {props.children}
+    </SlateElement>
+  );
+};
+const TableCellElementStatic = ({ isHeader, ...props }: TableCellElementStaticProps) => {
+  const { editor, element } = props;
+  const { api } = editor.getPlugin(BaseTablePlugin);
+  const { minHeight, width } = api.table.getCellSize({ element });
+  const borders = api.table.getCellBorders({ element });
+  return (
+    <SlateElement
+      {...props}
+      as={isHeader ? "th" : "td"}
+      attributes={{
+        ...props.attributes,
+        colSpan: api.table.getColSpan(element),
+        rowSpan: api.table.getRowSpan(element),
+      }}
+      className={cn(
+        "h-full overflow-visible border-none bg-background p-0",
+        element.background ? "bg-(--cellBackground)" : "bg-background",
+        isHeader && "text-left font-normal *:m-0",
+        "before:size-full",
+        "before:absolute before:box-border before:select-none before:content-['']",
+        borders &&
+          cn(
+            borders.bottom?.size && "before:border-b before:border-b-border",
+            borders.right?.size && "before:border-r before:border-r-border",
+            borders.left?.size && "before:border-l before:border-l-border",
+            borders.top?.size && "before:border-t before:border-t-border",
+          ),
+      )}
+      style={
+        {
+          "--cellBackground": element.background,
+          maxWidth: width || 240,
+          minWidth: width || 120,
+        } as React.CSSProperties
+      }
+    >
+      <div className="relative z-20 box-border h-full px-4 py-2" style={{ minHeight }}>
+        {props.children}
+      </div>
+    </SlateElement>
+  );
+};
+const TableCellHeaderElementStatic = (props: SlateElementProps<TTableCellElement>) => {
+  return <TableCellElementStatic {...props} isHeader />;
+};
 
-const TableElementStatic = TableNodeStatic;
-const TableRowElementStatic = TableRowNodeStatic;
-const TableCellElementStatic = TableCellNodeStatic;
-const TableCellHeaderElementStatic = TableCellHeaderStatic;
-
-export { TableCellElementStatic, TableCellHeaderElementStatic, TableCellHeaderStatic, TableCellNodeStatic, TableElementStatic, TableNodeStatic, TableRowElementStatic, TableRowNodeStatic };
-export type { TableNodeStaticProps };
+export { TableElementStatic, TableRowElementStatic, TableCellElementStatic, TableCellHeaderElementStatic };
+export type { TableCellElementStaticProps };
