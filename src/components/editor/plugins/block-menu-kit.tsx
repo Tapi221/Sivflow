1ebed3a2b3c09deb527caf8bb68acd255fc034f6
Tapi221 <aaatsub1@gmail.com>
2026-06-13 23:32:34 +0900

    Use plate block context menu

--- src/components/editor/plugins/block-menu-kit.tsx
+++ src/components/editor/plugins/block-menu-kit.tsx
@@ -1,9 +1,14 @@
 "use client";
 
 import { BlockMenuPlugin } from "@platejs/selection/react";
-import { BlockContextMenu } from "@/chip/ui/menu/block-context-menu";
+import { BlockContextMenu } from "@/chip/ui/plate/block-context-menu";
 import { BlockSelectionKit } from "@/components/editor/plugins/block-selection-kit";
 
-const BlockMenuKit = [...BlockSelectionKit, BlockMenuPlugin.configure({ render: { aboveEditable: BlockContextMenu } })];
+const BlockMenuKit = [
+  ...BlockSelectionKit,
+  BlockMenuPlugin.configure({
+    render: { aboveEditable: BlockContextMenu },
+  }),
+];
 
 export { BlockMenuKit };
