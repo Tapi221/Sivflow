36d81d1bc17e3005df454338cf2b8f310f28fd7b
Tapi221 <aaatsub1@gmail.com>
2026-06-15 07:29:41 +0900

    Use centralized emoji font token in callout nodes

--- src/chip/ui/plate/callout-node-static.tsx
+++ src/chip/ui/plate/callout-node-static.tsx
@@ -2,7 +2,7 @@ import type { SlateElementProps } from "platejs/static";
 import { SlateElement } from "platejs/static";
 import { cn } from "@/lib/utils";
 
-const EMOJI_FONT_FAMILY = "\"Apple Color Emoji\", \"Segoe UI Emoji\", NotoColorEmoji, \"Noto Color Emoji\", \"Segoe UI Symbol\", \"Android Emoji\", EmojiSymbols";
+const EMOJI_FONT_FAMILY = "var(--emoji-font-family)";
 
 const CalloutElementStatic = ({ children, className, ...props }: SlateElementProps) => {
   const backgroundColor = props.element.backgroundColor as string | undefined;
