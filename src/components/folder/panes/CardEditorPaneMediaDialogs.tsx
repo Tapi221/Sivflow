542c44b3f1ae874b4eef0d15b6a766547a865be8
Tapi221 <aaatsub1@gmail.com>
2026-06-15 06:58:52 +0900

    Remove card editor media dialog bridge before deletion

--- src/components/folder/panes/CardEditorPaneMediaDialogs.tsx
+++ src/components/folder/panes/CardEditorPaneMediaDialogs.tsx
@@ -1,2 +1,1 @@
-export { CardEditorPaneMediaDialogs } from "@/chip/panel/dialog.desktop/Dialog.CardEditorPaneMedia";
-export type { CardEditorPaneMediaDialogsProps } from "@/chip/panel/dialog.desktop/Dialog.CardEditorPaneMedia";
+export {};
