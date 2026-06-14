0b10d44c75c9c3e176ec732cf6515f301e4078d4
Tapi221 <aaatsub1@gmail.com>
2026-06-15 07:41:06 +0900

    Remove platform compatibility import

--- src/platform/capabilities/oauthBridge.ts
+++ src/platform/capabilities/oauthBridge.ts
@@ -1,5 +1,5 @@
 import type { OAuthBridgePort } from "@/application/ports/OAuthBridgePort";
-import platform from "@/platform";
+import { platform } from "@platform/index";
 
 const oauthBridge: OAuthBridgePort = { start: (authorizeUrl: string) => platform.oauth.start(authorizeUrl), cancel: () => platform.oauth.cancel(), takePendingCallback: () => platform.oauth.takePendingCallback(), storeRefreshToken: (input) => platform.oauth.storeRefreshToken(input), readRefreshToken: (accountId) => platform.oauth.readRefreshToken(accountId), deleteRefreshToken: (accountId) => platform.oauth.deleteRefreshToken(accountId), onCallback: (handler) => platform.oauth.onCallback(handler) };
 
