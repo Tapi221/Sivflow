19735cd92d148389dda2effd0ae44136552677d8
Tapi221 <aaatsub1@gmail.com>
2026-06-14 19:24:48 +0900

    Add Excalidraw editor kit

---
+++ src/components/editor/plugins/excalidraw-kit.tsx
@@ -0,0 +1,8 @@
+"use client";
+import { ExcalidrawElement, ExcalidrawPlugin } from "@platejs/excalidraw/react";
+
+const ExcalidrawKit = [
+  ExcalidrawPlugin.withComponent(ExcalidrawElement),
+];
+
+export { ExcalidrawKit };
