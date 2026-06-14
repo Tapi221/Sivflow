a45b79da565be333a8acf6a6ebb78aecbb4928c0
Tapi221 <aaatsub1@gmail.com>
2026-06-15 03:40:50 +0900

    r

--- src/features/import/presentation/web/XlsxImportDialog.tsx
+++ src/features/import/presentation/web/XlsxImportDialog.tsx
@@ -2,7 +2,7 @@ void "xlsx-import";
 
 
 
-export { XlsxImportDialog } from "@/chip/panel/dialog.desktop/Dialog.XlsxImport.nouse";
+export { XlsxImportDialog } from "@/chip/panel/dialog.desktop/Dialog.XlsxImport";
 
 
-export type { XlsxImportCompletedPayload } from "@/chip/panel/dialog.desktop/Dialog.XlsxImport.nouse";
+export type { XlsxImportCompletedPayload } from "@/chip/panel/dialog.desktop/Dialog.XlsxImport";
