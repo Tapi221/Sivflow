fbcb9a44f52320505432bd0afac5391f6bf6bb34
Tapi221 <aaatsub1@gmail.com>
2026-06-15 07:41:11 +0900

    Remove platform compatibility import

--- src/platform/capabilities/externalNavigation.ts
+++ src/platform/capabilities/externalNavigation.ts
@@ -1,5 +1,5 @@
 import type { ExternalNavigationPort } from "@/application/ports/ExternalNavigationPort";
-import platform from "@/platform";
+import { platform } from "@platform/index";
 
 const externalNavigation: ExternalNavigationPort = { openExternal: (url: string) => platform.shell.openExternal(url) };
 
