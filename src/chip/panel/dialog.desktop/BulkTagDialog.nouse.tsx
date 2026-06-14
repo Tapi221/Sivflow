3ede5323d6833409a1218cff11f787df96900d10
Tapi221 <aaatsub1@gmail.com>
2026-06-15 04:33:43 +0900

    r

--- src/chip/panel/dialog.desktop/BulkTagDialog.nouse.tsx
+++ src/chip/panel/dialog.desktop/BulkTagDialog.nouse.tsx
@@ -5,7 +5,7 @@ import { Dialog, DialogContent, DialogTitle } from "@/chip/ui/dialog/dialog";
 import { useTags } from "@/features/settings/hooks/useTags";
 import { cn } from "@/lib/utils";
 import { Tag as TagIcon } from "@/ui/icons";
-import { TagBadge } from "./Badge.Tag";
+import { TagBadge } from "../../budge/tag/Badge.Tag";
 
 interface BulkTagDialogProps {
   open?: boolean;
