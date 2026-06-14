ae81b3c8a55de9a564a53b38d5f96ebd353111c4
Tapi221 <aaatsub1@gmail.com>
2026-06-14 19:27:56 +0900

    Use local Excalidraw node component

--- src/components/editor/plugins/excalidraw-kit.tsx
+++ src/components/editor/plugins/excalidraw-kit.tsx
@@ -1,5 +1,6 @@
 "use client";
-import { ExcalidrawElement, ExcalidrawPlugin } from "@platejs/excalidraw/react";
+import { ExcalidrawPlugin } from "@platejs/excalidraw/react";
+import { ExcalidrawElement } from "@/chip/ui/plate/excalidraw-node";
 
 const ExcalidrawKit = [
   ExcalidrawPlugin.withComponent(ExcalidrawElement),
