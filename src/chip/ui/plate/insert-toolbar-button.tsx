30817b73240502f57e242d0060da0b030a9f7a5a
Tapi221 <aaatsub1@gmail.com>
2026-06-14 19:33:32 +0900

    Expose all media insert actions

--- src/chip/ui/plate/insert-toolbar-button.tsx
+++ src/chip/ui/plate/insert-toolbar-button.tsx
@@ -1,24 +1,13 @@
 "use client";
-
 import * as React from "react";
-
 import type { DropdownMenuProps } from "@radix-ui/react-dropdown-menu";
-
-import { CalendarIcon, ChevronRightIcon, Code2, Columns3Icon, FileCodeIcon, FilmIcon, Heading1Icon, Heading2Icon, Heading3Icon, ImageIcon, Link2Icon, ListIcon, ListOrderedIcon, MinusIcon, PenToolIcon, PilcrowIcon, PlusIcon, QuoteIcon, RadicalIcon, SquareIcon, SuperscriptIcon, TableIcon, TableOfContentsIcon } from "lucide-react";
-
+import { AudioLines, CalendarIcon, ChevronRightIcon, Code2, Columns3Icon, FileCodeIcon, FileUp, FilmIcon, Heading1Icon, Heading2Icon, Heading3Icon, ImageIcon, Link2Icon, ListIcon, ListOrderedIcon, MinusIcon, PenToolIcon, PilcrowIcon, PlusIcon, QuoteIcon, RadicalIcon, SquareIcon, SuperscriptIcon, TableIcon, TableOfContentsIcon } from "lucide-react";
 import { KEYS } from "platejs";
-
 import type { PlateEditor } from "platejs/react";
-
 import { useEditorRef } from "platejs/react";
-
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "@/chip/panel/dropdown-menu";
-
 import { insertBlock, insertInlineElement } from "@/components/editor/transforms";
-
 import { ToolbarButton, ToolbarMenuGroup } from "./toolbar";
-
-
 
 type Item = {
   focusEditor?: boolean;
@@ -27,13 +16,10 @@ type Item = {
   onSelect: (editor: PlateEditor, value: string) => void;
   value: string;
 };
-
 type Group = {
   group: string;
   items: Item[];
 };
-
-
 
 const createBlockItems = (items: Omit<Item, "onSelect">[]): Item[] => items.map((item) => ({
   ...item,
@@ -41,15 +27,12 @@ const createBlockItems = (items: Omit<Item, "onSelect">[]): Item[] => items.map(
     insertBlock(editor, value);
   },
 }));
-
 const createInlineItems = (items: Omit<Item, "onSelect">[]): Item[] => items.map((item) => ({
   ...item,
   onSelect: (editor, value) => {
     insertInlineElement(editor, value);
   },
 }));
-
-
 
 const groups: Group[] = [
   {
@@ -78,7 +61,10 @@ const groups: Group[] = [
     group: "Media",
     items: createBlockItems([
       { icon: <ImageIcon />, label: "Image", value: KEYS.img },
-      { icon: <FilmIcon />, label: "Embed", value: KEYS.mediaEmbed },
+      { icon: <FilmIcon />, label: "Video", value: KEYS.video },
+      { icon: <AudioLines />, label: "Audio", value: KEYS.audio },
+      { icon: <FileUp />, label: "File", value: KEYS.file },
+      { icon: <Link2Icon />, label: "Embed", value: KEYS.mediaEmbed },
     ]),
   },
   {
@@ -101,8 +87,6 @@ const groups: Group[] = [
     ]),
   },
 ];
-
-
 
 const InsertToolbarButton = (props: DropdownMenuProps) => {
   const editor = useEditorRef();
@@ -136,7 +120,5 @@ const InsertToolbarButton = (props: DropdownMenuProps) => {
     </DropdownMenu>
   );
 };
-
-
 
 export { InsertToolbarButton };
