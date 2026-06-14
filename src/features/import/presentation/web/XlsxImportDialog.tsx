6e8a444ffb9c6fb1e22a4b9bbe98d033ef0f33e0
Tapi221 <aaatsub1@gmail.com>
2026-06-15 07:05:43 +0900

    Clear legacy xlsx dialog module

--- src/features/import/presentation/web/XlsxImportDialog.tsx
+++ src/features/import/presentation/web/XlsxImportDialog.tsx
@@ -1,4 +1,1 @@
-void "xlsx-import";
-
-export { XlsxImportDialog } from "@/chip/panel/dialog.desktop/Dialog.XlsxImport";
-export type { XlsxImportCompletedPayload } from "@/chip/panel/dialog.desktop/Dialog.XlsxImport";
+export {};
