7338fa8629a89fea5d20117d273b6d3de182080e
Tapi221 <aaatsub1@gmail.com>
2026-06-15 06:50:54 +0900

    r

--- src/pane.desktop/tab.desktopnative/ScheduleScreen.desktopnative.tsx
+++ src/pane.desktop/tab.desktopnative/ScheduleScreen.desktopnative.tsx
@@ -1,5 +1,5 @@
 import type { ScheduleScreenProps } from "@/features/calendar/scheduleScreen.types";
-import { ScheduleScreen } from "@/pane.desktop/view/ScheduleScreen.desktop";
+import { ScheduleScreen } from "@/pane.desktop/view/Screen.Schedule.desktop";
 import { TabsBar } from "./TabsBar";
 
 const ScheduleScreenDesktopNative = (props: ScheduleScreenProps) => {
