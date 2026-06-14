d4c3dcea9261530171689c1b95822bec331485e7
Tapi221 <aaatsub1@gmail.com>
2026-06-15 07:05:05 +0900

    Clear legacy dialog module

--- src/features/import/presentation/web/ImportFormatDialog.tsx
+++ src/features/import/presentation/web/ImportFormatDialog.tsx
@@ -1,5 +1,1 @@
-const __moduleMarker = true;
-
-export { ImportFormatDialog } from "@/chip/panel/dialog.desktop/Dialog.ImportFormat";
-export { __moduleMarker };
-export type { ImportFormat } from "@/chip/panel/dialog.desktop/Dialog.ImportFormat";
+export {};
