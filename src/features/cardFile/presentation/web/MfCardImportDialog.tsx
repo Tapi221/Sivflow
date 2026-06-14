195064dfc69a32243ee73d8d049827e6487ea5ec
Tapi221 <aaatsub1@gmail.com>
2026-06-15 06:50:24 +0900

    Restore MFCard import bridge

--- src/features/cardFile/presentation/web/MfCardImportDialog.tsx
+++ src/features/cardFile/presentation/web/MfCardImportDialog.tsx
@@ -1,2 +1,5 @@
-// Removed. Import MfCardImportDialog from @/chip/panel/dialog.desktop/Dialog.MfCardImport.
-export {};
+import { MfCardImportDialog } from "@/chip/panel/dialog.desktop/Dialog.MfCardImport";
+import type { MfCardImportCompletedPayload, MfCardImportDialogProps } from "@/chip/panel/dialog.desktop/Dialog.MfCardImport";
+
+export { MfCardImportDialog };
+export type { MfCardImportCompletedPayload, MfCardImportDialogProps };
