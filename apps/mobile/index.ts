22c8848652cdc906bbfa67e6236c7df784cb292b
Tapi221 <aaatsub1@gmail.com>
2026-06-15 03:52:02 +0900

    Remove old mobile app entry

--- apps/mobile/index.ts
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { registerRootComponent } from "expo";
-import App from "@mobile-renderer/App";
-import { IosCalendarScheduleYear } from "@mobile/integration/ioscalendar/IosCalendarScheduleYear";
-
-const MobileApp = () => React.createElement(App, { ScheduleYearComponent: IosCalendarScheduleYear });
-
-registerRootComponent(MobileApp);
