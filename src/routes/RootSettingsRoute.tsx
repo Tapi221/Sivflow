174884f483cdaa0445a2dc99432194f7a1c7e0ea
Tapi221 <aaatsub1@gmail.com>
2026-06-15 06:41:14 +0900

    Add root settings route

---
+++ src/routes/RootSettingsRoute.tsx
@@ -0,0 +1,7 @@
+import { SettingsWorkspaceRootScreen } from "@/features/settings/SettingsWorkspaceRootScreen";
+
+const RootSettingsRoute = () => {
+  return <SettingsWorkspaceRootScreen />;
+};
+
+export { RootSettingsRoute };
