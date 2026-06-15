3e64a5411dced497387abd8b17e3e08fc5902e1f
Tapi221 <aaatsub1@gmail.com>
2026-06-16 00:15:17 +0900

    Normalize sidebar entity row spacing

--- src/components/folder/explorer/rows/SidebarEntityRow.tsx
+++ src/components/folder/explorer/rows/SidebarEntityRow.tsx
@@ -7,8 +7,6 @@ import { EXPLORER_ENTITY_ROW_DENSITY_COMPACT_CLASS, EXPLORER_ENTITY_ROW_INTERACT
 import { SidebarTreeRow } from "@/components/folder/explorer/rows/SidebarTreeRow";
 import { cn } from "@/lib/utils";
 
-
-
 interface SidebarEntityRowProps extends Omit<
   React.HTMLAttributes<HTMLDivElement>,
   "children" | "title"
@@ -43,8 +41,6 @@ interface SidebarEntityRowProps extends Omit<
   density?: "compact";
   selected?: boolean;
 }
-
-
 
 const SidebarEntityRowBase = ({
   menuOpen = false,
@@ -81,10 +77,7 @@ const SidebarEntityRowBase = ({
   ...props
 }: SidebarEntityRowProps) => {
   const handleMenuOpenChange = onMenuOpenChange ?? (() => {});
-
-  const densityClassName =
-    density === "compact" ? EXPLORER_ENTITY_ROW_DENSITY_COMPACT_CLASS : "";
-
+  const densityClassName = density === "compact" ? EXPLORER_ENTITY_ROW_DENSITY_COMPACT_CLASS : "";
   return (
     <SidebarTreeRow
       menuOpen={menuOpen}
@@ -144,8 +137,6 @@ const SidebarEntityRowBase = ({
     </SidebarTreeRow>
   );
 };
-
-
 
 const SidebarEntityRow = React.memo(SidebarEntityRowBase);
 SidebarEntityRow.displayName = "SidebarEntityRow";
