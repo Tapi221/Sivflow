f6807155b6e85640d0a6087880cb6694c9b92154
Tapi221 <aaatsub1@gmail.com>
2026-06-15 11:49:26 +0900

    Remove tag color barrel module

--- src/chip/budge/tag/tagColor.ts
+++
@@ -1,7 +0,0 @@
-import { DEFAULT_TAG_COLOR_KEY, TAG_COLOR_KEYS } from "@shared/design-tokens/color/Color.Tag";
-import type { TagColorKey, TagColorPalette } from "@shared/design-tokens/color/Color.Tag";
-import { getTagColorClassName, getTagColorKey } from "@/chip/budge/tag/tag.parser";
-import { getTagColorStyle, getTagColorSwatchStyle } from "@/chip/budge/tag/tag.style";
-
-export { DEFAULT_TAG_COLOR_KEY, TAG_COLOR_KEYS, getTagColorClassName, getTagColorKey, getTagColorStyle, getTagColorSwatchStyle };
-export type { TagColorKey, TagColorPalette };
