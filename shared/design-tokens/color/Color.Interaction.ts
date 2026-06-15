5e7066d8321ab4d8cdf4419526c9991449ca45f0
Tapi221 <aaatsub1@gmail.com>
2026-06-16 05:59:36 +0900

    Add outline interaction color token

--- shared/design-tokens/color/Color.Interaction.ts
+++ shared/design-tokens/color/Color.Interaction.ts
@@ -2,12 +2,14 @@ type InteractionColor = {
   accentHoverAlpha: number;
   accentPressedAlpha: number;
   accentSelectedAlpha: number;
+  accentOutlineAlpha: number;
 };
 
 const INTERACTION_COLOR = {
   accentHoverAlpha: 0.08,
   accentPressedAlpha: 0.12,
   accentSelectedAlpha: 0.14,
+  accentOutlineAlpha: 0.24,
 } as const satisfies InteractionColor;
 
 export { INTERACTION_COLOR };
