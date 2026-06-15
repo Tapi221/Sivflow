009b01b6599cf770d84b49d4e3e11a55499d5f52
Tapi221 <aaatsub1@gmail.com>
2026-06-15 20:21:15 +0900

    Move debounce hook to plate responsibility

---
+++ src/chip/ui/plate/hooks/useDebounce.ts
@@ -0,0 +1,19 @@
+import * as React from "react";
+
+const useDebounce = <T>(value: T, delay = 500) => {
+  const [debouncedValue, setDebouncedValue] = React.useState(value);
+
+  React.useEffect(() => {
+    const handler: NodeJS.Timeout = setTimeout(() => {
+      setDebouncedValue(value);
+    }, delay);
+
+    return () => {
+      clearTimeout(handler);
+    };
+  }, [value, delay]);
+
+  return debouncedValue;
+};
+
+export { useDebounce };
