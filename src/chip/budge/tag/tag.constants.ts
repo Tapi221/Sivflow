1e38e1227262eb721884f911dd0c7f4df921f96b
Tapi221 <aaatsub1@gmail.com>
2026-06-15 11:44:34 +0900

    Derive tag constants from shared tokens

--- src/chip/budge/tag/tag.constants.ts
+++ src/chip/budge/tag/tag.constants.ts
@@ -1,16 +1,5 @@
-const TAG_COLOR_KEYS = [
-  "gray",
-  "purple",
-  "teal",
-  "pink",
-  "amber",
-  "blue",
-  "green",
-  "red",
-  "coral",
-  "sky",
-] as const;
-const DEFAULT_TAG_COLOR_KEY = "gray";
+import { DEFAULT_TAG_COLOR_KEY } from "@shared/design-tokens/color/Color.Tag";
+
 const DEFAULT_TAG_COLOR_CLASS_NAME = `tag-color-${DEFAULT_TAG_COLOR_KEY}`;
 
-export { DEFAULT_TAG_COLOR_CLASS_NAME, DEFAULT_TAG_COLOR_KEY, TAG_COLOR_KEYS };
+export { DEFAULT_TAG_COLOR_CLASS_NAME };
