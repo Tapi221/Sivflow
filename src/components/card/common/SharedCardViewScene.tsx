4dd3a8d98e670011f5c39366d20b74670e25e957
Tapi221 <aaatsub1@gmail.com>
2026-06-16 02:06:35 +0900

    Use alias import in shared card view scene

--- src/components/card/common/SharedCardViewScene.tsx
+++ src/components/card/common/SharedCardViewScene.tsx
@@ -2,22 +2,17 @@ import React from "react";
 import { CardBlocksScene } from "@/components/card/blocks/shared/CardBlocksScene";
 import { filterRenderableCardBlocks } from "@/components/card/blocks/shared/isRenderableCardBlock";
 import { useViewerSceneProps } from "@/components/card/blocks/shared/useViewerSceneProps";
-import type { SharedCardContentViewProps } from "./SharedCardContent.types";
-
-
+import type { SharedCardContentViewProps } from "@/components/card/common/SharedCardContent.types";
 
 const SharedCardViewScene = ({ blocks, onGalleryFullscreenChange, displayMode, zoom }: SharedCardContentViewProps) => {
   const viewerProps = useViewerSceneProps({ onGalleryFullscreenChange, displayMode, zoom });
-
   const renderableBlocks = React.useMemo(
     () => filterRenderableCardBlocks(blocks),
     [blocks],
   );
-
   if (!renderableBlocks.length) {
     return null;
   }
-
   return (
     <CardBlocksScene
       blocks={renderableBlocks}
@@ -28,7 +23,5 @@ const SharedCardViewScene = ({ blocks, onGalleryFullscreenChange, displayMode, z
     />
   );
 };
-
-
 
 export { SharedCardViewScene };
