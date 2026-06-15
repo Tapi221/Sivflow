ad6ac75ce67df00d550b1605695aa259b1aed3c1
Tapi221 <aaatsub1@gmail.com>
2026-06-15 11:48:47 +0900

    Preserve tag key exports from shared tokens

--- src/chip/budge/tag/tag.constants.ts
+++ src/chip/budge/tag/tag.constants.ts
@@ -1,5 +1,5 @@
-import { DEFAULT_TAG_COLOR_KEY } from "@shared/design-tokens/color/Color.Tag";
+import { DEFAULT_TAG_COLOR_KEY, TAG_COLOR_KEYS } from "@shared/design-tokens/color/Color.Tag";
 
 const DEFAULT_TAG_COLOR_CLASS_NAME = `tag-color-${DEFAULT_TAG_COLOR_KEY}`;
 
-export { DEFAULT_TAG_COLOR_CLASS_NAME };
+export { DEFAULT_TAG_COLOR_CLASS_NAME, TAG_COLOR_KEYS };
