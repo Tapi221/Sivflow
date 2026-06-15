11b036d5bd6b82cd5e55ae8bf248a320ca436489
Tapi221 <aaatsub1@gmail.com>
2026-06-16 01:34:14 +0900

    Fix settings workspace root stylesheet import

--- src/chip/panel/dialog.desktop/Dialog.SettingsWorkspaceRoot.tsx
+++ src/chip/panel/dialog.desktop/Dialog.SettingsWorkspaceRoot.tsx
@@ -1,19 +1,13 @@
-import "@/features/settings/SettingsWorkspaceDialog.css";
-import { DialogDesktopPanel } from "./dialog";
+import "@/features/settings/Dialog.Settings.css";
+import { DialogDesktopPanel } from "@/chip/panel/dialog.desktop/dialog";
 import { SettingsWorkspaceRootScreen } from "@/features/settings/SettingsWorkspaceRootScreen";
-
-
 
 type SettingsWorkspaceRootPanelProps = {
   open: boolean;
   onOpenChange: (open: boolean) => void;
 };
 
-
-
 const SettingsWorkspaceRootContent = SettingsWorkspaceRootScreen;
-
-
 
 const SettingsWorkspaceRootPanel = ({ open, onOpenChange }: SettingsWorkspaceRootPanelProps) => {
   const handleClose = () => onOpenChange(false);
@@ -25,9 +19,5 @@ const SettingsWorkspaceRootPanel = ({ open, onOpenChange }: SettingsWorkspaceRoo
   );
 };
 
-
-
 export { SettingsWorkspaceRootPanel };
-
-
 export type { SettingsWorkspaceRootPanelProps };
