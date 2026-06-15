dfb5280ded3fc2fa7d3be9306bd589f25b56f10c
Tapi221 <aaatsub1@gmail.com>
2026-06-15 20:24:18 +0900

    Define local tag types from shared keys

--- src/chip/budge/tag/tag.types.ts
+++ src/chip/budge/tag/tag.types.ts
@@ -1,1 +1,11 @@
-export type { TagColorKey, TagColorPalette } from "@shared/design-tokens/color/Color.Tag";
+import { TAG_COLOR_KEYS } from "@shared/design-tokens/color/Color.Tag";
+type TagColorKey = (typeof TAG_COLOR_KEYS)[number];
+type TagColorPalette = {
+  bg: string;
+  bgRgb: readonly [number, number, number];
+  fg: string;
+  fgRgb: readonly [number, number, number];
+  border: string;
+  swatch: string;
+};
+export type { TagColorKey, TagColorPalette };
