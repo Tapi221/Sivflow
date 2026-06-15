2d281d9a185aedef98b9443051f9857c77aa5897
Tapi221 <aaatsub1@gmail.com>
2026-06-15 10:37:21 +0900

    Extract comment reply panel UI

--- src/chip/panel/buttonclickpanel.desktop/ButtonClickPanel.Note.Comment.tsx
+++ src/chip/panel/buttonclickpanel.desktop/ButtonClickPanel.Note.Comment.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import * as React from "react";
+import type * as React from "react";
 import { ArrowUpIcon } from "lucide-react";
 import type { Value } from "platejs";
 import { Plate } from "platejs/react";
