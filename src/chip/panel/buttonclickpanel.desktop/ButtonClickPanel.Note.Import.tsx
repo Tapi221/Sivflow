90786052fa345ccdd8410689409a0553604e15b4
Tapi221 <aaatsub1@gmail.com>
2026-06-15 09:09:19 +0900

    r

--- src/chip/panel/buttonclickpanel.desktop/ButtonClickPanel.Note.Import.tsx
+++ src/chip/panel/buttonclickpanel.desktop/ButtonClickPanel.Note.Import.tsx
@@ -9,11 +9,11 @@ import { useEditorRef } from "platejs/react";
 import { getEditorDOMFromHtmlString } from "platejs/static";
 import { useFilePicker } from "use-file-picker";
 import { DropdownMenu, DropdownMenuContent, DropdownMenuGroup, DropdownMenuItem, DropdownMenuTrigger } from "@/chip/panel/dropdown-menu";
-import { ToolbarButton } from "./toolbar";
+import { ToolbarButton } from "@/chip/ui/plate/toolbar";
 
 type ImportType = "html" | "markdown";
 
-const ImportToolbarButton = (props: DropdownMenuProps) => {
+const ButtonClickPanelNoteImport = (props: DropdownMenuProps) => {
   const editor = useEditorRef();
   const [open, setOpen] = React.useState(false);
   const getFileNodes = (text: string, type: ImportType) => {
@@ -67,4 +67,4 @@ const ImportToolbarButton = (props: DropdownMenuProps) => {
   );
 };
 
-export { ImportToolbarButton };
+export { ButtonClickPanelNoteImport };
