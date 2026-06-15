ed77f5e57e9b4ef4c6bea1fa5215524e37e4f434
Tapi221 <aaatsub1@gmail.com>
2026-06-16 02:06:02 +0900

    Fix duplicate block selection plugin registration

--- src/components/editor/plugins/block-menu-kit.tsx
+++ src/components/editor/plugins/block-menu-kit.tsx
@@ -1,20 +1,11 @@
 "use client";
-
 import { BlockMenuPlugin } from "@platejs/selection/react";
-
 import { BlockContextMenu } from "@/chip/ui/plate/block-context-menu";
 
-import { BlockSelectionKit } from "./block-selection-kit";
-
-
-
 const BlockMenuKit = [
-  ...BlockSelectionKit,
   BlockMenuPlugin.configure({
     render: { aboveEditable: BlockContextMenu },
   }),
 ];
-
-
 
 export { BlockMenuKit };
