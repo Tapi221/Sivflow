b9fba3c93e63ef12ec6d807624768841837979a8
Tapi221 <aaatsub1@gmail.com>
2026-06-17 07:27:35 +0900

    Remove providers from renderer app entry

--- packages/web-renderer/src/App.tsx
+++ packages/web-renderer/src/App.tsx
@@ -1,12 +1,7 @@
 import { AppContent } from "@web-renderer/app/AppContent";
-import { AppProviders } from "@web-renderer/app/AppProviders";
 
 const App = () => {
-  return (
-    <AppProviders>
-      <AppContent />
-    </AppProviders>
-  );
+  return <AppContent />;
 };
 
 export { App };
