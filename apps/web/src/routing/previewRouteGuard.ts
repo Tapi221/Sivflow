bcf961b40b884abbc1656249ea7f9ca799443ceb
Tapi221 <aaatsub1@gmail.com>
2026-06-17 07:28:06 +0900

    Add web preview route guard

---
+++ apps/web/src/routing/previewRouteGuard.ts
@@ -0,0 +1,14 @@
+import { DEV_MODE, isLocalHost } from "@/utils/envGuards";
+
+const isPreviewRouteEnabled = () => {
+  const hasPreviewParam = new URLSearchParams(window.location.search).get("preview_route") === "true";
+  if (!hasPreviewParam) {
+    return false;
+  }
+  if (!DEV_MODE) {
+    return false;
+  }
+  return isLocalHost(window.location.hostname);
+};
+
+export { isPreviewRouteEnabled };
