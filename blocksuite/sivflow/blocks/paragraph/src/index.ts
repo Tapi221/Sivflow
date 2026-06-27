b23bb2dee02aade86bdddcd23014dc29a8a57c06
Tapi221 <aaatsub1@gmail.com>
2026-06-27 21:58:34 +0900

    paragraph workerの公開を整理

--- blocksuite/sivflow/blocks/paragraph/src/index.ts
+++ blocksuite/sivflow/blocks/paragraph/src/index.ts
@@ -3,4 +3,3 @@ export * from './commands';
 export * from './paragraph-block.js';
 export * from './paragraph-block-config.js';
 export * from './turbo/paragraph-layout-handler';
-export * from './turbo/paragraph-painter.worker';
