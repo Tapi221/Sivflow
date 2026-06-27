fd929c4b1b2b240db22accd66c72522382af67f6
Tapi221 <aaatsub1@gmail.com>
2026-06-28 01:03:36 +0900

    非推奨GraphQLクエリを公開出口で上書き

--- packages/common/graphql/src/index.ts
+++ packages/common/graphql/src/index.ts
@@ -1,3 +1,15 @@
 export * from './fetcher';
 export * from './graphql';
 export * from './schema';
+export {
+  cancelSubscriptionMutation,
+  getCurrentUserQuery,
+  getWorkspacePageMetaByIdQuery,
+  getWorkspaceSubscriptionQuery,
+  quotaQuery,
+  refreshSubscriptionMutation,
+  requestApplySubscriptionMutation,
+  resumeSubscriptionMutation,
+  subscriptionQuery,
+  updateSubscriptionMutation,
+} from './graphql/deprecated-overrides';
