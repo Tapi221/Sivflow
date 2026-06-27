723fbdc33718648d7fd13ff3f79a4e7bcdf66fd6
Tapi221 <aaatsub1@gmail.com>
2026-06-28 01:23:44 +0900

    Blob一覧の戻り値型を修正

--- packages/backend/server/src/core/doc/storage/blob.ts
+++ packages/backend/server/src/core/doc/storage/blob.ts
@@ -12,5 +12,5 @@ export abstract class BlobStorageAdapter extends Connection {
   abstract getBlob(spaceId: string, key: string): Promise<Blob | null>;
   abstract setBlob(spaceId: string, blob: Blob): Promise<string>;
   abstract deleteBlob(spaceId: string, key: string): Promise<boolean>;
-  abstract listBlobs(spaceId: string): Promise<Blob>;
+  abstract listBlobs(spaceId: string): Promise<Blob[]>;
 }
