032ca22d02c4a8d687f2821c435bee316d233d92
Tapi221 <aaatsub1@gmail.com>
2026-06-27 21:21:07 +0900

    削除

--- packages/web-renderer/src/lib/utils.ts
+++
@@ -1,3 +0,0 @@
-const cn = (...inputs: Array<string | false | null | undefined>) => inputs.filter((input): input is string => typeof input === "string" && input.length > 0).join(" ");
-
-export { cn };
