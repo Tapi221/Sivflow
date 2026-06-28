be9d1bfed88fafbd5eb72e754479eaaa86a98d09
Tapi221 <aaatsub1@gmail.com>
2026-06-29 05:31:06 +0900

    空行ルールを整理

--- src/styles/tokens/codeTheme.ts
+++ src/styles/tokens/codeTheme.ts
@@ -1,5 +1,14 @@
-const codeTheme = { plain: { color: "var(--tok-plain)", backgroundColor: "transparent" }, styles: [{ types: ["comment", "prolog", "doctype", "cdata"], style: { color: "var(--tok-comment)" } }, { types: ["keyword", "atrule"], style: { color: "var(--tok-keyword)" } }, { types: ["class-name", "builtin", "type"], style: { color: "var(--tok-type)" } }, { types: ["function"], style: { color: "var(--tok-function)" } }, { types: ["string", "char", "inserted"], style: { color: "var(--tok-string)" } }, { types: ["number", "boolean", "constant", "symbol"], style: { color: "var(--tok-number)" } }, { types: ["operator", "entity", "url", "punctuation"], style: { color: "var(--tok-plain)" } }] };
-
-
+const codeTheme = {
+  plain: { color: "var(--tok-plain)", backgroundColor: "transparent" },
+  styles: [
+    { types: ["comment", "prolog", "doctype", "cdata"], style: { color: "var(--tok-comment)" } },
+    { types: ["keyword", "atrule"], style: { color: "var(--tok-keyword)" } },
+    { types: ["class-name", "builtin", "type"], style: { color: "var(--tok-type)" } },
+    { types: ["function"], style: { color: "var(--tok-function)" } },
+    { types: ["string", "char", "inserted"], style: { color: "var(--tok-string)" } },
+    { types: ["number", "boolean", "constant", "symbol"], style: { color: "var(--tok-number)" } },
+    { types: ["operator", "entity", "u\u0072l", "punctuation"], style: { color: "var(--tok-plain)" } },
+  ],
+};
 
 export { codeTheme };
