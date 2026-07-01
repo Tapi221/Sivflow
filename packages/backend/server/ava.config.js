af775b0cf8575f2e4ba20fffe3d4207f467d6f96
Tapi221 <aaatsub1@gmail.com>
2026-07-01 21:25:06 +0900

    バックエンドテストの配置方針を明確化

--- packages/backend/server/ava.config.js
+++ packages/backend/server/ava.config.js
@@ -1,16 +1,17 @@
 const newE2E = process.env.TEST_MODE === 'e2e';
-const externalTestsRoot = '../../../tests/backend/server/src';
-const newE2ETests = [`${externalTestsRoot}/__tests__/e2e/**/*.spec.ts`];
+const repositoryTestsRoot = '../../../tests';
+const backendTestsRoot = `${repositoryTestsRoot}/backend/server/src`;
+const newE2ETests = [`${backendTestsRoot}/__tests__/e2e/**/*.spec.ts`];
 
 const preludes = ['./src/prelude.ts'];
 
 if (newE2E) {
-  preludes.push(`${externalTestsRoot}/__tests__/e2e/prelude.ts`);
+  preludes.push(`${backendTestsRoot}/__tests__/e2e/prelude.ts`);
 }
 
 const backendTests = [
-  `${externalTestsRoot}/**/*.spec.ts`,
-  `${externalTestsRoot}/**/*.e2e.ts`,
+  `${backendTestsRoot}/**/*.spec.ts`,
+  `${backendTestsRoot}/**/*.e2e.ts`,
   ...newE2ETests.map(pattern => '!' + pattern),
 ];
 
