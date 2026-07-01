9f46ccb72cbe779704b3509c5aa0f6c3c8ab3951
Tapi221 <aaatsub1@gmail.com>
2026-07-01 21:46:08 +0900

    localチャンネルの対応を追加

--- packages/frontend/core/src/utils/channel.ts
+++ packages/frontend/core/src/utils/channel.ts
@@ -8,31 +8,34 @@ export const appSchemes = z.enum([
 ]);
 
 export type Scheme = z.infer<typeof appSchemes>;
-export type Channel = 'stable' | 'beta' | 'internal';
+export type Channel = 'stable' | 'beta' | 'internal' | 'local';
 
 export const schemeToChannel = {
   affine: 'stable',
   'affine-beta': 'beta',
   'affine-internal': 'internal',
-  'affine-dev': 'stable',
+  'affine-dev': 'local',
 } as Record<Scheme, Channel>;
 
 export const channelToScheme = {
   stable: BUILD_CONFIG.debug ? 'affine-dev' : 'affine',
   beta: 'affine-beta',
   internal: 'affine-internal',
+  local: 'affine-dev',
 } as Record<Channel, Scheme>;
 
 export const appIconMap = {
   stable: '/imgs/app-icon-stable.ico',
   beta: '/imgs/app-icon-beta.ico',
   internal: '/imgs/app-icon-internal.ico',
+  local: '/imgs/app-icon-stable.ico',
 } satisfies Record<Channel, string>;
 
 export const appNames = {
   stable: 'Sivflow',
   beta: 'Sivflow Beta',
   internal: 'Sivflow Internal',
+  local: 'Sivflow Local',
 } satisfies Record<Channel, string>;
 
 export const appSchemaUrl = z.custom<string>(
