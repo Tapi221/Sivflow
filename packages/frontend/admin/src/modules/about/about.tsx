5fe9473309ee936a22f73e33bdaab0b383a72bc9
Tapi221 <aaatsub1@gmail.com>
2026-07-01 21:41:36 +0900

    canaryチャンネルを削除

--- packages/frontend/admin/src/modules/about/about.tsx
+++ packages/frontend/admin/src/modules/about/about.tsx
@@ -9,13 +9,13 @@ import {
   UploadCloudIcon,
 } from 'lucide-react';
 
-type Channel = 'stable' | 'canary' | 'beta' | 'internal';
+type Channel = 'stable' | 'beta' | 'internal' | 'local';
 
 const appNames = {
-  stable: 'AFFiNE',
-  canary: 'AFFiNE Canary',
-  beta: 'AFFiNE Beta',
-  internal: 'AFFiNE Internal',
+  stable: 'Sivflow',
+  beta: 'Sivflow Beta',
+  internal: 'Sivflow Internal',
+  local: 'Sivflow Local',
 } satisfies Record<Channel, string>;
 const appName = appNames[BUILD_CONFIG.appBuildType];
 
@@ -23,7 +23,7 @@ const links = [
   {
     href: BUILD_CONFIG.githubUrl,
     icon: <GithubIcon size={20} />,
-    label: 'Star AFFiNE on GitHub',
+    label: 'Star Sivflow on GitHub',
   },
   {
     href: BUILD_CONFIG.githubUrl,
@@ -46,7 +46,7 @@ export function AboutSivflow() {
   return (
     <div className="flex flex-col h-full gap-3 py-5 px-6 w-full">
       <div className="flex items-center">
-        <span className="text-xl font-semibold">About AFFiNE</span>
+        <span className="text-xl font-semibold">About Sivflow</span>
       </div>
       <div className="overflow-y-auto space-y-[10px]">
         <div className="flex flex-col rounded-md border">
